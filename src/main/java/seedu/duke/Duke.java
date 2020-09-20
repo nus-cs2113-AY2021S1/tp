fc86cb34c3e3f9a4c53a629ab8cde4f3bc77e60f
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2020-09-21 01:58:54 +0800

    Rename in to SCANNER

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -2,17 +2,18 @@ package seedu.duke;
 
 import java.util.Scanner;
 import seedu.duke.constants.Logos;
+import seedu.duke.ui.UI;
 
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        System.out.println("Take a quiz with\n" + Logos.LOGO_1);
+        System.out.println("Take a quiz with\n" + Logos.DOTTED_CLICKER_LOGO);
         System.out.println("What is your name?");
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        Scanner SCANNER = new Scanner(System.in);
+        System.out.println("Hello " + SCANNER.nextLine());
 
     }
 }
