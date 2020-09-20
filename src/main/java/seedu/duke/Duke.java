55bc3e88de7007da93e32000b33d38833c8f1c67
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2020-09-21 02:08:34 +0800

    Rename clickerMessages to ClickerMessages

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -5,6 +5,7 @@ import seedu.duke.constants.Logos;
 import seedu.duke.ui.UI;
 
 public class Duke {
+    private static final Scanner SCANNER = new Scanner(System.in);
     /**
      * Main entry-point for the java.duke.Duke application.
      */
@@ -12,7 +13,6 @@ public class Duke {
         System.out.println("Take a quiz with\n" + Logos.DOTTED_CLICKER_LOGO);
         System.out.println("What is your name?");
 
-        Scanner SCANNER = new Scanner(System.in);
         System.out.println("Hello " + SCANNER.nextLine());
 
     }
--- src/main/java/seedu/duke/constants/ClickerMessages.java
+++ src/main/java/seedu/duke/constants/ClickerMessages.java
@@ -1,4 +1,4 @@
 package seedu.duke.constants;
 
-public class clickerMessages {
+public class ClickerMessages {
 }
