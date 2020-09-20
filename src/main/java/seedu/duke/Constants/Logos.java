08b7790a857850c87a6fb0fd19d4e5248429b15b
0xZ3RO <>
2020-09-20 23:41:07 +0800

    no message

--- src/main/java/seedu/duke/Constants/Logos.java
+++ src/main/java/seedu/duke/Constants/Logos.java
@@ -9,5 +9,5 @@ public class Logos {
     public static final String PLAIN_TEXT_DIVIDER = "--------------------------------------------------------------";
     public static final String CAT_TEXT_DIVIDER = "=^..^=  =^..^=  =^..^=   =^..^=   =^..^=   =^..^=   =^..^=";
     public static final String FENCE_TEXT_DIVIDER = "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+";
-    public static String custom_line_divider = "";
+    public static final String custom_line_divider = "";
 }
