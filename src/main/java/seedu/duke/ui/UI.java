41a0ed62932fab4ab402bdf2a2b1040f98e15df9
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2020-09-21 00:36:32 +0800

    Modify line divider and version number

--- src/main/java/seedu/duke/ui/UI.java
+++ src/main/java/seedu/duke/ui/UI.java
@@ -1,5 +1,22 @@
 package seedu.duke.ui;
 
 public class UI {
+    public static final String NEWLINE = System.lineSeparator();
+
+    public static void printCLIckerLogo() {
+    /*
+        String[] logos = new String[TootieConstants.NUM_LOGOS_AVAILABLE];
+        logos[0] = TootieSymbols.SIMPLE_TOOTIE_LOGO;
+        logos[1] = TootieSymbols.BLOCKY_TOOTIE_LOGO;
+        logos[2] = TootieSymbols.TRAIN_THEME_TOOTIE_LOGO;
+        logos[3] = TootieSymbols.THICK_TOOTIE_LOGO;
+        Random rand = new Random(System.currentTimeMillis());
+        System.out.println(String.format(TootieNormalMsgs.LOGO_PRINT_FORMAT, logos[Math.abs(rand.nextInt() % 4)]));
+    */
+    }
+
+    public static void printLineDivider() {
+
+    }
 
 }
