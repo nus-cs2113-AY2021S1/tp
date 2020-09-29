6693b2c160bf1322dec1014d62659e26051a8ca6
QX-CHEN <c.qixing@yahoo.com.sg>
2020-09-29 17:39:23 +0800

    Revert "First commit"
    This reverts commit a0e12cbf1afd1f2dbd4d7db0d67d23b85b8787c2.

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,10 +1,10 @@
-package seedu.PlaNUS;
+package seedu.duke;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class PlaNUSTest {
+class DukeTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
