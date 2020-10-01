c7fa39b88addde781ca0c77d9755881e308fbddf
czlin98 <czlin98@gmail.com>
2020-10-01 21:36:30 +0800

    Change '+' placements in string

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,11 +7,11 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = "    ____  __      _   ____  _______\n" +
-                "   / __ \\/ /___ _/ | / / / / / ___/\n" +
-                "  / /_/ / / __ `/  |/ / / / /\\__ \\ \n" +
-                " / ____/ / /_/ / /|  / /_/ /___/ / \n" +
-                "/_/   /_/\\__,_/_/ |_/\\____//____/  \n";
+        String logo = "    ____  __      _   ____  _______\n"
+                + "   / __ \\/ /___ _/ | / / / / / ___/\n"
+                + "  / /_/ / / __ `/  |/ / / / /\\__ \\ \n"
+                + " / ____/ / /_/ / /|  / /_/ /___/ / \n"
+                + "/_/   /_/\\__,_/_/ |_/\\____//____/  \n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
