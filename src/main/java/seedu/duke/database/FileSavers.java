5e30293d07a3bba8849a457e5d148942f46a1914
hungvo0603 <hungvo1011@gmail.com>
2020-10-01 18:12:05 +0800

    Add saver function of fill-in-the-blank questions

--- src/main/java/seedu/duke/database/FileSavers.java
+++ src/main/java/seedu/duke/database/FileSavers.java
@@ -27,4 +27,33 @@ public class FileSavers {
             e.printStackTrace();
         }
     }
+    public static void saveFillBlankQuestionToNewFile(String topic, String subTopic, String questionName,
+                                                      FillBlank question) {
+        // Creates a new directory for the file
+        String filePathWithoutQuestionName = "data\\Quizzes\\" + topic + "\\" + subTopic;
+        File directory = new File(filePathWithoutQuestionName);
+
+        if (directory.mkdirs()) {
+                System.out.println("A directory has just been created");
+        } else {
+            System.out.printf("Opening %s ...", filePathWithoutQuestionName);
+        }
+
+        // Creates a new _fb.txt file
+        String filePathWithQuestionName = filePathWithoutQuestionName + "\\" + questionName + "_fb.txt";
+        String textToWrite = "string before: " + question.getStringBefore() + System.lineSeparator() +
+                "blank: " + question.getCorrectAnswer() + System.lineSeparator() +
+                "string after: " + question.getStringAfter();;
+
+        File f = new File(filePathWithQuestionName);
+        try {
+            System.out.println("File name: " + f.getName());
+            FileWriter fw = new FileWriter(f.getAbsoluteFile());
+            fw.write(textToWrite);
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+    }
 }
