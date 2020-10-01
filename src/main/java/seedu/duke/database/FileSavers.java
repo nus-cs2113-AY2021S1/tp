fa506248c1da128377626e485f5a2da3e043168e
hungvo0603 <hungvo1011@gmail.com>
2020-10-01 18:13:43 +0800

    Merge saver functions for Qna and FillBlank questions

--- src/main/java/seedu/duke/database/FileSavers.java
+++ src/main/java/seedu/duke/database/FileSavers.java
@@ -1,34 +1,17 @@
 package seedu.duke.database;
 
+import seedu.duke.question.FillBlank;
+import seedu.duke.question.Qna;
+import seedu.duke.question.Question;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileSavers {
-    public static void saveQnaToNewFile(String topic, String subTopic, String questionName, Qna question) {
-        // Creates a new directory for the file
-        String filePathWithoutQuestionName = "data\\Quizzes\\" + topic + "\\" + subTopic;
-        File directory = new File(filePathWithoutQuestionName);
 
-        if (directory.mkdirs()) {
-            System.out.println("A directory has just been created");
-        } else {
-            System.out.printf("Opening %s ...", filePathWithoutQuestionName);
-        }
-
-        // Creates a new _qna.txt file
-        String filePathWithQuestionName = filePathWithoutQuestionName + "\\" + questionName + "_qna.txt";
-        String textToWrite = "question: " + question.getQuestion() + System.lineSeparator() +
-                "answer: " + question.getCorrectAnswer();
-        File f = new File(filePathWithQuestionName);
-        try {
-            System.out.println("File name: " + f.getName());
-            FileWriter fw = new FileWriter(f.getAbsoluteFile());
-            fw.write(textToWrite);
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("Error");
-            e.printStackTrace();
-        }
-    }
     public static void saveFillBlankQuestionToNewFile(String topic, String subTopic, String questionName,
-                                                      FillBlank question) {
+                                                      Question question) {
         // Creates a new directory for the file
         String filePathWithoutQuestionName = "data\\Quizzes\\" + topic + "\\" + subTopic;
         File directory = new File(filePathWithoutQuestionName);
@@ -41,9 +24,15 @@ public class FileSavers {
 
         // Creates a new _fb.txt file
         String filePathWithQuestionName = filePathWithoutQuestionName + "\\" + questionName + "_fb.txt";
-        String textToWrite = "string before: " + question.getStringBefore() + System.lineSeparator() +
-                "blank: " + question.getCorrectAnswer() + System.lineSeparator() +
-                "string after: " + question.getStringAfter();;
+        String textToWrite;
+        if (question instanceof FillBlank) {
+            textToWrite = "string before: " + ((FillBlank) question).getStringBefore() + System.lineSeparator() +
+                    "blank: " + question.getCorrectAnswer() + System.lineSeparator() +
+                    "string after: " + ((FillBlank) question).getStringAfter();
+        } else { //  if (question instanceof Qna)
+            textToWrite = "question: " + question.getQuestion() + System.lineSeparator() +
+                    "answer: " + question.getCorrectAnswer();
+        }
 
         File f = new File(filePathWithQuestionName);
         try {
@@ -56,4 +45,5 @@ public class FileSavers {
             e.printStackTrace();
         }
     }
+
 }
--- src/main/java/seedu/duke/question/Qna.java
+++ src/main/java/seedu/duke/question/Qna.java
@@ -1,7 +1,9 @@
 package seedu.duke.question;
 
 public class Qna extends Question {
+
     public Qna(String correctAnswer, String question) {
         super(correctAnswer, question);
     }
+
 }
