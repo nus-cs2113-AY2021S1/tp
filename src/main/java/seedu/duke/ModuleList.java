1f80b61409fbda9f41228e784e47116ee23b1df6
KennethEer <59533849+KennethEer@users.noreply.github.com>
2020-10-02 14:01:26 +0800

    Add ModuleList class with functions to add module and expected workload

---
+++ src/main/java/seedu/duke/ModuleList.java
@@ -0,0 +1,21 @@
+package seedu.duke;
+import java.util.ArrayList;
+
+public class ModuleList {
+
+    public static ArrayList<Module> modList = new ArrayList<>();
+
+    public static ArrayList<Module> add(String input) {
+        if (input.startsWith("addmod")) {
+            Module s = new Module(input.substring(7));
+            modList.add(s);
+            System.out.println(s + " is added");
+        } else if (input.startsWith("addexp")) {
+            Module s = new Module(input.substring(7, 13), input.substring(14));
+            modList.add(s);
+            System.out.println(s + " is added");
+        }
+        return modList;
+    }
+}
+
