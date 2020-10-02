d70c026035d4355f55ee1c15dd0330718824911f
limgl1998 <limgl1998@gmail.com>
2020-10-02 12:33:46 +0800

    testing tutorial

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -15,7 +15,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
-        Scanner IN = new Scanner(System.in);
-        System.out.println("Hello " + IN.nextLine());
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
     }
 }
