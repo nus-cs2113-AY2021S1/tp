72c6f2d7f8e0807a17cddadf584629cdd693e73c
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2020-10-02 11:40:19 +0800

    Fix Gradle issues

--- src/main/java/seedu/duke/parsers/CommandChecker.java
+++ src/main/java/seedu/duke/parsers/CommandChecker.java
@@ -20,25 +20,25 @@ public enum CommandChecker {
      * @return the type of command detected
      */
     public static CommandChecker extractCommandType(String userInput) {
-        if (userInput.toLowerCase().trim().startsWith("help")){
+        if (userInput.toLowerCase().trim().startsWith("help")) {
             return CommandChecker.HELP;
-        } else if (userInput.toLowerCase().trim().startsWith("settings")){
+        } else if (userInput.toLowerCase().trim().startsWith("settings")) {
             return CommandChecker.SETTINGS;
-        } else if (userInput.toLowerCase().trim().startsWith("add")){
+        } else if (userInput.toLowerCase().trim().startsWith("add")) {
             return CommandChecker.ADD;
-        } else if (userInput.toLowerCase().trim().startsWith("list")){
+        } else if (userInput.toLowerCase().trim().startsWith("list")) {
             return CommandChecker.LIST;
-        } else if (userInput.toLowerCase().trim().startsWith("history")){
+        } else if (userInput.toLowerCase().trim().startsWith("history")) {
             return CommandChecker.HISTORY;
-        } else if (userInput.toLowerCase().trim().startsWith("stats")){
+        } else if (userInput.toLowerCase().trim().startsWith("stats")) {
             return CommandChecker.STATS;
-        } else if (userInput.toLowerCase().trim().startsWith("review")){
+        } else if (userInput.toLowerCase().trim().startsWith("review")) {
             return CommandChecker.REVIEW;
-        } else if (userInput.toLowerCase().trim().startsWith("quiz")){
+        } else if (userInput.toLowerCase().trim().startsWith("quiz")) {
             return CommandChecker.QUIZ;
-        } else if (userInput.toLowerCase().trim().startsWith("clear")){
+        } else if (userInput.toLowerCase().trim().startsWith("clear")) {
             return CommandChecker.CLEAR;
-        } else if (userInput.toLowerCase().trim().startsWith("exit")){
+        } else if (userInput.toLowerCase().trim().startsWith("exit")) {
             return CommandChecker.EXIT;
         } else {
             return CommandChecker.UNRECOGNISED;
