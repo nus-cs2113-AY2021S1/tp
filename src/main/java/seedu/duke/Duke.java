5679539999c7f008406f5495947695291f304463
lhydl <dereklee9262@gmail.com>
2020-10-02 12:29:41 +0800

    Testing

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,12 +7,12 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String LOGO = " ____        _        \n"
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + LOGO);
+        System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
         Scanner in = new Scanner(System.in);
