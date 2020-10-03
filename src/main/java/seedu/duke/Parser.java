c28a47df880842ddfaa9d2d8204ccd353ced438b
lhydl <dereklee9262@gmail.com>
2020-10-03 12:30:12 +0800

    Add Parser class to parse user inputs

---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,60 @@
+package seedu.duke;
+
+import java.util.ArrayList;
+
+/**
+ * Parses user input
+ */
+public class Parser {
+    protected static boolean exit = false;
+
+    public static void parse(String input, ArrayList<Module> modList) {
+        String[] command = input.trim().split(" ");
+
+        switch (command[0].toLowerCase()) {
+        case "addmod":
+        case "addtime":
+        case "addexp":
+            ModuleList.add(input);
+            break;
+        case "deletemod":
+            //methods
+            break;
+        case "deleteexp":
+            //methods
+            break;
+        case "minus":
+            //methods
+            break;
+        case "compare":
+            //methods
+            break;
+        case "list":
+            //methods;
+            break;
+        case "help":
+            //methods
+            break;
+        case "exit":
+            if(input.trim().length()>4) {
+                Ui.printInvalidCommand();
+            } else {
+                Ui.printExitScreen();
+                exit = true;
+            }
+            break;
+        default:
+            System.out.println("Sorry, I don't understand");
+        }
+    }
+
+    /**
+     * Returns true if user types in "bye".
+     *
+     * @return status of exit
+     */
+    public static boolean isExit() {
+        return exit;
+    }
+
+}
