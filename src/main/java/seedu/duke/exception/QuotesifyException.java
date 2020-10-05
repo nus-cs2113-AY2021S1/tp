f6e7ff79ceb9dd283f4ffb9eb7229ac256ec250f
dozenmatter <chinheng.97@gmail.com>
2020-10-05 16:11:02 +0800

    Remove Javadoc comments

--- src/main/java/seedu/duke/exception/QuotesifyException.java
+++ src/main/java/seedu/duke/exception/QuotesifyException.java
@@ -1,15 +1,6 @@
 package seedu.duke.exception;
 
-import seedu.duke.quote.Quote;
-
-/**
- * Quotesify's personalised exception
- */
 public class QuotesifyException extends Exception {
-    /**
-     * Quotesify constructor
-     * @param errorMessage Quotesify exception message
-     */
     public QuotesifyException(String errorMessage) {
         super(errorMessage);
     }
