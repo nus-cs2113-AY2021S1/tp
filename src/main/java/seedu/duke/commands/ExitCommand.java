40db049fdbcd684d7059d6b3f9a9fd2d434ab2b3
chloesyy <chlyiyi@hotmail.com>
2020-10-05 19:40:01 +0800

    Add return statement for Parser to react to add commands.

--- src/main/java/seedu/duke/commands/ExitCommand.java
+++ src/main/java/seedu/duke/commands/ExitCommand.java
@@ -1,8 +1,11 @@
 package seedu.duke.commands;
 
+import seedu.duke.book.BookList;
+import seedu.duke.ui.TextUi;
+
 public class ExitCommand extends Command {
     @Override
-    public void execute() {
+    public void execute(TextUi ui, BookList books) {
 
     }
 
