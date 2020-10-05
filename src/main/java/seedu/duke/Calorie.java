945ee9068999232b973b39fe9daf77461fc4b9f4
harris maung <harrismaung3@gmail.com>
2020-10-06 02:00:46 +0800

    Exercise and Food Class

--- src/main/java/seedu/duke/Calorie.java
+++ src/main/java/seedu/duke/Calorie.java
@@ -1,4 +1,12 @@
 package seedu.duke;
 
 public class Calorie {
+    protected int amountOfCalories;
+
+    public Calorie(int calories){
+        this.amountOfCalories = calories;
+    }
+    public int get(){
+        return amountOfCalories;
+    }
 }
