40f5a99cc4c9395da4b0fb3532eae4edd1c81750
Kwek Zhan Hao <e0426161.u.nus.edu>
2020-10-05 16:55:21 +0800

    ijew

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,31 +1,66 @@
 package seedu.duke;
 
-import java.util.Scanner;
+import seedu.duke.commands.Command;
+import seedu.duke.commands.ExitCommand;
+import seedu.duke.exceptions.EmptyCommandException;
+import seedu.duke.exceptions.IncompleteCommandException;
+import seedu.duke.exceptions.UnknownCommandException;
+import seedu.duke.userinterface.Ui;
+import seedu.duke.tasks.Storage;
+import seedu.duke.tasks.TaskHelper;
+import seedu.duke.commands.Parser;
+import java.io.File;
+
 
 public class Duke {
+    private TaskHelper taskHelper;
+    private Ui ui;
+
+    private Storage storage;
+
     /**
-     * Main entry-point for the java.duke.Duke application.
+     * Initializes a new Ui, Storage and TaskHelper object
+     * Storage object is initialized with input String as the file path
+     * Performs a "list" method to display tasks already in the txt file from the file path
      */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
-
-        int check = 1;
-        if (check==1) {
-           System.out.println("Testin for forking workflow");
-        if (1 == 1)
-        {
-            System.out.println("true");
-        }
+    public Duke(String filePath) {
+        ui = new Ui();
+        ui.printWelcome();
+        storage = new Storage(getJarFilepath()+filePath);
+        taskHelper = new TaskHelper(storage.readFromFile(ui));
+        ui.printList(taskHelper.list());
     }
-}
 
-// jusufn
+    /**
+     * Takes in user input, deciphers and executes the command
+     * Method continues until exit condition isExit is true
+     * isExit is true when user inputs the exit Command
+     */
+    public void run() {
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                Command c = Parser.parse(fullCommand);
+                c.execute(taskHelper, ui, storage);
+                isExit = ExitCommand.isExit(c);
+            } catch (EmptyCommandException | IncompleteCommandException | UnknownCommandException e) {
+                ui.printToUser(e.getMessage());
+            }
+        }
+        ui.printGoodbye();
+    }
+
+    public static void main(String[] args) {
+        new Duke("/data/list.txt").run();
+    }
+
+    /**
+     * Returns path of jar file during execution to allow
+     * app to create txt file in the same location.
+     */
+    private static String getJarFilepath() {
+        return new File(Duke.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent().replace("%20", " ");
+    }
+
+}
