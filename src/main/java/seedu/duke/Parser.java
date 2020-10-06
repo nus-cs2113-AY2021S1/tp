41e9b09bcffc00e40d61c95214cd7b1b22a5a6f8
Zong Xian <zongxian96@gmail.com>
2020-10-06 15:43:31 +0800

    Removed Switch Case for Build Test

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -6,8 +6,5 @@ public class Parser {
         String[] words = userInput.trim().split(" ", 2);
         final String commandWord = words[0];
         final String arguments = userInput.replaceFirst(commandWord, "").trim();
-
-        switch (commandWord) {
-        }
     }
 }
