0fd9a1dcd6e15bed1c7ef0fb8a3c61d98f348fad
Zong Xian <zongxian96@gmail.com>
2020-10-06 15:37:58 +0800

    Create base Command & Parser Class.

---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,13 @@
+package seedu.duke;
+
+public class Parser {
+
+    public static void parseCommand(String userInput) {
+        String[] words = userInput.trim().split(" ", 2);
+        final String commandWord = words[0];
+        final String arguments = userInput.replaceFirst(commandWord, "").trim();
+
+        switch (commandWord) {
+        }
+    }
+}
