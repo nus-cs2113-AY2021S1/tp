b00b0109b3d1ccc57f0d2ab0553c5cb412b4a829
Wei Chin <eyoweichin@hotmail.com>
2020-10-06 20:29:45 +0800

    Update Duke.java to include a sample usage of Anime class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -17,5 +17,24 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+
+        addAnime();
     }
+
+    //Sample Usage of Anime Class [To Be Deleted]
+    private static void addAnime() {
+        System.out.println("===Running Sample Anime Class===");
+        String[] releaseDate = { "2020", "12", "30" };
+        String[] genre = { "Science", "Action", "Dance" };
+        String animeName = "Adventures of Adventurers";
+        int rating = 65;
+        int avgEpisodeLength = 30;
+        int totalEpisodes = 24;
+        Anime testAnime = new Anime(animeName, releaseDate, rating, genre, avgEpisodeLength, totalEpisodes);
+        System.out.println("Release Date in LocalTime: " + testAnime.getReleaseDate());
+        System.out.println("Release Date in String: " + testAnime.getReleaseDateInString());
+        System.out.println("===End of Sample Anime Class===");
+    }
+
 }
+
