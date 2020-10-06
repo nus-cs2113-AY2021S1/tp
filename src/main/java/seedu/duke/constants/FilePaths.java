37c8c5d9726b555ef5eb49717c34f112f8a863cc
0xZ3RO <>
2020-10-06 23:19:42 +0800

    Fix filepath

--- src/main/java/seedu/duke/constants/FilePaths.java
+++ src/main/java/seedu/duke/constants/FilePaths.java
@@ -6,16 +6,24 @@ import java.io.File;
  * Default file paths for save files.
  */
 public class FilePaths {
-    /** Default path to file for saving settings.  */
-    public static final String DEFAULT_USER_SETTINGS_FILE_PATH = "/data"+ File.pathSeparator+"userSettings.txt";
+    /**
+     * Default path to file for saving settings.
+     */
+    public static final String DEFAULT_USER_SETTINGS_FILE_PATH = "/data" + File.pathSeparator + "userSettings.txt";
 
-    /** Test file path to see where the files save. */
+    /**
+     * Test file path to see where the files save.
+     */
     public static final String TEST_FILE_PATH = "testFile.txt";
 
-    /** Path to store user past history. */
+    /**
+     * Path to store user past history.
+     */
     public static final String HISTORY_FILE_PATH = "data/history.txt";
 
-    /** The general path to store the statistics of the past quizzes. */
+    /**
+     * The general path to store the statistics of the past quizzes.
+     */
     public static final String STATS = "data/Stats/stats";
 
 }
