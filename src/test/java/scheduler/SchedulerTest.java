07a5ed0ade481764ccac080d63bd236bad947ffc
Darticune <lucastan@DESKTOP-5V1LNS0.localdomain>
2020-10-06 19:36:55 +0800

    Updated JUnit tests for Scheduler.java with proper naming convention

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -10,52 +10,52 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class SchedulerTest {
 
     @Test
-    void computeEasyDeadline_newIntervalOverLimit() {
+    void computeEasyDeadline_newIntervalOverLimit_intervalUnchanged() {
         assertEquals(LocalDate.now().plusDays(333), Scheduler.computeEasyDeadline(333));
     }
 
     @Test
-    void computeEasyDeadline_newIntervalWithinLimit() {
+    void computeEasyDeadline_newIntervalWithinLimit_increasedInterval() {
         assertEquals(LocalDate.now().plusDays(1), Scheduler.computeEasyDeadline(1));
     }
 
     @Test
-    void computeMediumDeadline_newIntervalOverLimit() {
+    void computeMediumDeadline_newIntervalOverLimit_intervalUnchanged() {
         assertEquals(LocalDate.now().plusDays(333), Scheduler.computeMediumDeadline(333));
     }
 
     @Test
-    void computeMediumDeadline_newIntervalWithinLimit() {
+    void computeMediumDeadline_newIntervalWithinLimit_increasedInterval() {
         assertEquals(LocalDate.now().plusDays(2), Scheduler.computeMediumDeadline(1));
     }
 
     @Test
-    void computeHardDeadline_newIntervalOverLimit() {
+    void computeHardDeadline_newIntervalOverLimit_intervalUnchanged() {
         assertEquals(LocalDate.now().plusDays(333), Scheduler.computeHardDeadline(333));
     }
 
     @Test
-    void computeHardDeadline_newIntervalWithinLimit() {
+    void computeHardDeadline_newIntervalWithinLimit_increasedInterval() {
         assertEquals(LocalDate.now().plusDays(4), Scheduler.computeHardDeadline(1));
     }
 
     @Test
-    void computeDeckDeadline_newIntervalOverLimit() {
+    void computeDeckDeadline_newIntervalOverLimit_intervalUnchanged() {
         assertEquals(LocalDate.now().plusDays(333), Scheduler.computeDeckDeadline(20, 1, 333));
     }
 
     @Test
-    void computeDeckDeadline_newIntervalWithinLimit() {
+    void computeDeckDeadline_newIntervalWithinLimit_increasedInterval() {
         assertEquals(LocalDate.now().plusDays(20), Scheduler.computeDeckDeadline(20, 1, 1));
     }
 
     @Test
-    void isDeadlineDue_deadlineIsDue() {
+    void isDeadlineDue_deadlineIsDue_true() {
         assertTrue(Scheduler.isDeadlineDue(Scheduler.getCurrentDate().minusDays(1)));
     }
 
     @Test
-    void isDeadlineDue_deadlineNotDue() {
+    void isDeadlineDue_deadlineNotDue_false() {
         assertFalse(Scheduler.isDeadlineDue(Scheduler.getCurrentDate().plusDays(1)));
     }
 
