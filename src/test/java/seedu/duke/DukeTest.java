fe93f0394400c7173270e5a702192794035298ed
Nathaniel Ho <nathaniel_ho1997@hotmail.com>
2020-10-06 23:49:27 +0800

    Update JUnitTest to test for adding quotes

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,12 +1,25 @@
 package seedu.duke;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.author.Author;
+import seedu.duke.quote.Quote;
+import seedu.duke.quote.QuoteList;
 
 class DukeTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void quoteAddTest() {
+        QuoteList quotes = new QuoteList();
+        Author author = new Author("Gill Bates");
+        Quote quote = new Quote("It was the butler", "Herlock Shomles", author);
+        quotes.add(quote);
+        assertEquals(quote, quotes.getQuote(0));
     }
 }
