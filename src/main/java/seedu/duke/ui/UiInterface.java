7b842dd3601064fee425e56d07457b9e6fe3e2ef
anqi20 <anqi20@hotmail.com>
2020-10-07 01:15:37 +0800

    Updated UI

--- src/main/java/seedu/duke/ui/UiInterface.java
+++ src/main/java/seedu/duke/ui/UiInterface.java
@@ -1,21 +1,23 @@
-package seedu.duke;
+package seedu.duke.ui;
+
+import seedu.duke.HintInterface;
+import seedu.duke.QuestionInterface;
+import seedu.duke.option.OptionInterface;
 
 public interface UiInterface {
     String getInputFromUser();
-
-    void showToUser();
 
     void printGreetMessage();
 
     void printExitMessage();
 
-    void printOption(OptionInterface option);
+    void printOption(OptionInterface option, int optionNumber);
 
-    void printQuestion(QuestionInterface question);
+    void printQuestion(QuestionInterface question, int questionNumber);
 
     void printHint(HintInterface hint);
 
-    void printStartQuizPage();
+    void printStartQuizPage(int numberOfQuestionsChosen, String topicsChosen);
 
     void printEndQuizPage();
 
