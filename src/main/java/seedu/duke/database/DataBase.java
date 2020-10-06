f582e3d086dcb9a20ed123c7ed7b2f3fc3d6aa52
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-10-06 20:36:06 +0800

    data base

--- src/main/java/seedu/duke/database/DataBase.java
+++ src/main/java/seedu/duke/database/DataBase.java
@@ -1,5 +1,10 @@
 package seedu.duke.database;
 
+import java.io.File;
+
 public class DataBase {
+    private static final String root_directory = System.getProperty("user.home");
+    private static final String duke_folder_name =  root_directory + File.separator + "diet-duke";
+    private static final String default_file_path = root_directory + File.separator + "diet-duke" + File.separator + "data-base";
 
 }
