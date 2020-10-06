d02fea170d0cedae5134cacce3bb9b8feb7de167
Nathaniel Ho <nathaniel_ho1997@hotmail.com>
2020-10-06 14:42:55 +0800

    Quote management - Add quotes

--- src/main/java/seedu/duke/commands/AddCommand.java
+++ src/main/java/seedu/duke/commands/AddCommand.java
@@ -5,10 +5,13 @@ import seedu.duke.book.Book;
 import seedu.duke.book.BookList;
 import seedu.duke.lists.ListManager;
 import seedu.duke.lists.QuotesifyList;
+import seedu.duke.quote.Quote;
+import seedu.duke.quote.QuoteList;
 import seedu.duke.ui.TextUi;
 
 public class AddCommand extends Command {
-    private static final String BOOK = "-b";
+    private static final String TAG_BOOK = "-b";
+    private static final String TAG_QUOTE = "-q";
 
     private String type;
     private String information;
@@ -21,20 +24,43 @@ public class AddCommand extends Command {
 
     public void execute(TextUi ui, ListManager listManager) {
         switch (type) {
-        case BOOK:
+        case TAG_BOOK:
             BookList books = (BookList) listManager.getList(ListManager.BOOK_LIST);
             addBook(books);
+            break;
+        case TAG_QUOTE:
+            QuoteList quotes = (QuoteList) listManager.getList(ListManager.QUOTE_LIST);
+            addQuote(quotes);
+            quotes.printAllQuotes();
             break;
         default:
         }
 
-        ui.printSuccessfulAddCommand();
+//        ui.printSuccessfulAddCommand();
     }
 
     private void addBook(BookList books) {
         String[] titleAndAuthor = information.split("/by", 2);
         Author author = new Author(titleAndAuthor[1].trim());
         books.add(new Book(author, titleAndAuthor[0].trim()));
+    }
+
+    private void addQuote(QuoteList quotes) {
+        if (information.contains("/from") && information.contains("/by")) {
+            String[] quoteAndInformation = information.split("/from", 2);
+            String[] referenceAndAuthor = quoteAndInformation[1].split("/by", 2);
+            Author author = new Author(referenceAndAuthor[1].trim());
+            quotes.add(new Quote(quoteAndInformation[0].trim(), referenceAndAuthor[0].trim(), author));
+        } else if (information.contains("/from")) {
+            String[] quoteAndReference = information.split("/from", 2);
+            quotes.add(new Quote(quoteAndReference[0].trim(), quoteAndReference[1].trim()));
+        } else if (information.contains("/by")) {
+            String[] quoteAndAuthor = information.split("/by", 2);
+            quotes.add(new Quote(quoteAndAuthor[0].trim(), quoteAndAuthor[1].trim()));
+        } else {
+            quotes.add(new Quote(information.trim()));
+        }
+
     }
 
     public boolean isExit() {
--- src/main/java/seedu/duke/quote/QuoteList.java
+++ src/main/java/seedu/duke/quote/QuoteList.java
@@ -1,7 +1,49 @@
 package seedu.duke.quote;
 
+import seedu.duke.lists.QuotesifyList;
+
 import java.util.ArrayList;
 
-public class QuoteList {
-    private ArrayList<Quote> quotes;
+public class QuoteList extends QuotesifyList<Quote> {
+    private ArrayList<Quote> quotes = super.getList();
+
+    public QuoteList() {
+        super(new ArrayList<>());
+    }
+
+    public QuoteList(ArrayList<Quote> quotes) {
+        super(quotes);
+    }
+
+    @Override
+    public void add(Quote newQuote) {
+        quotes.add(newQuote);
+    }
+
+    @Override
+    public void delete(int index) {
+        quotes.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public void printAllQuotes() {
+        System.out.println("Here are your quotes:");
+        for (Quote quote : quotes) {
+            String quoteToPrint = '\"' + quote.getQuote() + '\"';
+            String additionalInformation = "";
+            if (quote.getAuthor() != null && quote.getReference() != null) {
+                additionalInformation = " -" + quote.getAuthor().getName() + ", " + quote.getReference();
+            }else if (quote.getAuthor() != null) {
+                additionalInformation = " -" + quote.getReference();
+            } else if (quote.getReference() != null) {
+                additionalInformation = " -" + quote.getReference();
+            }
+            quoteToPrint += additionalInformation;
+            System.out.println(quoteToPrint);
+        }
+    }
 }
