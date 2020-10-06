f5028ba4b4d7cd8acbf2e8272cccd234845c0a2f
Zhi-You <tzyou98@gmail.com>
2020-10-07 01:45:22 +0800

    Add JUnit test for Hint class
    Added test case for getHintDescription method

--- src/main/java/seedu/duke/question/QuestionList.java
+++ src/main/java/seedu/duke/question/QuestionList.java
@@ -4,27 +4,28 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class QuestionList implements QuestionListInterface {
-    private ArrayList<QuestionInterface> questions;
+    private ArrayList<QuestionInterface> allQuestions;
     private ArrayList<QuestionInterface> questionsForQuiz;
-    private int questionsAnswered;
+    private int numberOfQuestionsAnswered;
 
     private static final Random RANDOM = new Random();
 
     public QuestionList(ArrayList<QuestionInterface> loadedQuestions) {
-        questions = loadedQuestions;
+        allQuestions = loadedQuestions;
         questionsForQuiz = null;
-        questionsAnswered = 0;
+        numberOfQuestionsAnswered = 0;
     }
 
 
     @Override
     public ArrayList<QuestionInterface> getChosenNumberOfRandomQuestion(int numberOfQuestionsForQuiz) {
         int numberOfQuestionsSelected = 0;
+        // prevent repeated questions from being selected again
         ArrayList<Integer> integersChosen = new ArrayList<>();
 
         while (numberOfQuestionsSelected < numberOfQuestionsForQuiz) {
             // get a random question that is within the bounds of the size of the available question list
-            int randomQuestionIndex = RANDOM.nextInt(questions.size() - 1);
+            int randomQuestionIndex = RANDOM.nextInt(allQuestions.size() - 1);
 
             // if the number is already selected - the question is already selected, we re run the loop
             // to select another random number
@@ -33,7 +34,7 @@ public class QuestionList implements QuestionListInterface {
             }
 
             // choose a random question from the question list and add it to the questions for quiz
-            questionsForQuiz.add(questions.get(randomQuestionIndex));
+            questionsForQuiz.add(allQuestions.get(randomQuestionIndex));
             numberOfQuestionsSelected++;
             integersChosen.add(randomQuestionIndex);
         }
@@ -53,23 +54,23 @@ public class QuestionList implements QuestionListInterface {
     @Override
     public void markQuestionAsAttempted() {
         // mark the current question as attempted if method is called in the Quiz class
-        questionsForQuiz.get(questionsAnswered).setAsAttempted();
+        questionsForQuiz.get(numberOfQuestionsAnswered).setAsAttempted();
         // the last increment will make it be the same as the number of questions there are
         // for instance, if there are 5 questions, the last call of this method will set the
         // question at index 4 to be attempted, and will increment the variable questionsAnswered
         // to 5, while not running again, since no more questions left.
-        questionsAnswered++;
+        numberOfQuestionsAnswered++;
     }
 
     @Override
     public QuestionInterface getNextQuestion() {
         // will be called after markQuestionAsAttempted(), so naturally the
         // questionsAnswered would have incremented
-        return questionsForQuiz.get(questionsAnswered);
+        return questionsForQuiz.get(numberOfQuestionsAnswered);
     }
 
     @Override
     public Boolean allQuestionsAnswered() {
-        return questionsAnswered == questionsForQuiz.size();
+        return numberOfQuestionsAnswered == questionsForQuiz.size();
     }
 }
