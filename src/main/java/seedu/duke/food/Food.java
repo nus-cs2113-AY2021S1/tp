e3111ed5d4b611c0794eb1178d2625fa7e3bdc61
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-10-06 20:13:32 +0800

    create food class

---
+++ src/main/java/seedu/duke/food/Food.java
@@ -0,0 +1,32 @@
+package seedu.duke.food;
+
+public class Food {
+    private final String name;
+    private final int calorie;
+    private final int carbohydrate;
+    private final int protein;
+    public Food(String name, int calorie, int carbohydrate, int protein){
+        this.name = name;
+        this.calorie = calorie;
+        this.carbohydrate = carbohydrate;
+        this.protein = protein;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getCalorie() {
+        return calorie;
+    }
+
+    public int getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+
+}
