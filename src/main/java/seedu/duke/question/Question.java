130a8f14085594c29c742910832e364a981d0f1c
Zhi-You <tzyou98@gmail.com>
2020-10-07 02:21:33 +0800

    Add javaDoc comments to Question class

--- src/main/java/seedu/duke/question/Question.java
+++ src/main/java/seedu/duke/question/Question.java
@@ -6,6 +6,10 @@ import seedu.duke.OptionListInterface;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a question of a certain topic. A <code>Question</code> object is represented by
+ * its description, options and hint.
+ */
 public class Question implements QuestionInterface {
     private String description;
     private OptionListInterface options;
@@ -33,26 +37,47 @@ public class Question implements QuestionInterface {
         return description;
     }
 
+    /**
+     * Returns the multiple options of the question.
+     *
+     * @return An arraylist of options.
+     */
     @Override
     public ArrayList<OptionInterface> getOptions() {
         return options.getOptionList();
     }
 
+    /**
+     * Returns the description of the hint tied to the question.
+     *
+     * @return Details of the hint.
+     */
     @Override
     public String getHint() {
         return hint.getHintDescription();
     }
 
+    /**
+     * Returns the description of the question.
+     *
+     * @return Question information.
+     */
     @Override
     public boolean hintWasShown() {
         return wasHintShown;
     }
+
 
     @Override
     public void setHintShown() {
         wasHintShown = true;
     }
 
+    /**
+     * Returns a boolean variable indicating if the question was answered correctly.
+     *
+     * @return True if question was answered correctly, false otherwise.
+     */
     @Override
     public boolean wasAnsweredCorrectly() {
         return wasAnsweredCorrectly;
@@ -63,10 +88,16 @@ public class Question implements QuestionInterface {
         wasAnsweredCorrectly = true;
     }
 
+    /**
+     * Returns a boolean variable indicating if the question was already attempted before.
+     *
+     * @return Indication of whether the question was attempted before or not.
+     */
     @Override
     public boolean wasAttempted() {
         return wasAttempted;
     }
+
 
     @Override
     public void setAsAttempted() {
