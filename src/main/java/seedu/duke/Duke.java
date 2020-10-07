9ca1fab35720c5abd2dafdf8af48d1bab9e91b75
wamikamalik <wamikamalik@hotmail.com>
2020-10-07 13:21:25 +0400

    Update main class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,35 @@
 package seedu.duke;
 
-import java.util.Scanner;
+import seedu.duke.exceptions.CustomException;
+import seedu.duke.logic.parser.Parser;
+import seedu.duke.ui.Ui;
 
 public class Duke {
+
+    private static Parser parser;
+
+    public Duke() {
+        parser = new Parser();
+    }
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        new Duke();
+        boolean isOngoing = true;
+
+        Ui.printWelcomeMessage();
+
+        while(isOngoing) {
+            try {
+                parser.getCommand();
+                isOngoing = parser.extractType();
+            } catch (CustomException error) {
+                Ui.showError(error);
+            }
+        }
     }
+
 }
