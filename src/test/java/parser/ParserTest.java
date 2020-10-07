d565a703c5ba2e7c2a6e374f7e185da5918e8b32
Jane <ngshulinjane@hotmail.com>
2020-10-08 00:41:06 +0800

    Fix checkstyle error

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -10,15 +10,15 @@ class ParserTest {
     public void parse_addCommandInvalidArgs_expectException() {
         Parser parser = new Parser();
         final String[] inputs = {
-                "add",
-                "add ",
-                "add wrong args format",
-                // no question and answer separator prefix
-                "add q:When is v1.0 due? a:15 Oct 2020",
-                // no question prefix
-                "add When is v1.0 due? | a:15 Oct 2020",
-                // no answer prefix
-                "add q:When is v1.0 due? | 15 Oct 2020",
+            "add",
+            "add ",
+            "add wrong args format",
+            // no question and answer separator prefix
+            "add q:When is v1.0 due? a:15 Oct 2020",
+            // no question prefix
+            "add When is v1.0 due? | a:15 Oct 2020",
+            // no answer prefix
+            "add q:When is v1.0 due? | 15 Oct 2020",
         };
         for (String input : inputs) {
             assertThrows(InvalidInputException.class, () -> parser.parse(input));
