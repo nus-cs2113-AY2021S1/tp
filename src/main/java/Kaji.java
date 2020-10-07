c084400eee39880505a8d1c4008d2cf7a34eaae8
Ng Shu Lin Jane <ngshulinjane@hotmail.com>
2020-10-07 22:13:08 +0800

    Revert "Add help command and capability to store questions"

--- src/main/java/Kaji.java
+++ src/main/java/Kaji.java
@@ -2,10 +2,7 @@ import commands.Command;
 import exception.InvalidInputException;
 import manager.chapter.CardList;
 import parser.Parser;
-import storage.Storage;
 import ui.Ui;
-
-import java.io.IOException;
 
 public class Kaji {
     private CardList cards;
@@ -18,20 +15,15 @@ public class Kaji {
 
     public void run() {
         ui.showWelcome();
-        ui.showHelpList();
         boolean isExit = false;
-        Storage.getFileContents(cards);
         while (!isExit) {
             try {
                 String fullCommand = ui.readCommand();
                 Command c = Parser.parse(fullCommand);
                 c.execute(cards, ui);
-                Storage.writeToFile(cards);
                 isExit = c.isExit();
             } catch (InvalidInputException e) {
                 System.out.println("Invalid input given");
-            } catch (IOException e) {
-                System.out.println("     Something went wrong: " + e.getMessage());
             }
         }
     }
