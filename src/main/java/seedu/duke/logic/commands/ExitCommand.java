a103533206f909a10b51750210204fd8b59b0712
wamikamalik <wamikamalik@hotmail.com>
2020-10-07 13:19:58 +0400

    Add command classes

---
+++ src/main/java/seedu/duke/logic/commands/ExitCommand.java
@@ -0,0 +1,18 @@
+package seedu.duke.logic.commands;
+
+import seedu.duke.ui.Ui;
+
+public class ExitCommand extends Command {
+
+
+
+    @Override
+    public void executeCommand() {
+        Ui.printExitMessage();
+    }
+
+    @Override
+    public boolean isOngoing() {
+        return false;
+    }
+}
