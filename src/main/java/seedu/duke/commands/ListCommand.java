ad425ace614e427701498ca85792f274da7bca95
Zong Xian <zongxian96@gmail.com>
2020-10-08 02:51:07 +0800

    Update ListCommand to print either locations or appliances only.

--- src/main/java/seedu/duke/commands/ListCommand.java
+++ src/main/java/seedu/duke/commands/ListCommand.java
@@ -8,15 +8,39 @@ public class ListCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Display all the appliances that have been added to SmartHomeBot \n"
             + "Example: " + COMMAND_WORD;
+    private final String parameter;
+    private static final String APPLIANCE_TYPE = "appliance";
+    private static final String LOCATION_TYPE = "location";
+
+    public ListCommand(String arguments) {
+        parameter = arguments;
+    }
 
     @Override
     public void execute() {
         int index = 1;
-        for (Appliance a : appliances.getAllAppliance()) {
-            System.out.print(index + ": " + a + "|" + a.getLocation() + "\n");
-            index++;
+        if (parameter.equals(LOCATION_TYPE)) {
+            if (homeLocationsList.getLocations().size() == 0) {
+                System.out.println("There is currently no locations in the list");
+                return;
+            }
+            System.out.println("Here are the location you have entered.");
+            for (String location : homeLocationsList.getLocations()) {
+                System.out.println(index + ": " + location);
+                index++;
+            }
+        } else if (parameter.equals(APPLIANCE_TYPE)) {
+            if (appliances.getAllAppliance().size() == 0) {
+                System.out.println("There is currently no appliances in the list");
+                return;
+            }
+            System.out.println("Here are the appliances in your list.");
+            for (Appliance a : appliances.getAllAppliance()) {
+                System.out.println(index + ": " + a.getName() + " | Location: " + a.getLocation() + " | Status: "
+                        + a.getStatus() + " | Watt: " + a.getPower() + " | Type: " + a.getType());
+                index++;
+            }
         }
-        System.out.println(homeLocationsList);
     }
 
 }
