981ed5ab3e4ae78cbcf3becbd260c210243e2665
Jazhten Low <jazhten@gmail.com>
2020-10-08 20:34:52 +0800

    Add Exception Template

--- src/main/java/seedu/duke/utility/Ui.java
+++ src/main/java/seedu/duke/utility/Ui.java
@@ -1,4 +1,8 @@
 package seedu.duke.utility;
 
 public class Ui {
+
+    public static void printIOException() {
+        System.out.println(ErrorHandling.ExceptionResponse.EXCEPTION_IO_EXCEPTION);
+    }
 }
