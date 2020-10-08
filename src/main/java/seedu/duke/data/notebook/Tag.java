85af0e93b9b2863b80d7a3c06c5440b0173dc126
Chongjx <Chongjx@users.noreply.github.com>
2020-10-08 20:57:06 +0800

    Changed naming to meet coding standard
    Co-authored-by: R Ramana <54029207+R-Ramana@users.noreply.github.com>

--- src/main/java/seedu/duke/data/notebook/Tag.java
+++ src/main/java/seedu/duke/data/notebook/Tag.java
@@ -26,7 +26,7 @@ public class Tag {
         COLOR_CYAN("\u001B[36m"),
         COLOR_RESET("\u001B[0m");
 
-        private final String color;
+        private final String COLOR;
 
         TagColor(String color) {
             this.color = color;
