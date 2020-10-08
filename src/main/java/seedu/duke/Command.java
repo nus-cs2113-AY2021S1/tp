fa2e2f9cfaf2c86bf8bde9daf77a3ab9665f866a
gohsonghan98 <gohsonghan98@gmail.com>
2020-10-08 15:41:31 +0800

    Bug Fixes

--- src/main/java/seedu/duke/Command.java
+++ src/main/java/seedu/duke/Command.java
@@ -3,7 +3,7 @@ package seedu.duke;
 public abstract class Command {
     protected String command;
 
-    public abstract void execute(FoodList foodlist,ExericseList exericseList, Storage storage);
+    public abstract void execute(FoodList foodlist,ExerciseList exerciseList, Storage storage);
 
 }
 
