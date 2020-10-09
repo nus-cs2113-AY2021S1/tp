1a2b2fa3ab55ff3080a18bd8c7ccb6628209f0d9
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-10-09 13:42:42 +0800

    made another implementation of init() that is easier to understand

--- src/main/java/seedu/duke/database/DataBase.java
+++ src/main/java/seedu/duke/database/DataBase.java
@@ -13,6 +13,8 @@ public class DataBase{
     private static final String STOP_SYMBOL = "&%STOP";
     private static final String UP_SYMBOL = "&%UP";
 
+    private static final String DATA_FILE_SEPERATOR = "\\|";
+
     private final ArrayList<Canteen> canteenList;
 
     public DataBase(){
@@ -26,17 +28,9 @@ public class DataBase{
         File dataFile = new File(System.getProperty("user.dir") + File.separator + "data.txt");
         Scanner fileReader = new Scanner(dataFile);
         String fileLine;
-        String[] fileData;
         boolean start = false;
-        boolean hasPreviousCanteen = false;
-        boolean hasPreviousStore = false;
-        DataBaseReadState readState = DataBaseReadState.CANTEEN;
-        Canteen currentCanteen = new Canteen("dummy");
-        Store currentStore = new Store("dummy");
-        Food currentFood;
         while (fileReader.hasNext()){
             fileLine = fileReader.nextLine();
-            fileData = fileLine.split("\\|");
             // ------ Check if the data base has started scanning ------
             if (fileLine.equals(START_SYMBOL)){
                 start = true;
@@ -48,38 +42,50 @@ public class DataBase{
             if (fileLine.equals(STOP_SYMBOL)){
                 break;
             }
-
-            // ------ Adjust the state of the data base and end the reading at the lower level -------
-            if (fileLine.equals(UP_SYMBOL)){
-                if (readState == DataBaseReadState.FOOD){
-                    currentCanteen.addStore(currentStore);
-                }
-                readState = DataBaseReadState.raiseState(readState);
-                continue;
-            }
-            // ----- Start reading the file here ------
-            if (readState == DataBaseReadState.CANTEEN){
-                if (hasPreviousCanteen){
-                    canteenList.add(currentCanteen);
-                }
-                currentCanteen = new Canteen(fileData[0]);
-                readState = DataBaseReadState.lowerState(readState);
-                hasPreviousCanteen = true;
-                hasPreviousStore = false;
-            }
-            else if (readState == DataBaseReadState.STORE){
-                if (hasPreviousStore){
-                    currentCanteen.addStore(currentStore);
-                }
-                currentStore = new Store(fileData[0]);
-                hasPreviousStore = true;
-            }
-            else{
-                currentFood = new Food(fileData[0], Integer.parseInt(fileData[1]),Integer.parseInt(fileData[2]),
-                        Integer.parseInt(fileData[3]),Integer.parseInt(fileData[4]));
-                currentStore.addFood(currentFood);
-            }
+            canteenList.add(fillCanteen(fileLine, fileReader));
         }
+    }
+
+    /***
+     * This function is called right after the canteen name is provided
+     * The very next line that the file reads is the store name
+     * It will turn call fillStore with that name inserted, when the function fillStore
+     * finishes executing, fileRead.nextLine() can either provide a new store name or UP_SYMBOL
+     * if the UP_SYMBOL is provided, the function ends and the final Canteen object is returned
+     * @param name
+     * @param fileSegment
+     * @return
+     */
+    private Canteen fillCanteen(String name, Scanner fileSegment){
+        Canteen canteen = new Canteen(name);
+        String fileLine = fileSegment.nextLine();
+        while (!(fileLine.equals(UP_SYMBOL))){
+            canteen.addStore(fillStore(name,fileSegment));
+            fileLine = fileSegment.nextLine();
+        }
+        return canteen;
+    }
+
+    /***
+     * This function is called right after the store name is provided
+     * The very next line in the file should be the first food to be added
+     * The function stops when it hits the line of the file that says UP_SYMBOL
+     * @param name name of the store
+     * @param fileSegment the Scanner object used for the init() function
+     * @return the completed store with all the food loaded
+     */
+    private Store fillStore(String name, Scanner fileSegment){
+        Store store = new Store(name);
+        Food food;
+        String fileLine = fileSegment.nextLine();
+        String[] fileData = fileLine.split(DATA_FILE_SEPERATOR);
+        while (!(fileLine.equals(UP_SYMBOL))){
+            food = new Food(fileData[0], Integer.parseInt(fileData[1]),Integer.parseInt(fileData[2])
+            ,Integer.parseInt(fileData[3]),Integer.parseInt(fileData[4]));
+            fileLine = fileSegment.nextLine();
+            fileData = fileLine.split(DATA_FILE_SEPERATOR);
+        }
+        return store;
     }
 
     /***
