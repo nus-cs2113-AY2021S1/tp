a855db7956cd8ca5442c76ff7846239ffd576471
rashien3 <hermanthong@gmail.com>
2020-10-09 16:42:01 +0800

    fix comment indentation

--- src/main/java/seedu/duke/command/subjectcommand/ReturnSubjectCommand.java
+++ src/main/java/seedu/duke/command/subjectcommand/ReturnSubjectCommand.java
@@ -57,7 +57,7 @@ public class ReturnSubjectCommand extends SubjectCommand {
                     c.execute(subject);
                 }
                 isSubjectExit = c.isExit();
-                // TODO: implement the storage methods for Topic
+//                 TODO: implement the storage methods for Topic
 //                topics.saveSubject(topicStorage.getFileName());
 //            } catch (IOException e) {
 //                Ui.printWritingError();
