1b8fed64b41ef53947d605709c7316df3ca5485b
n3wsoldier <n3wsoldier@gmail.com>
2020-10-09 14:29:48 +0800

    Update runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -6,10 +6,9 @@ cd "${0%/*}"
 cd ..
 ./gradlew clean shadowJar
 
+java  -jar $(find build/libs/ -mindepth 1 -print -quit) < text-ui-test/input.txt > text-ui-test/ACTUAL.TXT
+
 cd text-ui-test
-
-java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
-
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
 diff EXPECTED-UNIX.TXT ACTUAL.TXT
