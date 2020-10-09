cb3783ba11cd6a61e2c18fe9963f2d55fcf9b20a
TanJunHong <e0634975@u.nus.edu>
2020-10-09 18:13:39 +0800

    Change syntax to adhere to coding standard

--- src/test/java/seedu/rex/ui/UiTest.java
+++ src/test/java/seedu/rex/ui/UiTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UiTest {
 
