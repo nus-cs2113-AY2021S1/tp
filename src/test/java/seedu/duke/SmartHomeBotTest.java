22823b06ab1602de2588b5e77d682f6185799624
Ang Cheng Jun <e0415830@u.nus..edu>
2020-10-09 15:09:24 +0800

    Added RemoveLocation Test in the test file

--- src/test/java/seedu/duke/SmartHomeBotTest.java
+++ src/test/java/seedu/duke/SmartHomeBotTest.java
@@ -4,12 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.commands.Command;
+import seedu.duke.commands.RemoveCommand;
+import seedu.duke.common.Messages;
 import seedu.duke.data.ApplianceList;
 import seedu.duke.data.HomeLocations;
 import seedu.duke.data.type.Lights;
+import seedu.duke.exceptions.EmptyParameterException;
 import seedu.duke.exceptions.InvalidAdditionOfAppliance;
 import seedu.duke.exceptions.InvalidAddtionOfLocation;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 
 class SmartHomeBotTest {
@@ -36,4 +42,27 @@ class SmartHomeBotTest {
 
     }
 
+    @Test
+    public void removeLocationTest() throws InvalidAddtionOfLocation, EmptyParameterException {
+        //Create Sample Locations and empty appliance list
+        HomeLocations homeLocations = new HomeLocations();
+        homeLocations.addLocation("BedRoom1");
+        homeLocations.addLocation("BedRoom3");
+        // Prepare to read output of command
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        ApplianceList applianceList = new ApplianceList();
+        // Remove Invalid location
+        Command removeLocation = new RemoveCommand("Bedroom2");
+        removeLocation.setData(applianceList, homeLocations);
+        removeLocation.execute();
+
+        //Process ui output
+        String outputString = outContent.toString().replace(System.getProperty("line.separator"), "");
+
+        //compare outputs
+        assertEquals(Messages.MESSAGE_LOCATION_NOT_EXIST + " Nothing will be deleted.",outputString);
+
+    }
 }
