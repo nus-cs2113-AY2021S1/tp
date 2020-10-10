5569d960e4f7f786048147263e2dd6a00dfe68b4
Wei Chin <eyoweichin@hotmail.com>
2020-10-10 11:09:11 +0800

    Update Browse to retain original list after sorting
    Update Anime class to support Character class and Anime ID

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -11,7 +11,6 @@ public class Duke {
     private static Ui ui;
     private static AnimeData animeData;
 
-
     /**
      * Main entry-point for the java.duke.Duke application.
      */
@@ -144,10 +143,12 @@ public class Duke {
         String[] releaseDate = {"2020", "12", "30"};
         String[] genre = {"Science", "Action", "Dance"};
         String animeName = "Adventures of Adventurers";
+        int animeID = 0;
         int rating = 65;
         int avgEpisodeLength = 30;
         int totalEpisodes = 24;
-        Anime testAnime = new Anime(animeName, releaseDate, rating, genre, avgEpisodeLength, totalEpisodes);
+        Anime testAnime = new Anime(animeName, releaseDate,
+                rating, genre, avgEpisodeLength, totalEpisodes);
         System.out.println("Release Date in String: " + testAnime.getReleaseDateInString());
         System.out.println("===End of Sample Anime Class===");
     }
@@ -189,13 +190,14 @@ public class Duke {
     public static void browseAnime(String description) {
         Scanner input = new Scanner(System.in);
         //Browse Feature (Bare and un-refactored)
-        //Ad-Hoc User input for testing
-        //TODO: Make it so that the sorting is not persistent
-        System.out.println("1 - Just Browse / 2 - List from (A-Z) / 3 - List from (Z-A) / 4 - Sort "
+        System.out.println("Browse Options:"
+                + " 1 - Just Browse / 2 - List from (A-Z) / 3 - List from (Z-A) / 4 - Sort "
                 + "Rating Ascending / 5 - Sort Rating Descending");
-        String userInput = input.nextLine();
+
         ArrayList<Anime> usableList = animeData.animeDataList;
-        switch (userInput.trim()) {
+        switch (description.trim()) {
+        case "1":
+            break;
         case "2":
             usableList.sort(Comparator.comparing(Anime::getAnimeName));
             break;
@@ -210,10 +212,12 @@ public class Duke {
             break;
         default:
             System.out.println("Invalid Option!");
+            return;
         }
 
         int browseIndex = 0;
         boolean isBrowsing = true;
+        //Looping Mechanism
         System.out.println("Happy Browsing!");
         for (Anime anime : usableList) {
             if (!isBrowsing) {
@@ -221,23 +225,27 @@ public class Duke {
             }
             browseIndex++;
             System.out.println("\t" + browseIndex + ". " + anime.getAnimeName());
+
+            //Print 20 first then wait for user input
             if (browseIndex % 20 == 0) {
                 while (true) {
-                    System.out.println("Enter 'c': To show more");
-                    System.out.println("Enter 'q': To stop");
+                    System.out.println("Enter ID : More info on that series");
+                    System.out.println("Enter 'c': Continue browsing");
+                    System.out.println("Enter 'q': Stop browsing");
                     String browseOption = input.nextLine();
 
                     if (browseOption.matches("-?\\d+(\\.\\d+)?")) {
                         int indexObtained = Integer.parseInt(browseOption);
-                        System.out.println("Int detected: " + indexObtained);
+                        System.out.println("Requested List ID: " + indexObtained);
                         if (indexObtained > browseIndex || indexObtained < 1) {
                             System.out.println("Out of Range query!");
                         } else {
                             Anime browseAnime = usableList.get(indexObtained - 1);
+                            System.out.println("ID          :   " + browseAnime.getAnimeID());
                             System.out.println("Title       :   " + browseAnime.getAnimeName());
                             System.out.println("Rating      :   " + browseAnime.getRating());
                             System.out.println("Episodes    :   " + browseAnime.getTotalEpisodes());
-                            System.out.println("Release Date:   " + browseAnime.getReleaseDate());
+                            System.out.println("Release Date:   " + browseAnime.getReleaseDateInString());
                         }
                     }
                     if (browseOption.trim().equals("c")) {
@@ -251,6 +259,10 @@ public class Duke {
             }
         }
         System.out.println("Total results found: " + browseIndex);
+        //Put the list back in its original form
+        if (!description.trim().equals("1")) {
+            usableList.sort(Comparator.comparing(Anime::getAnimeID));
+        }
     }
 
     /**
@@ -290,18 +302,22 @@ public class Duke {
         String[] releaseDate = {"2020", "12", "30"};
         String[] genre = {"Science", "Action", "Dance"};
         String animeName = "Adventures of Adventurers";
+        int animeID = 1;
         int rating = 65;
         int avgEpisodeLength = 30;
         int totalEpisodes = 24;
-        Anime testAnime = new Anime(animeName, releaseDate, rating, genre, avgEpisodeLength, totalEpisodes);
+        Anime testAnime = new Anime(animeName, releaseDate, rating, genre,
+                avgEpisodeLength, totalEpisodes);
 
         String[] releaseDate2 = {"1997", "4", "15"};
         String[] genre2 = {"Adventure", "Action", "Dance"};
         String animeName2 = "Actions in Action";
+        animeID = 2;
         int rating2 = 80;
         int avgEpisodeLength2 = 20;
         int totalEpisodes2 = 13;
-        Anime testAnime2 = new Anime(animeName2, releaseDate2, rating2, genre2, avgEpisodeLength2, totalEpisodes2);
+        Anime testAnime2 = new Anime(animeName2, releaseDate2, rating2, genre2,
+                avgEpisodeLength2, totalEpisodes2);
 
         animeStorageList.add(testAnime);
         animeStorageList.add(testAnime2);
