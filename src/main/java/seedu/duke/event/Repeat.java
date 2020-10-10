e7539fce3b1f39b4b471ef304e1f6d594090ac28
Reinbowl <marcusngyong@gmail.com>
2020-10-10 21:02:12 +0800

    Add Repeat class in event

---
+++ src/main/java/seedu/duke/event/Repeat.java
@@ -0,0 +1,41 @@
+package seedu.duke.event;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Repeat {
+    public static final String DAILY = "DAILY";
+    public static final String WEEKLY = "WEEKLY";
+    public static final String MONTHLY = "MONTHLY";
+
+    private final ArrayList<DateStatusPair> repeatList = new ArrayList<>();
+    private final String repeatType;
+
+    public Repeat(LocalDate startDate, LocalTime startTime, String repeatType, int count) {
+        for (int i = 0; i < count; i++) {
+            LocalDate repeatDate;
+            if (repeatType.equals(MONTHLY)) {
+                repeatDate = startDate.plusMonths(1);
+            } else if (repeatType.equals(WEEKLY)){
+                repeatDate = startDate.plusDays(7);
+            } else {
+                repeatDate = startDate.plusDays(1);
+            }
+            DateStatusPair pair = new DateStatusPair(repeatDate, startTime);
+        }
+        this.repeatType = repeatType;
+    }
+
+    public ArrayList<DateStatusPair> getRepeatList() {
+        return repeatList;
+    }
+
+    public String getRepeatType() {
+        return repeatType;
+    }
+
+    public int getRepeatCount() {
+        return repeatList.size();
+    }
+}
