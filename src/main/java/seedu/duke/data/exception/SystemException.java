1a2ae7654389c5cc4f2de930cffe55b73eba028e
brandonywl <brandonywl@gmail.com>
2020-10-11 00:41:49 +0800

    Update src/main/java/seedu/duke/data/exception/SystemException.java
    Co-authored-by: R Ramana <54029207+R-Ramana@users.noreply.github.com>

--- src/main/java/seedu/duke/data/exception/SystemException.java
+++ src/main/java/seedu/duke/data/exception/SystemException.java
@@ -20,7 +20,7 @@ public class SystemException extends Exception {
         EXCEPTION_WRONG_RECURRING_TYPE("There are only the following recurring types: "
                 + "daily, weekly, monthly or yearly"),
         EXCEPTION_INDEX_OUT_OF_RANGE("The index you specified is out of range. Please specify "
-                + "the index " + "that is indicated when you print the event list"),
+                + "the index that is indicated when you print the event list"),
         EXCEPTION_MISSING_TAG_PREFIX("Missing tag prefix!"),
         EXCEPTION_MISSING_TAG("Missing tag name!"),
         EXCEPTION_MISSING_INDEX_PREFIX("Missing index prefix!"),
