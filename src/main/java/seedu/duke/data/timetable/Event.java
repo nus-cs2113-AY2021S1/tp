09fa37a99fdd5f4cb5b3e361b67bb713c973ec97
Brandon <brandonywl@gmail.com>
2020-10-11 00:34:12 +0800

    Ensure code standards

--- src/main/java/seedu/duke/data/timetable/Event.java
+++ src/main/java/seedu/duke/data/timetable/Event.java
@@ -97,7 +97,7 @@ public class Event {
         String remindString = "Reminder: " + isToRemind;
         String repeatingString = "Repeating: " + isRecurring;
         String lineSeparator = InterfaceManager.LS;
-        return titleString + lineSeparator + dateString + lineSeparator + remindString 
+        return titleString + lineSeparator + dateString + lineSeparator + remindString
                 + lineSeparator + repeatingString;
     }
 }
