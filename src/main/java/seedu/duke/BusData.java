358e92731602dabc1c7854ccd7cccdd678b82780
Wamzzz <60306071+wamikamalik@users.noreply.github.com>
2020-10-10 13:26:03 +0400

    Update src/main/java/seedu/duke/BusData.java
    Co-authored-by: Yuxin <60455558+Lezn0@users.noreply.github.com>

--- src/main/java/seedu/duke/BusData.java
+++ src/main/java/seedu/duke/BusData.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class BusData {
 
-    private static ArrayList<Bus> buses;
+    private static ArrayList<Bus> buses = new ArrayList<>();
 
     public static void createBusList(ArrayList<Bus> busList) {
         buses.addAll(busList);
