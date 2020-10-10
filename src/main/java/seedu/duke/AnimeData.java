e608afcd1dae0fcd8834c9b565a77bd4546aead2
n3wsoldier <n3wsoldier@gmail.com>
2020-10-11 00:02:27 +0800

    Add Bookmark.java

--- src/main/java/seedu/duke/AnimeData.java
+++ src/main/java/seedu/duke/AnimeData.java
@@ -49,4 +49,30 @@ public class AnimeData {
         System.out.println("Rating: " + anime.getRating());
         System.out.println("Genre: " + Arrays.toString(anime.getGenre()));
     }
+
+    public ArrayList<Anime> findName(String description) {
+        ArrayList<Anime> findList = new ArrayList<>();
+        System.out.println("Looking for \"" + description + "\"");
+        for (Anime anime : animeDataList) {
+            if (anime.getAnimeName().contains(description)) {
+                findList.add(anime);
+            }
+        }
+        return findList;
+    }
+
+    public Anime getAnimeByID(Integer animeIndex) throws IndexOutOfBoundsException, NullPointerException {
+        try {
+            for (Anime anime : animeDataList) {
+                if (anime.getAnimeID() == animeIndex) {
+                    return anime;
+                }
+            }
+            return null;
+        } catch (IndexOutOfBoundsException e) {
+            throw e;
+        } catch (NullPointerException e) {
+            throw e;
+        }
+    }
 }
