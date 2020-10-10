04c33b4bbf7af3fd0f5afbecadd840761ee0df84
YF <yf144114@gmail.com>
2020-10-10 17:25:38 +0800

    fixed gradler builder with Junit-test

--- src/test/java/seedu/duke/data/SmartHomeBotTest.java
+++ src/test/java/seedu/duke/data/SmartHomeBotTest.java
@@ -6,10 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import seedu.duke.commands.Command;
 import seedu.duke.commands.RemoveCommand;
-import seedu.duke.common.Messages;
 import seedu.duke.data.ApplianceList;
 import seedu.duke.data.HomeLocations;
-import seedu.duke.data.Lights;
+import seedu.duke.common.Messages;
 import seedu.duke.exceptions.EmptyParameterException;
 import seedu.duke.exceptions.InvalidAdditionOfAppliance;
 import seedu.duke.exceptions.InvalidAddtionOfLocation;
@@ -19,6 +18,22 @@ import java.io.PrintStream;
 
 
 class SmartHomeBotTest {
+
+    @Test
+    void getNameTest() {
+        assertEquals("Xiao Mi Light", new Lights("Xiao Mi Light", "BedRoom 1", "45").getName());
+    }
+
+    @Test
+    void getLocationTest() {
+        assertEquals("BedRoom1", new Lights("Xiao Mi Light", "BedRoom1", "45").getLocation());
+    }
+
+    @Test
+    void getStatusTest() {
+        //New appliance added should be always remain Off until User switch it On
+        assertEquals("Off", new Lights("Xiao Mi Light", "BedRoom 1", "45").getStatus());
+    }
 
     @Test
     public void addLocationTest() throws InvalidAddtionOfLocation {
@@ -39,27 +54,12 @@ class SmartHomeBotTest {
         applianceList.addAppliance(l1);
         applianceList.addAppliance(l2);
         assertThrows(InvalidAdditionOfAppliance.class, () -> applianceList.addAppliance(l1));
+
     }
 
     @Test
     void getTypeTest() {
         assertEquals("AirConditioner", new AirConditioner("aircon1", "br1", "25").getType());
-    }
-
-    @Test
-    void getNameTest() {
-        assertEquals("Xiao Mi Light", new Lights("Xiao Mi Light", "BedRoom 1", "45").getName());
-    }
-
-    @Test
-    void getLocationTest() {
-        assertEquals("BedRoom1", new Lights("Xiao Mi Light", "BedRoom1", "45").getLocation());
-    }
-
-    @Test
-    void getStatusTest() {
-        //New appliance added should be always remain Off until User switch it On
-        assertEquals("Off", new Lights("Xiao Mi Light", "BedRoom 1", "45").getStatus());
     }
 
     @Test
