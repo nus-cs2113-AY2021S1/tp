ea5c210ed9a81753ef1849508d58e1a56ef4f93a
farice9 <limyb50@gmail.com>
2020-10-11 12:57:43 +0800

    Add mode names and exception

--- src/main/java/InvalidModeException.java
+++ src/main/java/InvalidModeException.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class InvalidModeException {
+public class InvalidModeException extends Exception {
 }
--- src/main/java/ModeNames.java
+++ src/main/java/ModeNames.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DirectoryNames {
+public class ModeNames {
+    public static final String BOOKMARK_NAME = "bookmark";
+    public static final String TIMETABLE_NAME = "timetable";
+    public static final String ACADEMIC_NAME = "academic";
+    public static final String FLASHCARD_NAME = "flashcard";
+    public static final String MENU_NAME = "menu";
+
+    public static String getCurrentModeName() {
+        Mode currentMode = StudyIt.getCurrentMode();
+
+        switch (currentMode) {
+        case BOOKMARK:
+            return BOOKMARK_NAME;
+        case TIMETABLE:
+            return TIMETABLE_NAME;
+        case ACADEMIC:
+            return ACADEMIC_NAME;
+        case FLASHCARD:
+            return FLASHCARD_NAME;
+        default:
+            return MENU_NAME;
+        }
+    }
 }
