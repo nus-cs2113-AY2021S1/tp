73d00d0c95af73c1a33b6730b6beba56d60fbb16
Joseph <e0406453@u.nus.edu>
2020-10-12 04:42:00 +0800

    Fix the library call in MenuParser, by calling each individual class.

--- src/main/java/seedu/eduke8/parser/MenuParser.java
+++ src/main/java/seedu/eduke8/parser/MenuParser.java
@@ -1,9 +1,13 @@
 package seedu.eduke8.parser;
 
-import seedu.eduke8.command.*;
+import seedu.eduke8.command.Command;
+import seedu.eduke8.command.AboutCommand;
+import seedu.eduke8.command.ExitCommand;
+import seedu.eduke8.command.HelpCommand;
+import seedu.eduke8.command.QuizCommand;
+import seedu.eduke8.command.TextbookCommand;
+import seedu.eduke8.command.TopicsCommand;
 import seedu.eduke8.common.DisplayableList;
-import seedu.eduke8.quiz.SingleTopicQuiz;
-import seedu.eduke8.topic.Topic;
 import seedu.eduke8.topic.TopicList;
 import seedu.eduke8.ui.Ui;
 
