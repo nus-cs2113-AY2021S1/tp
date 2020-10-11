3f2f40b5c85ad45eed4cf17146c1f459f3c4ed88
chongjx <chongjx60@gmail.com>
2020-10-11 23:10:53 +0800

    Categorsied prefix by usage.

--- src/main/java/seedu/duke/util/PrefixSyntax.java
+++ src/main/java/seedu/duke/util/PrefixSyntax.java
@@ -5,23 +5,30 @@ package seedu.duke.util;
  */
 public class PrefixSyntax {
 
-    /* Prefix definitions */
+    /** Prefix definitions. */
     public static final String PREFIX_DELIMITER = "/";
 
+    /** Shared prefix. */
     public static final String PREFIX_TITLE = "t";
-    public static final String PREFIX_TAG = "tag";
     public static final String PREFIX_INDEX = "i";
+
+    /** Note related prefix. */
+    public static final String PREFIX_TAG = "tag";
     public static final String PREFIX_PIN = "pin";
     public static final String PREFIX_CONTENT = "c";
     public static final String PREFIX_LINE = "ln";
     public static final String PREFIX_SORT = "sort";
+
+    /** Event related prefix. */
     public static final String PREFIX_TIMING = "timing";
     public static final String PREFIX_RECURRING = "rec";
     public static final String PREFIX_REMIND = "rem";
     public static final String PREFIX_STOP_RECURRING = "stop";
     public static final String PREFIX_DATETIME = "d";
+
     public static final String STRING_SPLIT_DELIMITER = " ";
 
+    /** Note content related prefix. */
     public static final String STRING_NEW_LINE = "\n";
     public static final String PREFIX_END = "/end";
     public static final String PREFIX_DELETE_LINE = "/del";
