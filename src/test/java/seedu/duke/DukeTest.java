c63b438990b0f34adab81826e16cd291e405c112
WangZixin67 <e0376957@u.nus.edu>
2020-10-11 23:23:26 +0800

    Wang Zixin Finance version1.0

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -21,18 +21,18 @@ class DukeTest {
 
     @Test
     public void testFinance() {
-        String expected1 = "Got it! I've added this to the list.\n" +
-                "\t1.write $12.5\n" +
-                "There is 1 log in the list now.\n";
+        String expected1 = "Got it! I've added this to the list.\n"
+                + "\t1.write $12.5\n"
+                + "There is 1 log in the list now.\n";
         String actual1 = FinanceList.addLog(new FinanceLog("write",12.5));
         assertEquals(expected1,actual1);
-        String expected2 = "Here is the list:\n" +
-                "\t1.write $12.5\n" +
-                "Total budget: $12.50\n";
+        String expected2 = "Here is the list:\n"
+                + "\t1.write $12.5\n"
+                + "Total budget: $12.50\n";
         String actual2 = FinanceList.summary();
         assertEquals(expected2,actual2);
-        String expected3 = "Got it! I've removed this from list.\n" +
-                "There is 0 log in the list now.\n";
+        String expected3 = "Got it! I've removed this from list.\n" 
+                + "There is 0 log in the list now.\n";
         String actual3 = FinanceList.dellog(1);
         assertEquals(expected3,actual3);
     }
