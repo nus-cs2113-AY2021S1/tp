447a35d20010aee01b6c6a83132f7c5688028ad1
Artemis-Hunt <stevetheguy@naver.com>
2020-10-11 16:37:56 +0800

    Added comments and cleaned up code for parsers and various other classes

--- src/main/java/seedu/financeit/ui/UiManager.java
+++ src/main/java/seedu/financeit/ui/UiManager.java
@@ -13,7 +13,8 @@ public class UiManager {
     private static InputParser inputParser = new InputParser();
 
     public static String getLineWithSymbol(int width, String symbol) {
-        return new String(new char[width]).replace("\0", symbol);
+//        return new String(new char[width]).replace("\0", symbol);
+        return symbol.repeat(width);
     }
 
     public static void printLineWithSymbol(int width, String symbol) {
