fd3293dfdddca471e8d845d2dca1ba0745ce4d64
KennethEer <59533849+KennethEer@users.noreply.github.com>
2020-10-12 12:50:08 +0800

    Edit junit test

--- src/test/java/seedu/modtracker/ModuleListTest.java
+++ src/test/java/seedu/modtracker/ModuleListTest.java
@@ -6,20 +6,29 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ModuleListTest {
     private static final String TEST_DATA_FILEPATH = "test/data/modlist.txt";
     Storage storage = new Storage(TEST_DATA_FILEPATH);
 
     @Test
-    public void addModule_emptyModuleList_newModuleAdded() {
+    public void addExp_emptyModuleList_newModuleWithExpTimeAdded() {
         ArrayList<Module> testList = new ArrayList<>();
-        Module testMod = new Module("CS5000");
+        Module testMod = new Module("CS3030", "4");
         testList.add(testMod);
 
         ModuleList modules = new ModuleList();
-        modules.addMod("addMod CS5000", true, storage);
+        modules.addExp("addExp CS3030 4", true, storage);
         assertArrayEquals(testList.toArray(), modules.modList.toArray());
+    }
+
+    @Test
+    public void addModule_emptyModuleList_newModuleAdded() {
+
+        ModuleList modules = new ModuleList();
+        modules.addMod("addMod CS5000", true, storage);
+        assertTrue(modules.checkIfModuleExist("CS5000"));
     }
 
     @Test
@@ -33,15 +42,5 @@ class ModuleListTest {
         assertNotEquals(testList.toArray(), modules.modList.toArray());
     }
 
-    @Test
-    public void addExp_emptyModuleList_newModuleWithExpTimeAdded() {
-        ArrayList<Module> testList = new ArrayList<>();
-        Module testMod = new Module("CS3030", "4");
-        testList.add(testMod);
-
-        ModuleList modules = new ModuleList();
-        modules.addExp("addExp CS3030 4", true, storage);
-        assertArrayEquals(testList.toArray(), modules.modList.toArray());
-    }
 
 }
