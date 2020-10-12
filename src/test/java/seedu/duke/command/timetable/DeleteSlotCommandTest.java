de4d93177bc7c7224eced912b997235ce49e2dbd
Francisco Chensan <chensan@u.nus.edu>
2020-10-12 18:20:26 +0800

    Add DeleteSlotCommand execute JUnit test

--- src/test/java/seedu/duke/command/timetable/DeleteSlotCommandTest.java
+++ src/test/java/seedu/duke/command/timetable/DeleteSlotCommandTest.java
@@ -24,4 +24,28 @@ class DeleteSlotCommandTest {
             assertEquals(e.getError(), DukeExceptionType.INVALID_SLOT_NUMBER);
         }
     }
+
+    @Test
+    void execute() {
+        String startTime = "10:00";
+        String endTime = "12:00";
+        String title = "CS1231 Lecture";
+
+        SlotList slots = new SlotList();
+        slots.addSlot(new Slot(LocalTime.parse(startTime),
+                LocalTime.parse(endTime),
+                Slot.FRI,
+                title
+        ));
+
+        String input = DeleteSlotCommand.DEL_KW + " 1";
+        try {
+            DeleteSlotCommand command = new DeleteSlotCommand(input);
+            command.execute(slots, slots, new Ui(), new Storage("test.txt"), new Storage("test.txt"));
+        } catch (DukeException e) {
+            fail(e.getMessage());
+        }
+
+        assertEquals(new SlotList().getData(), slots.getData());
+    }
 }
