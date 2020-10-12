d8c815b7fb25d5520c839b13f551c74ba0e89386
wamikamalik <wamikamalik@hotmail.com>
2020-10-12 08:55:46 +0400

    Update bus and bus data to generate route

--- src/main/java/seedu/duke/Bus.java
+++ src/main/java/seedu/duke/Bus.java
@@ -13,6 +13,31 @@ public class Bus {
         route.addAll(Arrays.asList(stops));
     }
 
+    public Bus(String busNo, ArrayList<String> route) {
+        busNumber = busNo;
+        this.route.addAll(route);
+    }
 
+    ArrayList<String> getPossibleRoute(String startingLoc, String destination) {
 
+        ArrayList<String> allStopsFromStart = new ArrayList<>();
+        ArrayList<String> finalRoute = new ArrayList<>();
+        if (route.contains(startingLoc)) {
+            int startingIndex = route.indexOf(startingLoc) + 1;
+            int size = route.size();
+            allStopsFromStart.addAll(route.subList(startingIndex, size));
+            if (allStopsFromStart.contains(destination)) {
+                int endIndex = allStopsFromStart.indexOf(destination);
+                finalRoute.addAll(allStopsFromStart.subList(0, endIndex));
+            }
+        }
+        return finalRoute;
+
+    }
+
+    @Override
+    public String toString() {
+        String printableRoute = String.join("-> ", route);
+        return busNumber + ":\n\t" + printableRoute;
+    }
 }
