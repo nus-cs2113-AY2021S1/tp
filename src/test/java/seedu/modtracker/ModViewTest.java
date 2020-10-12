0dd8f06b3d3cc322cef1999d6d036660756af1f0
limgl1998 <limgl1998@gmail.com>
2020-10-12 23:57:18 +0800

    Added more tests
    Tested for no inputs and auto resizing of column module code

--- src/test/java/seedu/modtracker/ModViewTest.java
+++ src/test/java/seedu/modtracker/ModViewTest.java
@@ -6,18 +6,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ModViewTest {
-    //naming : methodUnderTest_inputGiven_expectedOutput
 
     @Test
     void printAllModuleInformation_emptyList_emptyModuleList() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
+        outContent.reset();
         ModView view = new ModView();
         ModuleList emptyList = new ModuleList();
+        emptyList.getData().clear();
         view.printAllModuleInformation(emptyList, 1);
         assertEquals(ModView.EMPTY_MODULE_LIST + System.lineSeparator(),
                 outContent.toString());
@@ -28,8 +29,10 @@ class ModViewTest {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
+        outContent.reset();
         ModView view = new ModView();
         ModuleList emptyList = new ModuleList();
+        emptyList.getData().clear();
 
         view.printAllModuleInformation(emptyList, 0);
         assertEquals(ModView.INVALID_WEEK_NUMBER + System.lineSeparator(),
@@ -46,5 +49,93 @@ class ModViewTest {
                 outContent.toString());
 
     }
-    
+
+    @Test
+    void printAllModuleInformation_6CharLongModuleCode_tableAdjustedTo6Char() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        outContent.reset();
+        ModView view = new ModView();
+        ModuleList filledList = new ModuleList();
+        ArrayList<Module> modList = filledList.getData();
+        modList.add(new Module("CS1231"));
+
+        String expected = ModView.FIRST_PART_OF_BORDER + ModView.SECOND_PART_OF_BORDER
+                + ModView.FIRST_PART_OF_HEADER + ModView.SECOND_PART_OF_HEADER
+                + ModView.FIRST_PART_OF_BORDER + ModView.SECOND_PART_OF_BORDER
+                + "|  01  | CS1231 | No Input | No Input |\n"
+                + ModView.FIRST_PART_OF_BORDER + ModView.SECOND_PART_OF_BORDER;
+        view.printAllModuleInformation(filledList, 1);
+        assertEquals(expected + System.lineSeparator(),
+                outContent.toString());
+    }
+
+    @Test
+    void printAllModuleInformation_8CharLongModuleCode_tableAdjustedTo8Char() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        outContent.reset();
+        ModView view = new ModView();
+        ModuleList filledList = new ModuleList();
+        ArrayList<Module> modList = filledList.getData();
+        modList.clear();
+        modList.add(new Module("GER1000H"));
+
+        String expected = ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER
+                + ModView.FIRST_PART_OF_HEADER + "  " + ModView.SECOND_PART_OF_HEADER
+                + ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER
+                + "|  01  | GER1000H | No Input | No Input |\n"
+                + ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER;
+        view.printAllModuleInformation(filledList, 1);
+        assertEquals(expected + System.lineSeparator(),
+                outContent.toString());
+    }
+
+    @Test
+    void printAllModuleInformation_hasExpectedWorkload_displaysExpectedWorkLoad() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        outContent.reset();
+        ModView view = new ModView();
+        ModuleList filledList = new ModuleList();
+        ArrayList<Module> modList = filledList.getData();
+        modList.clear();
+        modList.add(new Module("GER1000H", "8"));
+
+        String expected = ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER
+                + ModView.FIRST_PART_OF_HEADER + "  " + ModView.SECOND_PART_OF_HEADER
+                + ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER
+                + "|  01  | GER1000H |     8    | No Input |\n"
+                + ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER;
+        view.printAllModuleInformation(filledList, 1);
+        assertEquals(expected + System.lineSeparator(),
+                outContent.toString());
+    }
+
+    @Test
+    void printAllModuleInformation_hasActualWorkload_displaysActualWorkLoad() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        outContent.reset();
+        ModView view = new ModView();
+        ModuleList filledList = new ModuleList();
+        ArrayList<Module> modList = filledList.getData();
+        modList.clear();
+        Module mod = new Module("GER1000H");
+        mod.addActualTime("8", "1");
+        modList.add(mod);
+
+        String expected = ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER
+                + ModView.FIRST_PART_OF_HEADER + "  " + ModView.SECOND_PART_OF_HEADER
+                + ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER
+                + "|  01  | GER1000H | No Input |    8.0   |\n"
+                + ModView.FIRST_PART_OF_BORDER + "--" + ModView.SECOND_PART_OF_BORDER;
+        view.printAllModuleInformation(filledList, 1);
+        assertEquals(expected + System.lineSeparator(),
+                outContent.toString());
+    }
 }
