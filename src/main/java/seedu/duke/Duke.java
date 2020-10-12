d139f00efd3b606aafaf3817034f2e5b56867492
chocomango <weilingchan97@gmail.com>
2020-10-12 23:16:15 +0800

    Update main runner class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,5 +1,6 @@
 package seedu.duke;
 
+import seedu.duke.command.Command;
 import seedu.duke.parser.Parser;
 import seedu.duke.project.Project;
 import seedu.duke.ui.Ui;
@@ -12,14 +13,36 @@ public class Duke {
      */
 
     private static Parser parser = new Parser();
+    private static ArrayList<Project> projectList = new ArrayList<>(10);
 
     public static void main(String[] args) {
-        ArrayList<Project> projectList = new ArrayList<>(10);
-        Ui.showWelcomeScreen();
+        new Duke().run();
+    }
 
+    /**
+     * Runs the program.
+     */
+    private void run() {
+        init();
+        runInstructions();
+    }
+
+    /**
+     * Welcome the user and initialise the local storage.
+     */
+    private void init() {
+        Ui.showWelcomeScreen();
+    }
+
+    /**
+     * Get the user input and runs the instructions.
+     * Exits when ExitCommand returns "true"
+     */
+    private static void runInstructions() {
+        String input;
         while (true) {
-            String userInput = Ui.getUserCommand();
-            parser.parser(userInput, projectList);
+            input = Ui.getUserCommand();
+            parser.parser(input, projectList);
         }
     }
 }
