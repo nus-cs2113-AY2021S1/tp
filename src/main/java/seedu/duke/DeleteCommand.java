a88dd91ea048f1034a1b18b9667c471af34a4c9c
harris maung <harrismaung3@gmail.com>
2020-10-12 16:06:02 +0800

    Update of Delete Command

--- src/main/java/seedu/duke/DeleteCommand.java
+++ src/main/java/seedu/duke/DeleteCommand.java
@@ -12,15 +12,15 @@ public class DeleteCommand extends Command {
             String type = command.split(" ", 2)[0];
             if (type.equals("food")) {
                 int deletionIndex = Integer.parseInt(command.split(" ", 2)[1]);
+                System.out.println("The following has been deleted from the list of food consumed: " + foodlist.getFood(deletionIndex - 1).getFoodName());
                 foodlist.deleteFood(deletionIndex - 1);
             } else if (type.equals("exercise")) {
                 int deletionIndex = Integer.parseInt(command.split(" ", 2)[1]);
+                System.out.println("The following has been deleted from the list of food consumed: " + exerciseList.getExercise(deletionIndex - 1).getNameOfExercise());
                 exerciseList.deleteExercise(deletionIndex - 1);
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("Sorry that index does not exist in the list");
-        } catch (StringIndexOutOfBoundsException e) {
-            System.out.println("Please key in the delete function in the correct format");
         } catch (NumberFormatException e) {
             System.out.println("Sorry index deletion must be an number");
         }
