3538af2d206eb83d37fb018b0856f256f05ea09d
dmbclub <dmb437026158@gmail.com>
2020-10-12 16:22:44 +0800

    Handle the case when enter just food or exercise without any description.

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -3,6 +3,9 @@ package seedu.duke;
 public class Parser {
     public static Command parse(String userInput) {
         String[] fullCommand = userInput.split("\\s+");
+        if (fullCommand.length == 1) {
+            return new InvalidCommand(userInput);
+        }
         switch (fullCommand[0].toLowerCase()) {
         case "food":
             return new AddCommand(userInput);
