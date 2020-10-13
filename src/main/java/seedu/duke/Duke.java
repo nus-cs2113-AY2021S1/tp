5dcfde5053294acc732f192d81b801c0f74b37d9
slightlyharp <122560282@qq.com>
2020-10-13 15:53:52 +0800

    restore main

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,28 +1,21 @@
 package seedu.duke;
 
-
-import timetable.DateList;
-import timetable.TimeTableParser;
-import timetable.TimeTableStorage;
-
-import javax.swing.text.html.HTMLFrameHyperlinkEvent;
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        DateList events = new DateList();
-        TimeTableStorage storage = new TimeTableStorage("storage.txt", events);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
+
         Scanner in = new Scanner(System.in);
-        String command;
-        do {
-            command = in.nextLine();
-            TimeTableParser.commandParser(command, events,storage);
-        } while (!command.equals("exit"));
+        System.out.println("Hello " + in.nextLine());
     }
 }
