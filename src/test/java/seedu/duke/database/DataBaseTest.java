7b7cd2fb8ad8833819c4417e6ad5cb438c3ba2a7
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-10-13 21:12:28 +0800

    pass gradle checkstyle test

--- src/test/java/seedu/duke/database/DataBaseTest.java
+++ src/test/java/seedu/duke/database/DataBaseTest.java
@@ -3,10 +3,9 @@ package seedu.duke.database;
 import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class DataBaseTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         DataBase database = new DataBase();
         try {
             database.init();
@@ -30,23 +29,25 @@ class DataBaseTest {
             System.out.println("Input: Prawn  ## OutPut: " + database.searchFoodByName("Prawn"));
             System.out.println("Input: Mee ## OutPut: " + database.searchFoodByName("Mee"));
             System.out.println("Input: lobster ## OutPut: " + database.searchFoodByName("lobster"));
-
             System.out.println("Input: Prawn ## OutPut: " + database.searchFoodByName("koala bears"));
-        }
-        catch (NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             System.out.println("No such food found! " + e);
         }
 
         // ---- search food by store name test ----
         System.out.println("------- testing the search food by store and by name function --------");
         try {
-            System.out.println("Input: Fried , Halal Mini Wok ## OutPut: " + database.searchFoodByNameByStore("Fried", "Halal Mini Wok"));
-            System.out.println("Input: Chicken , Halal Mini Wok ## OutPut: " + database.searchFoodByNameByStore("Chicken", "Halal Mini Wok"));
-            System.out.println("Input: Chicken , Ayam Penyet ## OutPut: " + database.searchFoodByNameByStore("Chicken", "Ayam Penyet"));
-            System.out.println("Input: lobster , Michelin ## OutPut: " + database.searchFoodByNameByStore("lobster", "Michelin"));
-            System.out.println("Input: fish , Halal Mini Wok ## OutPut: " + database.searchFoodByNameByStore("fish", "Halal Mini Wok"));
-        }
-        catch (NoSuchElementException e){
+            System.out.println("Input: Fried , Halal Mini Wok ## OutPut: "
+                    + database.searchFoodByNameByStore("Fried", "Halal Mini Wok"));
+            System.out.println("Input: Chicken , Halal Mini Wok ## OutPut: "
+                    + database.searchFoodByNameByStore("Chicken", "Halal Mini Wok"));
+            System.out.println("Input: Chicken , Ayam Penyet ## OutPut: "
+                    + database.searchFoodByNameByStore("Chicken", "Ayam Penyet"));
+            System.out.println("Input: lobster , Michelin ## OutPut: "
+                    + database.searchFoodByNameByStore("lobster", "Michelin"));
+            System.out.println("Input: fish , Halal Mini Wok ## OutPut: "
+                    + database.searchFoodByNameByStore("fish", "Halal Mini Wok"));
+        } catch (NoSuchElementException e) {
             System.out.println("No such food found! " + e);
         }
 
@@ -61,20 +62,22 @@ class DataBaseTest {
             database.searchAllFoodByStore("Michelin").forEach(System.out::println);
             System.out.println("------- Input: Gordan Ramsey's restaurant -------- ");
             database.searchAllFoodByStore("Gordon Ramsey's restaurant").forEach(System.out::println);
-        }
-        catch (NoSuchElementException e) {
+        } catch (NoSuchElementException e) {
             System.out.println("There is no such store! " + e);
         }
 
         // ---- search food by Name by canteen ------
         try {
             System.out.println("------- testing the search food by canteen and by name function --------");
-            System.out.println("Input: Fried , Science ## OutPut: " + database.searchFoodByNameByCanteen("Fried", "Science"));
-            System.out.println("Input: Chicken , Science ## OutPut: " + database.searchFoodByNameByCanteen("Chicken", "Science"));
-            System.out.println("Input: lobster , Science ## OutPut: " + database.searchFoodByNameByCanteen("lobster", "Science"));
-            System.out.println("Input: lobster , Raffles Hotel Suite ## OutPut: " + database.searchFoodByNameByCanteen("lobster", "Raffles Hotel Suite"));
-        }
-        catch (NoSuchElementException e) {
+            System.out.println("Input: Fried , Science ## OutPut: "
+                    + database.searchFoodByNameByCanteen("Fried", "Science"));
+            System.out.println("Input: Chicken , Science ## OutPut: "
+                    + database.searchFoodByNameByCanteen("Chicken", "Science"));
+            System.out.println("Input: lobster , Science ## OutPut: "
+                    + database.searchFoodByNameByCanteen("lobster", "Science"));
+            System.out.println("Input: lobster , Raffles Hotel Suite ## OutPut: "
+                    + database.searchFoodByNameByCanteen("lobster", "Raffles Hotel Suite"));
+        } catch (NoSuchElementException e) {
             System.out.println("There is either no such canteen or no such food in that canteen!" + e);
         }
 
