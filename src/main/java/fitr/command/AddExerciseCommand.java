a5c81eab8642258ef20177095a393dffee895484
harris maung <harrismaung3@gmail.com>
2020-10-13 15:27:26 +0800

    UpdatesAndJunitTest

--- src/main/java/fitr/command/AddExerciseCommand.java
+++ src/main/java/fitr/command/AddExerciseCommand.java
@@ -7,6 +7,8 @@ import fitr.list.FoodList;
 import fitr.storage.Storage;
 import fitr.ui.Ui;
 
+import java.io.IOException;
+
 public class AddExerciseCommand extends Command {
     public AddExerciseCommand(String command) {
         this.command = command;
@@ -22,17 +24,21 @@ public class AddExerciseCommand extends Command {
             if (command.split(" ").length == 1) {
                 Calorie amountOfCaloriesBurnt = new Calorie(Integer.parseInt(command.split(" ")[0]));
                 exerciseList.addExercise(new Exercise(nameOfExercise, amountOfCaloriesBurnt));
+                storage.writeExerciseList(exerciseList);
                 Ui.showAdd("The following execise has been added: " + nameOfExercise);
             } else if (command.split(" ").length == 2) {
                 Calorie amountOfCaloriesBurnt = new Calorie(Integer.parseInt(command.split(" ")[0]));
                 int durationOfExercise = Integer.parseInt(command.split(" ", 2)[1]);
                 exerciseList.addExercise(new Exercise(nameOfExercise, amountOfCaloriesBurnt, durationOfExercise));
+                storage.writeExerciseList(exerciseList);
                 Ui.showAdd("The following execise has been added: " + nameOfExercise);
             }
         } catch (NumberFormatException | NullPointerException e) {
-            System.out.println("Sorry calories have to be a number");
+            Ui.printCustomError("Sorry calories have to be a number");
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.print("Please key in the correct format");
+            Ui.printCustomError("Please key in the correct format");
+        } catch (IOException e) {
+            Ui.printCustomError("Sorry, there is an error in the file");
         }
     }
 
--- src/main/java/fitr/command/AddFoodCommand.java
+++ src/main/java/fitr/command/AddFoodCommand.java
@@ -7,6 +7,8 @@ import fitr.list.FoodList;
 import fitr.storage.Storage;
 import fitr.ui.Ui;
 
+import java.io.IOException;
+
 public class AddFoodCommand extends Command {
     public AddFoodCommand(String command) {
         this.command = command;
@@ -22,17 +24,21 @@ public class AddFoodCommand extends Command {
             if (command.split(" ").length == 1) {
                 Calorie amountOfCalories = new Calorie(Integer.parseInt(command.split(" ")[0]));
                 foodlist.addFood(new Food(nameOfFood, amountOfCalories));
+                storage.writeFoodList(foodlist);
                 Ui.showAdd("The following food has been added: " + nameOfFood);
             } else if (command.split(" ").length == 2) {
                 Calorie amountOfCalories = new Calorie(Integer.parseInt(command.split(" ")[0]));
                 int amountOfFood = Integer.parseInt(command.split(" ", 2)[1]);
                 foodlist.addFood(new Food(nameOfFood, amountOfCalories, amountOfFood));
+                storage.writeFoodList(foodlist);
                 Ui.showAdd("The following food has been added: " + nameOfFood);
             }
         } catch (NumberFormatException | NullPointerException e) {
             System.out.println("Sorry calories have to be a number");
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.print("Please key in the correct format");
+        } catch (IOException e) {
+            Ui.printCustomError("Sorry, there is an error in the file");
         }
     }
 
