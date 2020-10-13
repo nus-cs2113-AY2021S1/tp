0e7002dcccc0125a5af8880bb5be35ea37d1e3c7
LAPTOP-QPAQOSSJ\dixon <dixon.1996@hotmail.co.uk>
2020-10-13 19:51:49 +0800

    Edited the PrintType for goal status and apply slap to target goal tracker

--- src/main/java/seedu/financeit/goaltracker/GoalTracker.java
+++ src/main/java/seedu/financeit/goaltracker/GoalTracker.java
@@ -140,65 +140,77 @@ public class GoalTracker {
         entryData = entry;
         entryCategory = entryData.getEntryType().toString();
         if (entryCategory.equals("Expense")) {
-            try {
-                LedgerList ledgerList = ManualTracker.getLedgerList();
-                int ledgerSize = ledgerList.getItemsSize();
-                for (int i = ledgerSize - 1; i < ledgerSize; i++) {
-                    Ledger ledger = (Ledger) ledgerList.getItemFromIndex(i);
-                    EntryList entryList = ledger.entryList;
-                    int entrySize = entryList.getItemsSize();
-                    for (int x = entrySize - 1; x < entrySize; x++) {
-                        expenses = entryData.getAmount();
-                        totalExpenses += expenses;
-                        System.out.println(totalExpenses);
-                    }
-                }
-                expenseGoal = totalGoalList.getGoal().get(0).getExpenseGoal();
-                double goalDifference = expenseGoal - totalExpenses;
-                if (goalDifference < 0) {
-                    UiManager.printWithStatusIcon(Constants.PrintType.SYS_MSG, "Expense Budget Updated, "
-                            + " You have spent $" + totalExpenses + " / $" + expenseGoal + ". You have exceeded your "
-                            + "expense budget.");
-                } else {
-                    UiManager.printWithStatusIcon(Constants.PrintType.SYS_MSG, "Expense Budget Updated, "
-                            + "You have spent $" + totalExpenses + " / $" + expenseGoal + " You have not reached your "
-                            + "expense budget." + " You still have $" + goalDifference + " to spend.");
-                }
-            } catch (IndexOutOfBoundsException e) {
-                UiManager.printWithStatusIcon(Constants.PrintType.SYS_MSG, "You did not set "
-                        + "a goal for expense.");
-
-            }
+            handleExpenseGoal();
         } else {
-            try {
-                LedgerList ledgerList = ManualTracker.getLedgerList();
-                int ledgerSize = ledgerList.getItemsSize();
-                for (int i = ledgerSize - 1; i < ledgerSize; i++) {
-                    Ledger ledger = (Ledger) ledgerList.getItemFromIndex(i);
-                    EntryList entryList = ledger.entryList;
-                    int entrySize = entryList.getItemsSize();
-                    for (int x = entrySize - 1; x < entrySize; x++) {
-                        incomes = entryData.getAmount();
-                        totalIncomes += incomes;
-                        System.out.println(totalIncomes);
-                    }
-                }
-                incomeGoal = totalGoalList.getGoal().get(0).getIncomeGoal();
-                double goalDifference = incomeGoal - totalIncomes;
-                if (goalDifference < 0) {
-                    UiManager.printWithStatusIcon(Constants.PrintType.SYS_MSG, "Revenue Goal Updated, "
-                            + " You have saved $" + totalIncomes + " / $" + incomeGoal + ". You have met your "
-                            + "revenue goal.");
-                } else {
-                    UiManager.printWithStatusIcon(Constants.PrintType.SYS_MSG, "Revenue Goal Updated, "
-                            + "You have saved $" + totalIncomes + " / $" + incomeGoal + " You have not met your "
-                            + "revenue goal." + " You are $" + goalDifference + " away from your goal.");
-                }
-            } catch (IndexOutOfBoundsException e) {
-                UiManager.printWithStatusIcon(Constants.PrintType.SYS_MSG, "You did not set "
-                        + "a goal for income.");
+            handleIncomeGoal();
+        }
+    }
 
+    public static void handleExpenseGoal() {
+        try {
+            LedgerList ledgerList = ManualTracker.getLedgerList();
+            int ledgerSize = ledgerList.getItemsSize();
+            for (int i = ledgerSize - 1; i < ledgerSize; i++) {
+                Ledger ledger = (Ledger) ledgerList.getItemFromIndex(i);
+                EntryList entryList = ledger.entryList;
+                int entrySize = entryList.getItemsSize();
+                for (int x = entrySize - 1; x < entrySize; x++) {
+                    expenses = entryData.getAmount();
+                    totalExpenses += expenses;
+                }
             }
+            printExpenseGoal();
+        } catch (IndexOutOfBoundsException e) {
+            UiManager.printWithStatusIcon(Constants.PrintType.GOAL_STATUS, "You did not set "
+                    + "a goal for expense.");
+        }
+    }
+
+    public static void printExpenseGoal() {
+        expenseGoal = totalGoalList.getGoal().get(0).getExpenseGoal();
+        double goalDifference = expenseGoal - totalExpenses;
+        if (goalDifference < 0) {
+            UiManager.printWithStatusIcon(Constants.PrintType.GOAL_STATUS, "Expense Budget Updated, "
+                    + " You have spent $" + totalExpenses + " / $" + expenseGoal + ". You have exceeded your "
+                    + "expense budget.");
+        } else {
+            UiManager.printWithStatusIcon(Constants.PrintType.GOAL_STATUS, "Expense Budget Updated, "
+                    + "You have spent $" + totalExpenses + " / $" + expenseGoal + " You have not reached your "
+                    + "expense budget." + " You still have $" + goalDifference + " to spend.");
+        }
+    }
+
+    public static void handleIncomeGoal() {
+        try {
+            LedgerList ledgerList = ManualTracker.getLedgerList();
+            int ledgerSize = ledgerList.getItemsSize();
+            for (int i = ledgerSize - 1; i < ledgerSize; i++) {
+                Ledger ledger = (Ledger) ledgerList.getItemFromIndex(i);
+                EntryList entryList = ledger.entryList;
+                int entrySize = entryList.getItemsSize();
+                for (int x = entrySize - 1; x < entrySize; x++) {
+                    incomes = entryData.getAmount();
+                    totalIncomes += incomes;
+                }
+            }
+            printIncomeGoal();
+        } catch (IndexOutOfBoundsException e) {
+            UiManager.printWithStatusIcon(Constants.PrintType.GOAL_STATUS, "You did not set "
+                    + "a goal for income.");
+        }
+    }
+
+    public static void printIncomeGoal() {
+        incomeGoal = totalGoalList.getGoal().get(0).getIncomeGoal();
+        double goalDifference = incomeGoal - totalIncomes;
+        if (goalDifference < 0) {
+            UiManager.printWithStatusIcon(Constants.PrintType.GOAL_STATUS, "Revenue Goal Updated, "
+                    + " You have saved $" + totalIncomes + " / $" + incomeGoal + ". You have met your "
+                    + "revenue goal.");
+        } else {
+            UiManager.printWithStatusIcon(Constants.PrintType.GOAL_STATUS, "Revenue Goal Updated, "
+                    + "You have saved $" + totalIncomes + " / $" + incomeGoal + " You have not met your "
+                    + "revenue goal." + " You are $" + goalDifference + " away from your goal.");
         }
     }
 }
