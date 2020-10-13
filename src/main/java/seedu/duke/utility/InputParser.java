d10b6efeebe96c87c7c88d21c3bde44b17054675
benardotang <benardotang@gmail.com>
2020-10-13 16:12:15 +0800

    Added some tests for InputParser

--- src/main/java/seedu/duke/utility/InputParser.java
+++ src/main/java/seedu/duke/utility/InputParser.java
@@ -29,70 +29,65 @@ public class InputParser {
     }
 
 
-    public void parseInput(String input) {
+    public String parseInput(String input) {
 
-        String command = StringOperations.getFirstWord(input);
-        switch (command.toLowerCase()) {
+        String command = StringOperations.getFirstWord(input).toLowerCase();
+
+        switch (command) {
         case "bye":
             Ui.printByeMessage();
             this.isBye = true;
-            return;
+            return command;
 
         case "help":
             Ui.printHelp();
-            return;
+            return command;
 
         case "episode":
             parseEpisodeUpdateCommand(input, command);
+            return command;
 
-            return;
         case "season":
             parseSeasonUpdateCommand(input, command);
-            return;
+            return command;
 
         case "rating":
             parseAddRatingCommand(input);
-            return;
+            return command;
 
         case "deleterating":
             parseDeleteRatingCommand(input);
-            return;
+            return command;
 
         case "list":
             Ui.printShowList();
-            //parseListCommand(ShowList.getShowList());
-            return;
+            return command;
 
         case "changerating":
             parseChangeRatingCommand(input);
-            return;
+            return command;
 
         case "add":
             parseAddCommand(input);
-            return;
-
+            return command;
 
         case "delete":
             parseDeleteCommand(input);
-            return;
+            return command;
 
         case "edit":
             parseEditCommand(input);
-            return;
-
+            return command;
 
         case "":
             Ui.printNoInputException();
-            return;
+            return command;
 
         default:
             Ui.printBadInputException();
+            return command;
         }
     }
-
-
-    //TODO: differentiate between show and movie soon
-    //TODO: fix error handling for parsing - follow episode and season
 
     private static void parseEditCommand(String input) {
         ArrayList<String> tokenizedString = StringOperations.tokenizeStringArray(input);
@@ -150,8 +145,6 @@ public class InputParser {
     }
 
     private static void parseChangeRatingCommand(String input) {
-        // catch exceptions for 1)not enough inputs, 2)invalid args, 3)invalid rating
-        // split input using tokenizer
         input = StringOperations.removeFirstWord(input);
         String[] tokenizedInput = input.split(" ");
         int showRating = Integer.parseInt(tokenizedInput[1]);
@@ -161,40 +154,17 @@ public class InputParser {
     }
 
     private static void parseAddCommand(String input) {
-        //catch for 1)too many/not enough inputs , 2)invalid input, 3)show alr added
-        //maybe last arg is optional? idk cos user might not wanna put rating yet
         String[] tokenizedInput = input.split(" ");
         new AddCommand(tokenizedInput);
-        //add show detail into appropriate class
         Ui.printShowAdded(tokenizedInput[1]);
     }
 
     private static void parseDeleteCommand(String input) {
-        //catch for 1)show not found , 2) invalid no. of args
         input = StringOperations.removeFirstWord(input);
         DeleteCommand deletingShow = new DeleteCommand(input);
         deletingShow.delete(input);
         Ui.printDeleteShow(input);
     }
-
-    /*
-    private static seedu.duke.commands.Command parseEditCommand(String input) {
-        //this one might need check agn, the current i/o is a yikes imo
-        seedu.duke.utility.Ui.queryEditShow(input);
-        java.util.Scanner scan = null;
-        boolean isEditFinished = false;
-        while (!isEditFinished)){
-            System.out.println("Name: (Press enter to skip)");
-            //edit name
-            System.out.println("Episodes: (Press enter to skip or done to exit)");
-            //edit episode
-            System.out.println("Seasons: (Press enter to skip or done to exit)");
-            //edit seasons
-        }
-
-    }
-
-*/
 
 }
 
