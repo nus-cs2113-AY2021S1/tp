63059f76c96871cbcfef538647876c3b27ba25a8
Greek <ygreek@gmail.com>
2020-10-13 15:55:23 +0800

    Add JUnit for UI test

--- src/main/java/fitr/ui/Ui.java
+++ src/main/java/fitr/ui/Ui.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 import static fitr.common.Commands.COMMAND_FOOD;
 import static fitr.common.Commands.COMMAND_EXERCISE;
+import static fitr.common.Commands.COMMAND_VIEW_PROFILE;
+import static fitr.common.Commands.COMMAND_VIEW_BMI;
 import static fitr.common.Commands.COMMAND_VIEW_FOOD;
 import static fitr.common.Commands.COMMAND_VIEW_EXERCISE;
 import static fitr.common.Commands.COMMAND_VIEW_SUMMARY;
@@ -59,6 +61,8 @@ public class Ui {
         System.out.printf(SPACE, COMMAND_EXERCISE, "Adds exercise entry to Fitr program");
         printCustomMessage(FORMAT + FORMAT_EXERCISE);
 
+        System.out.printf(SPACE, COMMAND_VIEW_PROFILE, "View your profile");
+        System.out.printf(SPACE, COMMAND_VIEW_BMI, "View your BMI");
         System.out.printf(SPACE, COMMAND_VIEW_FOOD, "View food entries");
         System.out.printf(SPACE, COMMAND_VIEW_EXERCISE, "View exercise entries");
         System.out.printf(SPACE, COMMAND_VIEW_SUMMARY, "View calorie summary");
@@ -72,24 +76,29 @@ public class Ui {
     public static void printFormatError(String command) {
         if (command.equals(COMMAND_VIEW)) {
             printCustomError(ERROR_INVALID_COMMAND);
+            System.out.printf(SPACE, COMMAND_VIEW_PROFILE, "View your profile");
+            System.out.printf(SPACE, COMMAND_VIEW_BMI, "View your BMI");
             System.out.printf(SPACE, COMMAND_VIEW_FOOD, "View food entries");
             System.out.printf(SPACE, COMMAND_VIEW_EXERCISE, "View exercise entries");
             System.out.printf(SPACE, COMMAND_VIEW_SUMMARY, "View calorie summary");
             return;
         }
 
-        printCustomError("Please input in the correct format!");
         switch (command) {
         case COMMAND_FOOD:
+            printCustomError("Please input in the correct format!");
             printCustomError(FORMAT_FOOD);
             break;
         case COMMAND_EXERCISE:
+            printCustomError("Please input in the correct format!");
             printCustomError(FORMAT_EXERCISE);
             break;
         case COMMAND_DELETE:
+            printCustomError("Please input in the correct format!");
             printCustomError(FORMAT_DELETE);
             break;
         default:
+            printInvalidCommandError();
             break;
         }
     }
