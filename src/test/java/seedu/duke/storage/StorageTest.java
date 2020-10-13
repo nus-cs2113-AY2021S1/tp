6df0030738f243b7616d4d0989d8c1ebf5f07c89
Ong De Zhi <39303087+OngDeZhi@users.noreply.github.com>
2020-10-13 16:03:23 +0800

    Update JUnit testing

--- src/test/java/seedu/duke/storage/StorageTest.java
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -6,6 +6,7 @@ import seedu.duke.human.User;
 import seedu.duke.ui.Ui;
 import seedu.duke.watchlist.Watchlist;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,40 +16,20 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 class StorageTest {
     private static final String INVALID_USER_PROFILE_FILE = "a/b/c/invalid_userprofile.txt";
     private static final String INVALID_WATCHLIST_FILE = "a/b/c/invalid_watchlist.txt";
-    private static final String USER_PROFILE_FILE = "../../text-ui-test/data/AniChan/userprofile.txt";
-    private static final String WATCHLIST_FILE = "../../text-ui-test/data/AniChan/watchlist.txt";
 
     private Ui ui;
-    private Storage validStorage;
     private Storage invalidStorage;
 
     @BeforeEach
     void setUp() {
         ui = new Ui();
-        validStorage = new Storage(USER_PROFILE_FILE, WATCHLIST_FILE);
         invalidStorage = new Storage(INVALID_USER_PROFILE_FILE, INVALID_WATCHLIST_FILE);
-    }
-
-    @Test
-    void loadUser_validStorage_notNullObject() {
-        User user = validStorage.loadUser(ui);
-        String expected = "\nName: Tester\nBirthdate: 01/01/2020\nGender: Other";
-        assertEquals(user.toString(), expected);
     }
 
     @Test
     void loadUserProfile_invalidStorage_nullObject() {
         User user = invalidStorage.loadUser(ui);
         assertNull(user);
-    }
-
-    @Test
-    void loadWatchlist_validStorage_notNullObject() {
-        ArrayList<Watchlist> watchlists = validStorage.loadWatchlist(ui);
-        String expected = "Test List" + System.lineSeparator();
-        expected += "1. Testing" + System.lineSeparator() + "2. The" + System.lineSeparator();
-        expected += "3. Watchlist" + System.lineSeparator();
-        assertEquals(watchlists.get(0).toString(), expected);
     }
 
     @Test
