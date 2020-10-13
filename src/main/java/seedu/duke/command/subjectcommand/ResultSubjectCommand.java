a0900590d49f9c30fc7f1b12355b50efed10b90c
I MUTHUKUMAR <imuthukumar1998@gmail.com>
2020-10-13 13:02:42 +0800

    Integrating the quiz and result features of a subject into the application
    The QuizSubjectCommand.class instantiated the quiz feature while
    the ResultSubjectCommand.class instantiates the results feature
    for the subject.

---
+++ src/main/java/seedu/duke/command/subjectcommand/ResultSubjectCommand.java
@@ -0,0 +1,27 @@
+package seedu.duke.command.subjectcommand;
+
+import seedu.duke.card.Subject;
+import seedu.duke.card.SubjectList;
+import seedu.duke.ui.Ui;
+
+public class ResultSubjectCommand extends SubjectCommand {
+    private String fullcommand;
+    public ResultSubjectCommand(String fullcommand){
+        this.fullcommand= fullcommand;
+
+    }
+    public Subject execute(SubjectList subjectList){
+
+        String[] message = this.fullcommand.split(" ");
+        Subject quizSubject=null;
+
+        for(Subject subject: subjectList.getList()){
+            if (subject.toString().contains(message[1])) {
+                quizSubject=subject;
+            }
+        }
+        assert (quizSubject != null): "No such subject exists!";
+        Ui.printSubjectResults(quizSubject);
+        return null;
+    }
+}
--- src/main/java/seedu/duke/command/subjectcommand/ReturnSubjectCommand.java
+++ src/main/java/seedu/duke/command/subjectcommand/ReturnSubjectCommand.java
@@ -2,10 +2,10 @@ package seedu.duke.command.subjectcommand;
 
 import seedu.duke.card.Subject;
 import seedu.duke.card.Topic;
-import seedu.duke.card.TopicList;
+import seedu.duke.card.quiz.TopicQuiz;
 import seedu.duke.command.Command;
 import seedu.duke.command.taskcommand.TaskCommand;
-import seedu.duke.command.topiccommand.ListTopicCommand;
+import seedu.duke.command.topiccommand.QuizTopicCommand;
 import seedu.duke.command.topiccommand.ReturnTopicCommand;
 import seedu.duke.command.topiccommand.TopicCommand;
 import seedu.duke.card.SubjectList;
@@ -17,12 +17,10 @@ import seedu.duke.exception.TaskException;
 import seedu.duke.exception.TaskTodoException;
 import seedu.duke.parser.SubjectParser;
 import seedu.duke.parser.TaskParser;
+import seedu.duke.exception.NoFlashCardException;
 import seedu.duke.parser.TopicParser;
 import seedu.duke.task.TaskList;
 import seedu.duke.ui.Ui;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class ReturnSubjectCommand extends SubjectCommand {
     private String fullcommand;
@@ -51,21 +49,28 @@ public class ReturnSubjectCommand extends SubjectCommand {
         while (!isSubjectExit) {
             try {
                 String fullCommand = Ui.readCommand();
-                Command c =  TopicParser.parse(fullCommand);
+                Command c = TopicParser.parse(fullCommand);
                 if (c instanceof ReturnTopicCommand) {
                     Topic topic = ((ReturnTopicCommand) c).execute(subject);
                     ((ReturnTopicCommand) c).goToTopic(topic, subject);
+
+                } else if (c instanceof QuizTopicCommand) {
+                    Topic topic = ((QuizTopicCommand) c).execute(subject);
+                    TopicQuiz topicQuiz = new TopicQuiz(topic);
+                    topicQuiz.startQuiz(topic.getResults());
                 } else if (c instanceof TopicCommand) {
                     ((TopicCommand) c).execute(subject);
                 } else if (c instanceof TaskCommand) {
                     TaskList taskList = subject.getTasks();
                     ((TaskCommand) c).execute(taskList);
                 }
+
                 isSubjectExit = c.isExit();
                 //TODO: implement the storage methods for Topic
                 //topics.saveSubject(topicStorage.getFileName());
-            //} catch (IOException e) {
+                //} catch (IOException e) {
                 //Ui.printWritingError();
+
             } catch (NumberFormatException e) {
                 Ui.printIndexError();
             } catch (NoSubjectException e) {
@@ -82,9 +87,12 @@ public class ReturnSubjectCommand extends SubjectCommand {
                 Ui.printEventError();
             } catch (TaskException e) {
                 Ui.printError();
+            } catch (NoFlashCardException e) {
+                Ui.printNoFlashcards();
             }
         }
-        Ui.printExitToMain();
+            Ui.printBackToSubjects();
+
     }
 
     public boolean isExit() {
