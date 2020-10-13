34b33894122ae0c6199c877c1958728cf65c20ce
I MUTHUKUMAR <imuthukumar1998@gmail.com>
2020-10-13 12:54:32 +0800

    Instantiate necessary classes for quiz function
    The SubjectQuiz and TopicQuiz classes instantiates the
    quiz feature for the topic or subject which it is called for.
    
    The Result class checks the result that the user had obtained for the quiz.
    The ResultList class stores all of the results from the quizzes for each subject/topic.

---
+++ src/main/java/seedu/duke/card/quiz/TopicQuiz.java
@@ -0,0 +1,82 @@
+package seedu.duke.card.quiz;
+
+import seedu.duke.card.Flashcard;
+import seedu.duke.card.Topic;
+import seedu.duke.exception.NoFlashCardException;
+import seedu.duke.exception.NoTopicException;
+import seedu.duke.ui.Ui;
+
+import java.time.Instant;
+
+public class TopicQuiz extends Quiz {
+
+    private Topic topic;
+
+    public TopicQuiz(Topic topic) {
+        this.topic = topic;
+        this.flashcards = topic.getFlashcards();
+        this.result.setMaxScore(this.flashcards.size());
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public void setUpQuiz() throws NoFlashCardException {
+        int maxScore = 0;
+        if (this.flashcards.size() == 0) {
+            throw new NoFlashCardException();
+        }
+        this.result.setMaxScore(this.flashcards.size());
+
+    }
+
+    public void startQuiz(ResultList results) throws NoFlashCardException {
+        setUpQuiz();
+        this.result.setScore(0);
+
+        Ui.printStartTopicQuiz(this.topic);
+
+        Instant end = Instant.now().plusSeconds(60);
+        String answer = null;
+
+        for (Flashcard flashcard : this.flashcards) {
+            if (Instant.now().isAfter(end)) {
+                break;
+            }
+            Ui.printQuestion(flashcard.getQuestion());
+            answer = Ui.readCommand().strip();
+            if (answer.equals("stop")) {
+                Ui.printStopQuiz();
+                Ui.printScore(this.result);
+                break;
+            } else {
+                checkAnswer(answer, flashcard);
+            }
+
+        }
+        assert answer != null;
+
+
+        if (!answer.equals("stop")) {
+            Ui.printEndQuiz();
+            Ui.printScore(this.result);
+            if(this.result.getScore() < this.result.getMaxScore()) {
+                Ui.printIncorrectAnswers(this.incorrectAnswers);
+            }
+
+        }
+
+
+
+        this.topic.getResults().add(this.result);
+
+
+    }
+
+
+}
