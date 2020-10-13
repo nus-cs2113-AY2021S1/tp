13584e24ff9b746b32467c3511c1eaa38f50b095
Shikai Zhou <shikaizhou@outlook.com>
2020-10-13 13:01:53 +0800

    Added test for add command

--- src/main/java/seedu/duke/commands/AddCommand.java
+++ src/main/java/seedu/duke/commands/AddCommand.java
@@ -6,6 +6,10 @@ import seedu.duke.utility.ShowList;
 public class AddCommand extends Command {
     String[] description;
 
+    /**
+     * Add a new show.
+     * @param input the user input
+     */
     public AddCommand(String[] input) {
         int numSeasons = Integer.parseInt(input[2]);
         String[] tokenizedSeasons = input[3].split(",");
@@ -14,6 +18,11 @@ public class AddCommand extends Command {
         for (String s : tokenizedSeasons) {
             seasonEpisodes[i] = Integer.parseInt(s);
             i++;
+            //check that the episodes do not exceed seasons
+            if (i == numSeasons) {
+                System.out.println("Mismatched number of seasons and episodes");
+                break;
+            }
         }
         Show show = new Show(input[1], Integer.parseInt(input[2]), seasonEpisodes);
         String name = input[1];
---
+++ src/test/java/seedu/duke/commands/AddCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.duke.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import seedu.duke.classes.Show;
+import seedu.duke.utility.ShowList;
+
+class AddCommandTest {
+
+    public static AddCommand updateObject;
+
+    @BeforeAll
+    public static void main() {
+        new ShowList();
+        int[] episodes = new int[]{21, 10, 12};
+        Show show = new Show("friends", 3, episodes);
+        ShowList.setShow("friends", show);
+        assertEquals(show, ShowList.getShow("friends"));
+        String[] inputExample = {"add", "friends", "3", "21,10,12"};
+        updateObject = new AddCommand(inputExample);
+    }
+
+    @Test
+    public void processCommand() {
+        assertEquals(21,ShowList.getShow("friends").getEpisodesForSeason(1));
+
+    }
+}
