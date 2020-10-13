86bc662a0a95f478b18c78104c4c001603591458
lhydl <dereklee9262@gmail.com>
2020-10-13 14:58:10 +0800

    Add Junit tests

---
+++ src/test/java/seedu/modtracker/ParserTest.java
@@ -0,0 +1,67 @@
+package seedu.modtracker;
+
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    private static final String TEST_FILEPATH = "test/data/modlist.txt";
+    Storage storage = new Storage(TEST_FILEPATH);
+    ModuleList modList = new ModuleList();
+    Parser parser = new Parser();
+
+    @Test
+    public void parse_invalidInput_expectPrintInvalid() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        String input = "exit addmod ";
+        parser.parse(input, modList, null, storage, true);
+        String expected = "OOPS!!! I'm sorry, but I don't know what that means :-(" + System.lineSeparator();
+
+        assertEquals(expected + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    public void parse_blankInput_expectPrintInvalid() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        String input = "";
+        parser.parse(input, modList, null, storage, true);
+        String expected = "OOPS!!! I'm sorry, but I don't know what that means :-(" + System.lineSeparator();
+
+        assertEquals(expected + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    public void parse_exitInput_expectPrintExit() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        String input = "exit";
+        String name = "Lee";
+        parser.parse(input, modList, name, storage, true);
+        String expected = "All changes saved." + System.lineSeparator() + "Bye " + name + ". Hope to see you again soon!" + System.lineSeparator();
+
+        assertEquals(expected + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    public void parse_exitInput_expectExitStatusTrue() {
+        String input = "exit";
+        parser.parse(input, modList, null, storage, true);
+        assertTrue(parser.isExit());
+    }
+
+    @Test
+    public void parse_anyInput_expectExitStatusFalse() {
+        String input = "addexp ";
+        parser.parse(input, modList, null, storage, true);
+        assertFalse(parser.isExit());
+    }
+}
