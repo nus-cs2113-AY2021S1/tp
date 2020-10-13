5fc3339acfb1c43977756d1b2e794e3bad14fce2
QX-CHEN <c.qixing@yahoo.com.sg>
2020-10-13 14:55:01 +0800

    Expand help message

--- src/main/java/seedu/messages/Messages.java
+++ src/main/java/seedu/messages/Messages.java
@@ -1,6 +1,7 @@
 package seedu.messages;
 
 import static seedu.font.Colors.ANSI_CYAN;
+import static seedu.font.Colors.ANSI_PURPLE;
 import static seedu.font.Colors.ANSI_RESET;
 import static seedu.font.Colors.ANSI_YELLOW;
 
@@ -22,10 +23,29 @@ public class Messages {
             + "/_/   /_/\\__,_/_/ |_/\\____//____/  "
             + "v1.0" + ANSI_RESET + LS;
     public static final String HELP_MESSAGE = "List of available commands:" + LS
+            + "Notes: " + LS
+            + "1. Command end with space is invalid." + LS
+            + "2. One space between options." + LS
+            + "3. Square bracket input is optional." + LS
             + "- help: show list of available commands" + LS
+            + "format: " + ANSI_PURPLE + "help" + ANSI_RESET + LS
             + "- add: add a task" + LS
+            + "format: " + ANSI_PURPLE
+            + "add DESCRIPTION [d/dd-MM-yyyy] [st/HHmm] [et/HHmm] [p/NUMBER]" + ANSI_RESET + LS
+            + "- edit: edit a task" + LS
+            + "format: " + ANSI_PURPLE
+            + "edit INDEX [des/DESCRIPTION] [d/dd-MM-yyyy] [st/HHmm] [et/HHmm] [p/NUMBER]" + ANSI_RESET + LS
             + "- list: show list of tasks" + LS
-            + "- bye: exit the program" + LS;
+            + "format: " + ANSI_PURPLE + "list" + ANSI_RESET + LS
+            + "- search: find task(s) with keyword in description" + LS
+            + "format: " + ANSI_PURPLE + "search KEYWORD(S)" + ANSI_RESET + LS
+            + "- delete remove particular task by index" + LS
+            + "format: " + ANSI_PURPLE + "delete INDEX" + ANSI_RESET + LS
+            + "- clear: remove all tasks" + LS
+            + "format: " + ANSI_PURPLE + "clear" + ANSI_RESET + LS
+            + "- bye: exit the program" + LS
+            + "format: " + ANSI_PURPLE + "bye" + ANSI_RESET + LS;
+
     public static final String BYE_MESSAGE = "Bye! See you again!";
     public static final String LIST_MESSAGE = "Here is your list of tasks:";
     public static final String ADD_MESSAGE = "Task added.";
