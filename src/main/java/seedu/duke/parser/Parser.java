ee5dd6588bfe4a2a355a2e897ef08dbcca7c5124
Michael Dinata <michaeldinata@Michaels-MacBook-Pro.local>
2020-10-13 18:18:56 +0800

    Added some assertions and logging

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -11,7 +11,11 @@ import seedu.duke.command.HelpCommand;
 import seedu.duke.command.ExitCommand;
 import seedu.duke.exception.AniException;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Parser {
+    private static Logger logger = Logger.getLogger("ParserLogger");
 
     /**
      * Prints the main menu of the application
@@ -56,9 +60,12 @@ public class Parser {
     }
 
     public static String[] parseUserInput(String input) throws AniException {
+        logger.log(Level.INFO, "Starting parser processing");
         if (input == null || input.isEmpty()) {
+            logger.log(Level.WARNING, "Exception occurred");
             throw new AniException("Input is empty");
         }
+        logger.log(Level.INFO, "End of parser processing");
 
         String[] inputSplit = input.split(" ", 2);
         return inputSplit;
