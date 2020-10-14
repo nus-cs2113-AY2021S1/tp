eff28d11f2da6592fade0589aad8ec6a76d83616
LAPTOP-LK4JO5OO\65837 <varsha.ms30@gmail.com>
2020-10-14 13:28:30 +0800

    Added Junit tests for event

--- src/test/java/seedu/duke/event/EventTest.java
+++ src/test/java/seedu/duke/event/EventTest.java
@@ -2,18 +2,41 @@ package seedu.duke.event;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
+    Event event = new Event("PR meeting", "2000-06-30", "8pm");
 
     @Test
     void testPrintEvent() {
-        Event event = new Event("PR meeting", "2000-06-30", "8pm");
         assertEquals("Event Name: " + "PR meeting" + "\nDate: "
                 + "Jun 30 2000" + "\nTime: " + "8pm" + "\n", event.printEvent());
+    }
+
+    @Test
+    void testEventAdd() {
+        String expected1 = "Got it. I've added this Event:\n"
+                + "Event Name: PR meeting"
+                + "\nDate: Jun 30 2000"
+                + "\nTime: 8pm\n"+
+                "\nNow you have 1 event in the list.\n";
+        assertEquals(expected1,EventList.addEvent(event));
+
+        String expected3 = "Here are the current events in your list:\n"
+                + "1.Event Name: PR meeting"
+                + "\nDate: Jun 30 2000"
+                + "\nTime: 8pm\n";
+        assertEquals(expected3, EventList.printEventList());
+
+        String expected2 = "I'll remove this Event:\n"
+                + "Event Name: PR meeting"
+                + "\nDate: Jun 30 2000"
+                + "\nTime: 8pm\n"+
+                "\nNow you have 0 event in the list.";
+        assertEquals(expected2,EventList.deleteEvent(0));
     }
 
 }
