a64a38e0ccad6266e1f7e83992edde56cbf55fd6
WYing333 <e0634980@u.nus.edu>
2020-10-14 12:42:12 +0800

    no message

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,10 +1,10 @@
-//for pull request
+package seedu.duke;//for pull request
 
 import java.util.Scanner;
 
 public class Duke {
     /**
-     * Main entry-point for the java.duke.Duke application.
+     * Main entry-point for the java.duke.seedu.duke.Duke application.
      */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
