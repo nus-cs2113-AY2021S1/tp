0d3ce07e285c3fc2fb98ad9d41a43c0cd3634801
ChanJianHao <49814823+ChanJianHao@users.noreply.github.com>
2020-10-14 22:15:21 +0800

    Deconflict JUnit test

--- src/test/java/seedu/duke/human/UserManagementTest.java
+++ src/test/java/seedu/duke/human/UserManagementTest.java
@@ -9,8 +9,8 @@ import java.text.ParseException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserManagementTest {
-    private static final String USER_PROFILE_FILE_NAME = "userprofile.txt";
-    private static final String WATCHLIST_FILE_NAME = "watchlist.txt";
+    private static final String USER_PROFILE_FILE_NAME = "userprofileTEST.txt";
+    private static final String WATCHLIST_FILE_NAME = "watchlistTEST.txt";
 
     @Test
     void getTotalUsers_noUsers_sizeZero() {
@@ -21,8 +21,8 @@ class UserManagementTest {
 
     @Test
     void testAddUser() throws ParseException, AniException {
-        Storage storage = new Storage(USER_PROFILE_FILE_NAME, WATCHLIST_FILE_NAME);
-        UserManagement userManagement = new UserManagement(storage);
+        Storage testStorage = new Storage(USER_PROFILE_FILE_NAME, WATCHLIST_FILE_NAME);
+        UserManagement userManagement = new UserManagement(testStorage);
 
         User newUser = userManagement.addUser("Wallace", "14/2/2019", "Male");
         assertEquals("\n Name: Wallace\n Birthdate: 14/02/2019\n Gender: Male", newUser.toString());
