c095a3d2a23b9557ed46f65f20900586b7561f1f
Joseph <e0406453@u.nus.edu>
2020-10-15 04:43:20 +0800

    Fix JUnit test errors.

--- src/test/java/seedu/eduke8/parser/MenuParserTest.java
+++ src/test/java/seedu/eduke8/parser/MenuParserTest.java
@@ -2,26 +2,24 @@ package seedu.eduke8.parser;
 
 import org.junit.jupiter.api.Test;
 import seedu.eduke8.command.Command;
-import seedu.eduke8.exception.Eduke8Exception;
+import seedu.eduke8.command.IncorrectCommand;
 import seedu.eduke8.topic.TopicList;
 import seedu.eduke8.ui.Ui;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MenuParserTest {
     @Test
-    public void menuParser_wrongStringInput_expectEduk8Exception() {
+    public void menuParser_wrongStringInput_expectIncorrectCommand() {
         Ui ui = new Ui();
         TopicList topicList = new TopicList(null);
         MenuParser menuParser = new MenuParser();
 
         try {
             Command badCommand = menuParser.parseCommand(topicList, "quiz /twrongtopic /nbadnnum");
-            assertThrows(Eduke8Exception.class, () -> {
-                badCommand.execute(topicList, ui);
-            });
-        } catch (Eduke8Exception ee) {
-            ee.printStackTrace();
+            assertTrue(badCommand instanceof IncorrectCommand);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
