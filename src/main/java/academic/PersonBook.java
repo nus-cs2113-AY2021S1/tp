2d0348397af15382341f549fc32076c97ea6c263
farice9 <limyb50@gmail.com>
2020-10-14 19:56:18 +0800

    Move person list and grade list creation to the start of program

--- src/main/java/academic/PersonBook.java
+++ src/main/java/academic/PersonBook.java
@@ -6,24 +6,21 @@ import java.util.ArrayList;
  * Represents a grade book in study-it.
  */
 public class PersonBook {
-    public static ArrayList<Person> listOfPerson = new ArrayList<>(); //TODO change to local storage
-
-    public static void addPerson(String[] args) {
-
-        PersonBook.listOfPerson.add(new Person(args[0],args[1],args[2]));
+    public static void addPerson(String[] args, ArrayList<Person> listOfPerson) {
+        listOfPerson.add(new Person(args[0],args[1],args[2]));
     }
 
-    public static String printPersonBook() {
+    public static String printPersonBook(ArrayList<Person> listOfPerson) {
         int listIndex = 0;
         StringBuilder listToPrint = new StringBuilder();
-        for (Person person : PersonBook.listOfPerson) {
+        for (Person person : listOfPerson) {
             if (person != null) {
                 listToPrint.append(listIndex + 1);
                 listToPrint.append(". [" + person.nameOfPerson + "]");
                 listToPrint.append(" [" + person.contactNumberOfPerson + "]");
                 listToPrint.append(" [" + person.emailOfPerson + "]");
                 listIndex++;
-                if (PersonBook.listOfPerson.size() != listIndex) {
+                if (listOfPerson.size() != listIndex) {
                     listToPrint.append("\n");
                 }
 
