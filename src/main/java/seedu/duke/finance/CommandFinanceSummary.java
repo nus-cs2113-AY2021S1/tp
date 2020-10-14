c75f15a06a9e58b5ad4660f09afbfa8a23af48c7
WangZixin67 <e0376957@u.nus.edu>
2020-10-14 14:09:33 +0800

    folder name change complete

---
+++ src/main/java/seedu/duke/finance/CommandFinanceSummary.java
@@ -0,0 +1,27 @@
+package seedu.duke.finance;
+
+import seedu.duke.Command;
+import seedu.duke.backend.UserInput;
+
+public class CommandFinanceSummary extends Command {
+    UserInput userinput;
+
+    @Override
+    public String execute() {
+        String output = FinanceList.summary();
+        return output;
+    }
+
+    @Override
+    public String help() {
+        return "The format of input to show finance list is: finance summary";
+    }
+
+    public int validate(UserInput ui) {
+        if (ui.getCategory().equals("finance") && ui.getCommand().equalsIgnoreCase("summary")) {
+            userinput = ui;
+            return ACCEPT;
+        }
+        return NO_MATCH;
+    }
+}
