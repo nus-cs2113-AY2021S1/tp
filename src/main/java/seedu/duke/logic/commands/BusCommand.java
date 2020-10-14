73dd0dcd629acf268b3339212033bc5fa170dbbf
ethan2212 <e0407555@u.nus.edu>
2020-10-15 02:27:53 +0800

    Update BusCommand class constructor

--- src/main/java/seedu/duke/logic/commands/BusCommand.java
+++ src/main/java/seedu/duke/logic/commands/BusCommand.java
@@ -2,26 +2,28 @@ package seedu.duke.logic.commands;
 
 import seedu.duke.Bus;
 import seedu.duke.BusData;
+import seedu.duke.BusStops;
 import seedu.duke.exceptions.CustomException;
+import seedu.duke.exceptions.ExceptionType;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 
 public class BusCommand extends Command {
 
     public String busStop;
 
-    public BusCommand(String busStop) {
-        this.busStop=busStop.trim();
+    public BusCommand(String busStop) throws CustomException {
+        this.busStop = BusStops.formatName(busStop.trim());
     }
 
     @Override
-    public void executeCommand() throws CustomException {
+    public void executeCommand() {
         ArrayList<Bus> busList = BusData.busAtStop(busStop);
         System.out.print(busStop + ":  ");
         for (Bus bus: busList) {
             System.out.print(bus.getBusNumber() + "   ");
         }
         System.out.println();
-
     }
 }
