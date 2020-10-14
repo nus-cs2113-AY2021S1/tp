1239613f1845be1cf3d43a22a1516fd35331afbc
n3wsoldier <n3wsoldier@gmail.com>
2020-10-14 09:41:12 +0800

    Update BookmarkAnimeCommand and Bookmark
    1) Change bookmark to storing anime index
    2) Clone animedatalist for quick access
    3) Refactor bookmarkanimecommand using slap

--- src/main/java/seedu/duke/command/BookmarkAnimeCommand.java
+++ src/main/java/seedu/duke/command/BookmarkAnimeCommand.java
@@ -11,8 +11,29 @@ import java.util.ArrayList;
 
 public class BookmarkAnimeCommand extends Command {
 
-    public BookmarkAnimeCommand(String description) {
+    private String bookmarkName;
+    private int bookmarkIndex;
+    private int animeIndex;
+    private int bookmarkEpisode;
+    // e for edit, a for add, d for delete
+    private String bookmarkAction;
+
+    public BookmarkAnimeCommand(String description) throws AniException {
         this.description = description;
+        String[] paramGiven = description.split("-");
+        if(paramGiven.length > 2){
+            String invalidDescription = "-" + description + " has too many parameter!";
+            throw new AniException(invalidDescription);
+        }
+        parameterParser(paramGiven[1]);
+        if (bookmarkAction.equals("e")) {
+            boolean isValidBookmarkIndex = setBookmarkIndex(paramGiven[0].trim());
+            if(!isValidBookmarkIndex){
+                String invalidBookmarkIndex = "" + paramGiven[0] + " is an invalid parameter!"
+                        + " Bookmark index for edit episode requires integer.";
+                throw new AniException(invalidBookmarkIndex);
+            }
+        }
     }
 
     @Override
@@ -20,79 +41,74 @@ public class BookmarkAnimeCommand extends Command {
                           UserManagement userManagement) {
         String result = "";
         Bookmark bookmark = userManagement.getActiveUser().bookmark;
-        if (description.contains(" ")) {
-            String[] descriptionSplit = description.split(" ", 2);
-            // Code to be added
-            String commandOption = descriptionSplit[0];
-            String commandArgument = descriptionSplit[1];
-            if (commandOption.equals("-a")) {
-                if (isInt(commandArgument)) {
-                    int animeDataListIndex = Integer.parseInt(commandArgument);
-                    Anime anime = animeData.getAnimeByID(animeDataListIndex);
-                    result = "Saving " + anime.getAnimeID() + ". " + anime.getAnimeName() + " " + " to bookmark.";
-                    //System.out.println("Saving " + anime.getAnimeID() + ".
-                    // + anime.getAnimeName() + " " + " to bookmark.");
-                    bookmark.addAnimeBookmark(anime.getAnimeName());
-                } else {
-                    ArrayList<Anime> findList = animeData.findName(commandArgument);
-                    for (Anime anime : findList) {
-                        result += "\t" + anime.getAnimeID() + ". " + anime.getAnimeName() + System.lineSeparator();
-                        //System.out.println("\t" + anime.getAnimeID() + ". " + anime.getAnimeName());
-                    }
-                }
-            } else if (commandOption.equals("-d")) {
-                int bookmarkIndex = Integer.parseInt(commandArgument);
-                String animeName = bookmark.getAnimeBookmarkByIndex(bookmarkIndex - 1);
-                result = "Removing " + animeName + "! :(";
-                //System.out.println("Removing " + animeName + "! :(");
-                bookmark.removeAnimeBookmark(bookmarkIndex - 1);
-            } else {
-                int bookmarkIndex = Integer.parseInt(commandOption);
-                String[] commandArgumentSplit = commandArgument.split(" ", 2);
-                String commandOption2 = commandArgumentSplit[0];
-                String commandArgument2 = commandArgumentSplit[1];
-                if (commandOption2.equals("-e")) {
-                    int episode = Integer.parseInt(commandArgument2);
-                    bookmark.editAnimeBookmarkEpisode(bookmarkIndex - 1, episode);
-                    String animeName = bookmark.getAnimeBookmarkByIndex(bookmarkIndex - 1);
-                    result = "Editing " + animeName + " to have " + episode + " episode";
-                    //System.out.println("Editing " + animeName + " to have " + episode + " episode");
-                }
-            }
-        } else {
-            if (description.equals("-l")) {
-                String bookmarks = bookmark.animeListInString();
-                result = bookmarks;
-                //System.out.println(bookmarks);
-            }
+
+        switch (bookmarkAction) {
+        case "e":
+            bookmark.editAnimeBookmarkEpisode(bookmarkIndex - 1, bookmarkEpisode);
+            Anime animeToEdit = bookmark.getAnimeBookmarkByIndex(animeData, bookmarkIndex - 1);
+            result = "Editing " + animeToEdit.getAnimeName() + " to have " + bookmarkEpisode + " episode";
+            //System.out.println("Editing " + animeName + " to have " + episode + " episode");
+            break;
+        case "a":
+            Anime animeToAdd = animeData.getAnimeByID(animeIndex - 1);
+            result = "Saving " + animeToAdd.getAnimeID() + ". " + animeToAdd.getAnimeName() + " " + " to bookmark.";
+            bookmark.addAnimeBookmark(animeToAdd.getAnimeID());
+            break;
+        case "d":
+            Anime animeToDelete = bookmark.getAnimeBookmarkByIndex(animeData, bookmarkIndex - 1);
+            result = "Removing " + animeToDelete.getAnimeName() + "! :(";
+            //System.out.println("Removing " + animeName + "! :(");
+            bookmark.removeAnimeBookmark(bookmarkIndex - 1);
+            break;
+        case "l":
+            result = "Listing all anime in bookmark: ";
+            String bookmarks = bookmark.animeListInString(animeData);
+            result += bookmarks;
+            break;
+        default:
         }
         return result;
     }
 
-    private void parameterParser(String[] paramGiven) throws AniException {
-        for (String param : paramGiven) {
-            String[] paramParts = param.split(" ");
-            switch (paramParts[0].trim()) {
-            case "": //skip the first empty param
-                break;
-            case "s":
-                paramLengthCheck(paramParts);
-
-                break;
-            case "f":
-                paramLengthCheck(paramParts);
-
-                break;
-            case "o":
-                paramLengthCheck(paramParts);
-                break;
-            case "p":
-                paramLengthCheck(paramParts);
-                break;
-            default:
-                String invalidParameter = "-" + param + " is an invalid parameter!";
+    private void parameterParser(String paramGiven) throws AniException {
+        String[] paramParts = paramGiven.split(" ");
+        switch (paramParts[0].trim()) {
+        case "e":
+            paramLengthCheck(paramParts);
+            setBookmarkAction(paramParts[0]);
+            Boolean isValidBookmarkEpisode = setBookmarkEpisode(paramParts[1].trim());
+            if(!isValidBookmarkEpisode){
+                String invalidParameter = "-" + paramGiven + " is an invalid parameter!"
+                        + " Bookmark edit episode param requires integer.";
                 throw new AniException(invalidParameter);
             }
+            break;
+        case "a":
+            paramLengthCheck(paramParts);
+            setBookmarkAction(paramParts[0]);
+            Boolean isValidAnimeIndex = setAnimeIndex(paramParts[1].trim());
+            if(!isValidAnimeIndex){
+                String invalidParameter = "-" + paramGiven + " is an invalid parameter!"
+                        + " Bookmark Add param requires integer.";
+                throw new AniException(invalidParameter);
+            }
+            break;
+        case "d":
+            paramLengthCheck(paramParts);
+            setBookmarkAction(paramParts[0]);
+            Boolean isValidBookmarkIndex = setBookmarkIndex(paramParts[1].trim());
+            if(!isValidBookmarkIndex){
+                String invalidParameter = "-" + paramGiven + " is an invalid parameter!"
+                        + " Bookmark delete param requires integer.";
+                throw new AniException(invalidParameter);
+            }
+            break;
+        case "l":
+            setBookmarkAction(paramParts[0]);
+            break;
+        default:
+            String invalidParameter = "-" + paramGiven + " is an invalid parameter!";
+            throw new AniException(invalidParameter);
         }
     }
 
@@ -107,6 +123,36 @@ public class BookmarkAnimeCommand extends Command {
         }
     }
 
+    private void setBookmarkAction(String actionString){
+        this.bookmarkAction = actionString;
+    }
+
+    private boolean setBookmarkIndex(String bookmarkIndexString) {
+        if (isInt(bookmarkIndexString)) {
+            bookmarkIndex = Integer.parseInt(bookmarkIndexString);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean setAnimeIndex(String animeIndexString) {
+        if (isInt(animeIndexString)) {
+            animeIndex = Integer.parseInt(animeIndexString);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean setBookmarkEpisode(String bookmarkEpisodeString) {
+        if (isInt(bookmarkEpisodeString)) {
+            bookmarkEpisode = Integer.parseInt(bookmarkEpisodeString);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /**
      * Checks if String is a parsable int.
