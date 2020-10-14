a1171154a02475667d9a908273491f29096cbc88
wamikamalik <wamikamalik@hotmail.com>
2020-10-14 22:16:18 +0400

    Add logging

--- src/main/java/seedu/duke/logic/parser/RouteParser.java
+++ src/main/java/seedu/duke/logic/parser/RouteParser.java
@@ -3,9 +3,17 @@ package seedu.duke.logic.parser;
 import seedu.duke.exceptions.CustomException;
 import seedu.duke.exceptions.ExceptionType;
 
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+
 public class RouteParser extends Parser {
 
     public static final String DELIMITER = "/to";
+    private static final Logger logger = Logger.getLogger(RouteParser.class.getName());
 
     public RouteParser(String message) {
         super(message);
@@ -14,15 +22,34 @@ public class RouteParser extends Parser {
     public String[] getLocations() throws CustomException {
         try {
             if (super.getUserInput().trim().length() == 0) {
+                logger.warning("User input is empty.");
                 throw new CustomException(ExceptionType.NO_LOCATIONS);
             } else if (super.getUserInput().contains(DELIMITER)) {
                 String[] components = super.splitCommands(2, DELIMITER);
                 return new String[]{components[0], components[1]};
             } else {
+                logger.warning("Delimiter /to is missing.");
                 throw new CustomException(ExceptionType.NO_ROUTE_DELIMITER);
             }
         } catch (StringIndexOutOfBoundsException error) {
+            logger.warning("Either of the locations is missing");
             throw new CustomException(ExceptionType.NO_LOCATIONS);
         }
     }
+
+    public static void initLogger() {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.SEVERE);
+        logger.addHandler(ch);
+        try {
+            FileHandler fh = new FileHandler("logger.log");
+            fh.setLevel(Level.WARNING);
+            logger.addHandler(fh);
+        } catch (IOException e) {
+            logger.severe("Error in creating logger file.");
+        }
+    }
+
 }
