2fd86601a2a50067f3f465c817ad606fbc5bc35a
slightlyharp <122560282@qq.com>
2020-10-14 19:10:28 +0800

    add timetable help message

--- src/main/java/HelpMessage.java
+++ src/main/java/HelpMessage.java
@@ -27,6 +27,12 @@ public class HelpMessage extends Ui {
             + "list          - prints the list of categories and respective list of links\n"
             + "back          - go back to bookmark main\n";
 
+    private static final String timetableCommand =  "Here are the timetable commands available:\n"
+            + "add class /MODULE CODE /ONLINE or OFFLINE\n"
+            + "/ZOOM LINK or VENUE /DAYS at TIME /NUMBER OF WEEKS                - add a class \n"
+            + "add activity /ONLINE or OFFLINE /ZOOM LINK or VENUE /DATE at TIME - add an activity\n"
+            + "show schedule                                                     - display schedule";
+
 
 
 
@@ -58,6 +64,7 @@ public class HelpMessage extends Ui {
 
     //TODO: Fill this up after you're done
     public static void printTimetableHelp() {
+        System.out.println(timetableCommand);
     }
 
     //TODO: Fill this up after you're done
