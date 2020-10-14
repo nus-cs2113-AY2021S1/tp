3bcf9cac0558dcf6753e3c6717aab07f8e2bf51d
lhydl <dereklee9262@gmail.com>
2020-10-14 11:45:01 +0800

    Revert "Add assertion for Parser class"
    This reverts commit 010ebdaa9a007d1ee02abcf5cd948c7928875653.

--- src/main/java/seedu/modtracker/Parser.java
+++ src/main/java/seedu/modtracker/Parser.java
@@ -28,9 +28,6 @@ public class Parser {
         Ui ui = new Ui();
         String[] command = input.trim().split(" ");
 
-        assert input != null;
-        assert name != null;
-
         switch (command[0].toLowerCase()) {
         case COMMAND_ADDMOD:
             modList.addMod(input, toPrint, storage);
