ee5c0888044fcf97b9302d3dfeafa27316af107e
LAPTOP-LK4JO5OO\65837 <varsha.ms30@gmail.com>
2020-10-14 13:37:23 +0800

    Junit test update

--- src/test/java/seedu/duke/event/EventTest.java
+++ src/test/java/seedu/duke/event/EventTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class EventTest {
     Event event = new Event("PR meeting", "2000-06-30", "8pm");
@@ -21,8 +22,8 @@ class EventTest {
         String expected1 = "Got it. I've added this Event:\n"
                 + "Event Name: PR meeting"
                 + "\nDate: Jun 30 2000"
-                + "\nTime: 8pm\n"+
-                "\nNow you have 1 event in the list.\n";
+                + "\nTime: 8pm\n"
+                + "\nNow you have 1 event in the list.\n";
         assertEquals(expected1,EventList.addEvent(event));
 
         String expected3 = "Here are the current events in your list:\n"
@@ -34,8 +35,8 @@ class EventTest {
         String expected2 = "I'll remove this Event:\n"
                 + "Event Name: PR meeting"
                 + "\nDate: Jun 30 2000"
-                + "\nTime: 8pm\n"+
-                "\nNow you have 0 event in the list.";
+                + "\nTime: 8pm\n"
+                + "\nNow you have 0 event in the list.";
         assertEquals(expected2,EventList.deleteEvent(0));
     }
 
