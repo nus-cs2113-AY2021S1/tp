af287a3cb575eba0cb0cfba88e1bc149d3e9c762
prachi.goenka@u.nus.edu <prachi.goenka@u.nus.edu>
2020-10-15 08:30:21 +0800

    Change to spaces in import lines for pinCommandTest

--- src/test/java/seedu/duke/command/PinCommandTest.java
+++ src/test/java/seedu/duke/command/PinCommandTest.java
@@ -2,8 +2,10 @@ package seedu.duke.command;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import seedu.duke.data.notebook.Note;
 import seedu.duke.data.notebook.Notebook;
 
