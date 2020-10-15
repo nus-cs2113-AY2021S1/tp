7a1f057d0724266f4f1aae2a64810e7bf06cb4e5
Joseph <e0406453@u.nus.edu>
2020-10-15 17:21:10 +0800

    Fix MenuParserTest errors.

--- src/test/java/seedu/eduke8/parser/MenuParserTest.java
+++ src/test/java/seedu/eduke8/parser/MenuParserTest.java
@@ -3,8 +3,17 @@ package seedu.eduke8.parser;
 import org.junit.jupiter.api.Test;
 import seedu.eduke8.command.Command;
 import seedu.eduke8.command.IncorrectCommand;
+import seedu.eduke8.common.Displayable;
+import seedu.eduke8.hint.Hint;
+import seedu.eduke8.option.OptionList;
+import seedu.eduke8.question.Question;
+import seedu.eduke8.question.QuestionList;
+import seedu.eduke8.topic.Topic;
 import seedu.eduke8.topic.TopicList;
 import seedu.eduke8.ui.Ui;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,7 +21,16 @@ public class MenuParserTest {
     @Test
     public void menuParser_wrongStringInput_expectIncorrectCommand() {
         Ui ui = new Ui();
-        TopicList topicList = new TopicList(null);
+        Hint hint = new Hint("description");
+        OptionList optionList = new OptionList();
+        ArrayList<Displayable> questions = new ArrayList<>();
+        Question question = new Question("description", optionList, hint);
+        questions.add(question);
+        QuestionList questionList = new QuestionList(questions);
+        Topic topic = new Topic("description", questionList);
+        ArrayList<Displayable> topics = new ArrayList<>();
+        topics.add(topic);
+        TopicList topicList = new TopicList(topics);
         MenuParser menuParser = new MenuParser();
 
         try {
