41fd62e8de6938d3ed8bf0c4edc5ff2665978e33
Michael Dinata <michaeldinata@Michaels-MacBook-Pro.local>
2020-10-15 14:29:59 +0800

    Updated logger in addToWatchlist and Parser to be final

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -15,7 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Parser {
-    private static Logger LOGGER = Logger.getLogger(Command.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Command.class.getName());
 
     /**
      * Prints the main menu of the application
