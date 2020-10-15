242e0c9c1be9189fe43daf8f3df60bf7b1ea816f
LAPTOP-LK4JO5OO\65837 <varsha.ms30@gmail.com>
2020-10-15 18:48:51 +0800

    Updated help command

--- src/main/java/seedu/duke/others/CommandHelp.java
+++ src/main/java/seedu/duke/others/CommandHelp.java
@@ -6,8 +6,17 @@ import seedu.duke.backend.UserInput;
 public class CommandHelp extends Command {
     @Override
     public String execute() {
-        return "Commands: hr add, hr delete, hr list, events add, events delete, finance add, finance delete, "
-                + "help, bye";
+        return "Hello! Here is a list of commands you can try:\n"
+                + "\t1. Add members:  hr add /n NAME /p PHONE_NUMBER /e EMAIL /r ROLE\n"
+                + "\t2. Delete member: hr delete index\n"
+                + "\t3. View hr list: hr list\n"
+                + "\t4. Add events: event addEvent /n EVENT_NAME /d EVENT_DATE /t EVENT_TIME\n"
+                + "\t5. Delete event: event delEvent EVENT_INDEX\n"
+                + "\t6. View event list: event listEvent\n"
+                + "\t7. Add financial log: finance addLog ITEM_NAME ITEM_VALUE\n"
+                + "\t8. Delete financial log:  finance delLog ITEM_INDEX\n"
+                + "\t9. View financial summary: finance summary\n"
+                + "\t10. Exit the program: bye\n";
     }
 
     @Override
