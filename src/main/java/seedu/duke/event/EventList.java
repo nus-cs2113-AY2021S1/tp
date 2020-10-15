4a3926ad0f50d1db37f02a5a76c6e42e7f4dd1e8
LAPTOP-LK4JO5OO\65837 <varsha.ms30@gmail.com>
2020-10-15 17:27:03 +0800

    added logger to event code

--- src/main/java/seedu/duke/event/EventList.java
+++ src/main/java/seedu/duke/event/EventList.java
@@ -2,20 +2,23 @@ package seedu.duke.event;
 
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class EventList {
     public static ArrayList<Event> events = new ArrayList<>();
+    public static Logger logger = Logger.getGlobal();
 
     /**
      * Adds member to the arraylist.
      * @param event event to be added in the list.
      */
     public static String addEvent(Event event) {
+        logger.info("Adding event\n");
         String userOutput;
         events.add(event);
         userOutput = "Got it. I've added this Event:\n" + event.printEvent() + "\n"
                 + "Now you have " + events.size() + " event in the list.\n";
-
+        logger.info("Added event to list\n");
         return userOutput;
     }
 
@@ -24,12 +27,14 @@ public class EventList {
      * @param index index to be deleted from list
      */
     public static String deleteEvent(int index) {
+        logger.info("Deleting event\n");
         String userOutput;
         try {
             userOutput = "I'll remove this Event:\n";
             userOutput = userOutput.concat(events.get(index).printEvent() + "\n");
             events.remove(index);
             userOutput = userOutput.concat("Now you have " + events.size() + " event in the list.");
+            logger.info("Deleted test from list\n");
         } catch (IndexOutOfBoundsException e) {
             userOutput = "OOPS!!! The event does not exist.Please try our help command!\n";
         }
@@ -37,8 +42,10 @@ public class EventList {
     }
 
     public static String printEventList() {
+        logger.info("Initialising event list\n");
         String userOutput;
         if (events.size() == 0) {
+            logger.warning("Empty event list.\n");
             userOutput = ("Oops! The event list is empty!");
         } else {
             userOutput = "Here are the current events in your list:\n";
@@ -47,7 +54,9 @@ public class EventList {
                 userOutput = userOutput.concat(events.indexOf(event) + 1 + ".");
                 userOutput = userOutput.concat(event.printEvent());
             }
+            logger.info("Event List ready");
         }
+
         return userOutput;
     }
 
