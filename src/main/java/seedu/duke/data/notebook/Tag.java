f2806f41bb9abc68d62480655386423a5d06a6f1
chongjx <chongjx60@gmail.com>
2020-10-16 19:13:02 +0800

    Updated code to use JColor library

--- src/main/java/seedu/duke/data/notebook/Tag.java
+++ src/main/java/seedu/duke/data/notebook/Tag.java
@@ -12,36 +12,11 @@ public class Tag {
     public static final String COLOR_GREEN_STRING = "GREEN";
     public static final String COLOR_BLUE_STRING = "BLUE";
     public static final String COLOR_YELLOW_STRING = "YELLOW";
-    public static final String COLOR_PURPLE_STRING = "PURPLE";
+    public static final String COLOR_MAGENTA_STRING = "PURPLE";
     public static final String COLOR_CYAN_STRING = "CYAN";
     public static final String COLOR_WHITE_STRING = "WHITE";
 
-    /**
-     * Types of color.
-     */
-    public enum TagColor {
-        COLOR_WHITE("\u001B[97m"),
-        COLOR_RED("\u001B[31m"),
-        COLOR_GREEN("\u001B[32m"),
-        COLOR_YELLOW("\u001B[33m"),
-        COLOR_BLUE("\u001B[34m"),
-        COLOR_PURPLE("\u001B[35m"),
-        COLOR_CYAN("\u001B[36m"),
-        COLOR_RESET("\u001B[0m");
-
-        private String color;
-
-        TagColor(String color) {
-            this.color = color;
-        }
-
-        public String getColor() {
-            return color;
-        }
-    }
-
     private String tagName;
-    private TagColor tagColor;
     private Attribute tagAttribute;
 
     /**
@@ -65,49 +40,31 @@ public class Tag {
         this.tagName = tagName;
     }
 
-    /** Getter function for the color of the tag. */
-    public TagColor getTagColor() {
-        return tagColor;
-    }
-
-    /** Setter function for the color of the tag. */
-    public void setTagColor(TagColor tagColor) {
-        this.tagColor = tagColor;
-    }
-
     /** Setter function for the color of the tag. */
     public void setTagColor(String tagColor) {
         switch (tagColor.toUpperCase()) {
         case COLOR_RED_STRING:
-            this.tagColor = TagColor.COLOR_RED;
-            this.tagAttribute = Attribute.RED_TEXT();
+            this.tagAttribute = Attribute.BRIGHT_RED_TEXT();
             break;
         case COLOR_GREEN_STRING:
-            this.tagColor = TagColor.COLOR_GREEN;
-            this.tagAttribute = Attribute.GREEN_TEXT();
+            this.tagAttribute = Attribute.BRIGHT_GREEN_TEXT();
             break;
         case COLOR_BLUE_STRING:
-            this.tagColor = TagColor.COLOR_BLUE;
-            this.tagAttribute = Attribute.BLUE_TEXT();
+            this.tagAttribute = Attribute.BRIGHT_BLUE_TEXT();
             break;
         case COLOR_YELLOW_STRING:
-            this.tagColor = TagColor.COLOR_YELLOW;
-            this.tagAttribute = Attribute.YELLOW_TEXT();
+            this.tagAttribute = Attribute.BRIGHT_YELLOW_TEXT();
             break;
-        case COLOR_PURPLE_STRING:
-            this.tagColor = TagColor.COLOR_PURPLE;
-            this.tagAttribute = Attribute.MAGENTA_TEXT();
+        case COLOR_MAGENTA_STRING:
+            this.tagAttribute = Attribute.BRIGHT_MAGENTA_TEXT();
             break;
         case COLOR_CYAN_STRING:
-            this.tagColor = TagColor.COLOR_CYAN;
-            this.tagAttribute = Attribute.CYAN_TEXT();
+            this.tagAttribute = Attribute.BRIGHT_CYAN_TEXT();
             break;
         case COLOR_WHITE_STRING:
-            this.tagColor = TagColor.COLOR_WHITE;
-            this.tagAttribute = Attribute.WHITE_TEXT();
+            this.tagAttribute = Attribute.BRIGHT_WHITE_TEXT();
             break;
         default:
-            this.tagColor = TagColor.COLOR_RESET;
             this.tagAttribute = Attribute.CLEAR();
             break;
         }
@@ -129,6 +86,5 @@ public class Tag {
     @Override
     public String toString() {
         return colorize("[" + tagName + "]", tagAttribute);
-        //return tagColor.color + "[" + tagName + "]" + TagColor.COLOR_RESET.color;
     }
 }
