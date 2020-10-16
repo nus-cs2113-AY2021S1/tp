700fe4758d3fd4f7bac61e75cf922a9dec755ac2
lingsihui <e0426314@u.nus.edu>
2020-10-16 23:38:54 +0800

    Fix checkstyle error

--- src/main/java/StudyItLog.java
+++ src/main/java/StudyItLog.java
@@ -1,10 +1,16 @@
 import java.io.IOException;
-import java.util.logging.*;
+import java.util.logging.Logger;
+import java.util.logging.LogManager;
+import java.util.logging.Level;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+
 
 public class StudyItLog {
     public static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-    public static void setUpLogger(){
+    public static void setUpLogger() {
         LogManager.getLogManager().reset();
         logger.setLevel(Level.ALL);
 
