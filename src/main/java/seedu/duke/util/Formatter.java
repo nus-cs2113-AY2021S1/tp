5019dd80f10cf7618ec28d7a62d74c2342595923
chongjx <chongjx60@gmail.com>
2020-10-16 16:02:34 +0800

    Updated some functions to take in header

--- src/main/java/seedu/duke/util/Formatter.java
+++ src/main/java/seedu/duke/util/Formatter.java
@@ -14,36 +14,40 @@ public class Formatter {
     public static final String LS = System.lineSeparator();
     private static final String ROW_SPLIT = "-";
     private static final String COLUMN_SPLIT = "|";
+    private static final String COLUMN_START = "|| ";
+    private static final String COLUMN_END = " ||";
+
+    // Temporary value for the maximum number of characters
     private static final int MAX_CONTENT_LENGTH = 50;
 
-    public static String formatNotebook(Notebook notebook) {
+    public static String formatNotebook(String header, Notebook notebook) {
+        String formattedString = createsRowSplit() + LS;
+        return formattedString;
+    }
+
+    public static String formatNote(String header, Note note) {
         String formattedString = "";
         return formattedString;
     }
 
-    public static String formatNote(Note note) {
+    public static String formatTags(String header, ArrayList<Tag> tags) {
         String formattedString = "";
         return formattedString;
     }
 
-    public static String formatTags(ArrayList<Tag> tags) {
+    public static String formatTimetable(String header, Timetable timetable) {
         String formattedString = "";
         return formattedString;
     }
 
-    public static String formatTimetable(Timetable timetable) {
+    public static String formatEvent(String header, Event event) {
         String formattedString = "";
         return formattedString;
     }
 
-    public static String formatEvent(Event event) {
-        String formattedString = "";
-        return formattedString;
-    }
-
-    public static String formatString(String input) {
-        String formattedString = "";
-        return formattedString;
+    public static String formatString(String message) {
+        // may have to handle the message length if it exceeds MAX_LENGTH_CONTENT
+        return encloseTopAndBottom(encloseRow(message));
     }
 
     public static String formatString(String[] input, boolean hasHeader) {
@@ -51,9 +55,15 @@ public class Formatter {
         return formattedString;
     }
 
-    private static String createsHeader() {
-        String formattedString;
-        formattedString = ROW_SPLIT.repeat(MAX_CONTENT_LENGTH);
-        return formattedString;
+    private static String createsRowSplit() {
+        return ROW_SPLIT.repeat(MAX_CONTENT_LENGTH);
+    }
+
+    private static String encloseTopAndBottom(String message) {
+        return createsRowSplit() + LS + message + createsRowSplit() + LS;
+    }
+
+    private static String encloseRow(String message) {
+        return COLUMN_START + message + COLUMN_END;
     }
 }
