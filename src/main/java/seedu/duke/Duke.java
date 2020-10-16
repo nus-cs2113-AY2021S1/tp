cb98db8e9a3e0c7c77dcb75232742e013c8fa115
chongjx <chongjx60@gmail.com>
2020-10-16 18:01:55 +0800

    Refactored enable virtual terminal code

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,7 +1,7 @@
 package seedu.duke;
 
 import com.sun.jna.Function;
-import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.Wincon;
 import seedu.duke.command.Command;
 import seedu.duke.command.ExitCommand;
 import seedu.duke.data.notebook.Notebook;
@@ -12,18 +12,15 @@ import seedu.duke.ui.InterfaceManager;
 import seedu.duke.util.Formatter;
 import seedu.duke.util.Parser;
 
+import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinNT;
-import com.sun.jna.platform.win32.Wincon;
-import static com.sun.jna.platform.win32.Kernel32.INSTANCE;
-import static com.sun.jna.platform.win32.WinBase.INVALID_HANDLE_VALUE;
-import static com.sun.jna.platform.win32.Wincon.STD_OUTPUT_HANDLE;
 
 /**
  * Entry point of the NotUS application.
  */
 public class Duke {
 
-    private static final int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0200;
+    private static final int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;
 
     private InterfaceManager interfaceManager;
     private StorageManager storageManager;
@@ -99,38 +96,42 @@ public class Duke {
         exit();
     }
 
+    /**
+     * Calls the execute function of the command.
+     *
+     * @param command The command to be executed.
+     * @return String of the executed command.
+     */
     private String executeCommand(Command command) {
         command.setData(notebook, timetable, tagManager, storageManager);
         return command.execute();
     }
 
     /**
+     * Sets windows console mode to enable virtual terminal processing to allow printing of ansi escape code.
+     */
+    private static void enableVirtualTerminalProcessing() {
+        Function getStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");
+        WinDef.DWORD stdOutputHandle = new WinDef.DWORD(Wincon.STD_OUTPUT_HANDLE);
+        WinNT.HANDLE outputHandle = (WinNT.HANDLE) getStdHandleFunc.invoke(
+                WinNT.HANDLE.class, new Object[]{stdOutputHandle});
+
+        WinDef.DWORDByReference doubleWord = new WinDef.DWORDByReference(new WinDef.DWORD(0));
+        Function getConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");
+        getConsoleModeFunc.invoke(WinDef.BOOL.class, new Object[]{outputHandle, doubleWord});
+
+        WinDef.DWORD dwMode = doubleWord.getValue();
+        dwMode.setValue(dwMode.intValue() | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+        Function setConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");
+        setConsoleModeFunc.invoke(WinDef.BOOL.class, new Object[]{outputHandle, dwMode});
+    }
+
+    /**
      * Main entry-point for the application.
      */
     public static void main(String[] args) {
-        /*WinNT.HANDLE handle = INSTANCE.GetStdHandle(Wincon.STD_OUTPUT_HANDLE);
-
-        if (handle == INVALID_HANDLE_VALUE) {
-            System.out.println(42);
-        }
-
-        boolean result = INSTANCE.SetConsoleMode(handle, 4);*/
-
         if (System.getProperty("os.name").startsWith("Windows")) {
-            Function getStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");
-            WinDef.DWORD stdOutputHandle = new WinDef.DWORD(Wincon.STD_OUTPUT_HANDLE);
-            WinNT.HANDLE outputHandle = (WinNT.HANDLE) getStdHandleFunc.invoke(
-                    WinNT.HANDLE.class, new Object[]{stdOutputHandle});
-
-            WinDef.DWORDByReference doubleWord = new WinDef.DWORDByReference(new WinDef.DWORD(0));
-            Function getConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");
-            getConsoleModeFunc.invoke(WinDef.BOOL.class, new Object[]{outputHandle, doubleWord});
-
-            int enableVirtualTerminalProcessing = 4;
-            WinDef.DWORD dwMode = doubleWord.getValue();
-            dwMode.setValue(dwMode.intValue() | enableVirtualTerminalProcessing);
-            Function setConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");
-            setConsoleModeFunc.invoke(WinDef.BOOL.class, new Object[]{outputHandle, dwMode});
+            enableVirtualTerminalProcessing();
         }
         new Duke().run();
     }
