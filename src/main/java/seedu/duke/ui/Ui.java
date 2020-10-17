b75136e1c8ebacd5b72ec70487e161277ce20cf6
LAPTOP-POOFUI22\User <jun_xian@live.com>
2020-10-17 21:43:26 +0800

    Update view

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -2,6 +2,7 @@ package seedu.duke.ui;
 
 import seedu.duke.model.Stock;
 import seedu.duke.model.Transaction;
+import seedu.duke.model.TransactionType;
 import seedu.duke.parser.Parser;
 
 import java.util.ArrayList;
@@ -47,18 +48,24 @@ public class Ui {
         }
         for (int i = 0; i < stocks.size(); i++) {
             print((i + 1) + ". " + stocks.get(i).toString());
+            int totalStocksBought = 0;
             double totalCost = 0;
-            double totalAsset = stocks.get(i).getLatestPrice() * stocks.get(i).getTotalQuantity();
             for (Transaction t: stocks.get(i).getTransactions()) {
                 print("\t" + t.toString());
-                totalCost += t.getUnitPrice() * t.getQuantity();
+                if (t.getTransactionType() == TransactionType.BUY) {
+                    totalCost += t.getUnitPrice() * t.getQuantity();
+                    totalStocksBought += t.getQuantity();
+                }
             }
-            print("Total revenue = $" + Parser.parsePrice(totalAsset));
-            print("Total cost = $" + Parser.parsePrice(totalCost));
+
+            double totalAsset = stocks.get(i).getLatestPrice() * stocks.get(i).getTotalQuantity();
+            double weightedAverageCost = (totalCost/totalStocksBought) * stocks.get(i).getTotalQuantity();
+            print("Total holding asset = $" + Parser.parsePrice(totalAsset));
+            print("Total cost = $" + Parser.parsePrice(weightedAverageCost));
 
             if (totalAsset != 0) {
-                double earnings = Parser.parsePrice(totalAsset) - Parser.parsePrice(totalCost);
-                print("Total Profit/Loss = $" + earnings);
+                double earnings = Parser.parsePrice(totalAsset) - Parser.parsePrice(weightedAverageCost);
+                print("Total Profit/Loss = $" + Parser.parsePrice(earnings));
             } else {
                 print("Total Profit/Loss = error!");
             }
