f5166339ef0bb138fedda830fbcff6b0c4b2cb35
neojiaern <e0417959@u.nus.edu>
2020-10-18 22:44:08 +0800

    Modify ui method call in execute

--- src/main/java/commands/RemoveCommand.java
+++ src/main/java/commands/RemoveCommand.java
@@ -47,75 +47,84 @@ public class RemoveCommand extends Command {
     public void execute(Ui ui, Access access, Storage storage)
             throws IncorrectAccessLevelException, IOException {
         if (access.isAdminLevel()) {
-            removeModule(ui, access, storage);
+            String result = removeModule(access, storage);
+            ui.showToUser(result);
         } else if (access.isModuleLevel()) {
-            removeChapter(ui, access, storage);
+            String result = removeChapter(access, storage);
+            ui.showToUser(result);
         } else if (access.isChapterLevel()) {
-            removeCard(ui, access, storage);
+            String result = removeCard(access, storage);
+            ui.showToUser(result);
         } else {
             throw new IncorrectAccessLevelException("Remove command can only be called at admin, "
                     + "module and chapter level.\n");
         }
     }
 
-    private void removeModule(Ui ui, Access access, Storage storage) throws IOException {
+    private String removeModule(Access access, Storage storage) throws IOException {
         assert access.isAdminLevel() : "Not admin level";
+        StringBuilder result = new StringBuilder();
         try {
             ModuleList modules = access.getAdmin().getModules();
             ArrayList<Module> allModules = modules.getAllModules();
             Module module = allModules.get(removeIndex);
             File directory = new File(storage.getFilePath() + "/" + module.toString());
-            logger.log(Level.INFO, "Deleting module...");
+            //logger.log(Level.INFO, "Deleting module...");
             boolean isRemoved = storage.deleteDirectory(directory);
             if (!isRemoved) {
                 throw new IOException("There was a problem deleting module in directory.");
             }
-            String message = String.format(MESSAGE_SUCCESS_MODULE, module.toString());
+            result.append(String.format(MESSAGE_SUCCESS_MODULE, module.toString()));
             allModules.remove(removeIndex);
-            ui.showToUser(message + String.format(MESSAGE_REMAINING_MODULE, allModules.size()));
+            result.append(String.format(MESSAGE_REMAINING_MODULE, allModules.size()));
         } catch (IndexOutOfBoundsException e) {
-            ui.showToUser(MESSAGE_INVALID_INDEX_MODULE);
+            result.append(MESSAGE_INVALID_INDEX_MODULE);
         }
+        return result.toString();
     }
 
-    private void removeChapter(Ui ui, Access access, Storage storage) throws IOException {
+    private String removeChapter(Access access, Storage storage) throws IOException {
         assert access.isModuleLevel() : "Not module level";
+        StringBuilder result = new StringBuilder();
         try {
             ChapterList chapters = access.getModule().getChapters();
             ArrayList<Chapter> allChapters = chapters.getAllChapters();
             Chapter chapter = allChapters.get(removeIndex);
             File directory = new File(storage.getFilePath() + "/" + access.getModule()
                 + "/" + chapter.toString() + ".txt");
-            logger.log(Level.INFO, "Deleting chapter...");
+            //logger.log(Level.INFO, "Deleting chapter...");
             boolean isRemoved = storage.deleteDirectory(directory);
             if (!isRemoved) {
-                logger.log(Level.WARNING, "problem deleting chapter");
+                //logger.log(Level.WARNING, "problem deleting chapter");
                 throw new IOException("There was a problem deleting chapter in directory.");
             }
-            String message = String.format(MESSAGE_SUCCESS_CHAPTER, chapter.toString());
+            result.append(String.format(MESSAGE_SUCCESS_CHAPTER, chapter.toString()));
             allChapters.remove(removeIndex);
-            ui.showToUser(message + String.format(MESSAGE_REMAINING_CHAPTER, allChapters.size()));
-            logger.log(Level.INFO, "Chapter successfully deleted.");
+            result.append(String.format(MESSAGE_REMAINING_CHAPTER, allChapters.size()));
+            //logger.log(Level.INFO, "Chapter successfully deleted.");
         } catch (IndexOutOfBoundsException e) {
-            ui.showToUser(MESSAGE_INVALID_INDEX_CHAPTER);
+            result.append(MESSAGE_INVALID_INDEX_CHAPTER);
         }
+        return result.toString();
     }
 
-    private void removeCard(Ui ui, Access access, Storage storage) throws IOException {
+    private String removeCard(Access access, Storage storage) throws IOException {
         assert access.isChapterLevel() : "Not chapter level";
+        StringBuilder result = new StringBuilder();
         try {
             CardList cards = access.getChapter().getCards();
             ArrayList<Card> allCards = cards.getAllCards();
             Card card = allCards.get(removeIndex);
-            logger.log(Level.INFO, "Deleting flashcard...");
+            //logger.log(Level.INFO, "Deleting flashcard...");
             cards.removeCard(removeIndex);
-            ui.showToUser(MESSAGE_SUCCESS_FLASHCARD + card.toString() + "\n"
+            result.append(MESSAGE_SUCCESS_FLASHCARD + card.toString() + "\n"
                     + String.format(MESSAGE_REMAINING_FLASHCARD, cards.getCardCount()));
             storage.saveCards(cards, access.getModule().getModuleName(), access.getChapter().getChapterName());
-            logger.log(Level.INFO, "Flashcard successfully deleted.");
+            //logger.log(Level.INFO, "Flashcard successfully deleted.");
         } catch (IndexOutOfBoundsException e) {
-            ui.showToUser(MESSAGE_INVALID_INDEX_FLASHCARD);
+            result.append(MESSAGE_INVALID_INDEX_FLASHCARD);
         }
+        return result.toString();
     }
 
     @Override
