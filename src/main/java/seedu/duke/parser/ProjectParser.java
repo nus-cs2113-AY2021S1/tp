01226a00b45b77684fe185eac132d388e2228a44
e0426051 <e0426051@u.nus.edu>
2020-10-18 13:34:17 -0700

    Fi

--- src/main/java/seedu/duke/parser/ProjectParser.java
+++ src/main/java/seedu/duke/parser/ProjectParser.java
@@ -4,7 +4,11 @@ import seedu.duke.exception.DukeException;
 
 import java.util.Hashtable;
 
-import static seedu.duke.command.CommandSummary.*;
+import static seedu.duke.command.CommandSummary.CREATE;
+import static seedu.duke.command.CommandSummary.DESCRIPTION;
+import static seedu.duke.command.CommandSummary.DURATION;
+import static seedu.duke.command.CommandSummary.SPRINT_DURATION;
+import static seedu.duke.command.CommandSummary.TITLE;
 
 public class ProjectParser implements ExceptionsParser {
     @Override
