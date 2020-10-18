fb1fc1ddf472801166fdc70154cbe75bf0e971a7
neojiaern <e0417959@u.nus.edu>
2020-10-18 22:18:30 +0800

    Modify ui method call in execute

--- src/main/java/commands/ListCommand.java
+++ src/main/java/commands/ListCommand.java
@@ -13,6 +13,10 @@ import ui.Ui;
 
 import java.util.ArrayList;
 
+import static common.Messages.CARD;
+import static common.Messages.CHAPTER;
+import static common.Messages.MODULE;
+
 public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
@@ -20,41 +24,85 @@ public class ListCommand extends Command {
             + ": Shows a list of modules / chapters / flashcards available. \n"
             + "Example: " + COMMAND_WORD + "\n";
 
+    public static final String MESSAGE_EXIST = "Here are the %s(s) in your list:\n";
+    public static final String MESSAGE_DOES_NOT_EXIST = "There are no %s(s) in your list.";
 
     @Override
     public void execute(Ui ui, Access access, Storage storage) throws IncorrectAccessLevelException {
         if (access.isChapterLevel()) {
-            listCards(ui, access);
+            String result = listCards(access);
+            ui.showToUser(result);
         } else if (access.isModuleLevel()) {
-            listChapters(ui, access);
+            String result = listChapters(access);
+            ui.showToUser(result);
         } else if (access.isAdminLevel()) {
-            listModules(ui, access);
+            String result = listModules(access);
+            ui.showToUser(result);
         } else {
             throw new IncorrectAccessLevelException("List command can only be called at admin, "
                     + "module and chapter level.\n");
         }
     }
 
-    private void listCards(Ui ui, Access access) {
+    private String listCards(Access access) {
         assert access.isChapterLevel() : "Not chapter level";
         CardList cards = access.getChapter().getCards();
         ArrayList<Card> allCards = cards.getAllCards();
         int cardCount = cards.getCardCount();
-        ui.showCardList(allCards, cardCount);
+        StringBuilder result = new StringBuilder();
+        if (cardCount == 0) {
+            result.append(String.format(MESSAGE_DOES_NOT_EXIST, CARD));
+            return result.toString();
+        }
+        result.append(String.format(MESSAGE_EXIST, CARD));
+        for (Card c : allCards) {
+            if (allCards.indexOf(c) == cardCount - 1) {
+                result.append(allCards.indexOf(c) + 1).append(".").append(c);
+            } else {
+                result.append(allCards.indexOf(c) + 1).append(".").append(c).append("\n");
+            }
+        }
+        return result.toString();
     }
 
-    private void listModules(Ui ui, Access access) {
+    private String listModules(Access access) {
         ModuleList modules = access.getAdmin().getModules();
         ArrayList<Module> allModules = modules.getAllModules();
-        int chapterCount = modules.getModuleCount();
-        ui.showModuleList(allModules, chapterCount);
+        int moduleCount = modules.getModuleCount();
+        StringBuilder result = new StringBuilder();
+        if (moduleCount == 0) {
+            result.append(String.format(MESSAGE_DOES_NOT_EXIST, MODULE));
+            return result.toString();
+        }
+        result.append(String.format(MESSAGE_EXIST, MODULE));
+        for (Module m : allModules) {
+            if (allModules.indexOf(m) == moduleCount - 1) {
+                result.append(allModules.indexOf(m) + 1).append(".").append(m);
+            } else {
+                result.append(allModules.indexOf(m) + 1).append(".").append(m).append("\n");
+            }
+        }
+        return result.toString();
     }
 
-    private void listChapters(Ui ui, Access access) {
+    private String listChapters(Access access) {
         ChapterList chapters = access.getModule().getChapters();
         ArrayList<Chapter> allChapters = chapters.getAllChapters();
         int chapterCount = chapters.getChapterCount();
-        ui.showChapterList(allChapters, chapterCount);
+        StringBuilder result = new StringBuilder();
+        if (chapterCount == 0) {
+            result.append(String.format(MESSAGE_DOES_NOT_EXIST, CHAPTER));
+            return result.toString();
+        }
+        result.append(String.format(MESSAGE_EXIST, CHAPTER));
+        for (Chapter c : allChapters) {
+            if (allChapters.indexOf(c) == chapterCount - 1) {
+                result.append(allChapters.indexOf(c) + 1).append(".").append(c);
+            } else {
+                result.append(allChapters.indexOf(c) + 1).append(".").append(c).append("\n");
+            }
+        }
+        return result.toString();
     }
 
 
