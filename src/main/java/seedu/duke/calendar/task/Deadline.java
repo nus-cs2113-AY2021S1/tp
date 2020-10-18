7b7e37e8e7c8d9b50a2af34cdaa91a999b22486d
LyuJiawen <gwendoline0812@gmail.com>
2020-10-18 19:28:38 +0800

    Update constructors.

--- src/main/java/seedu/duke/calendar/task/Deadline.java
+++ src/main/java/seedu/duke/calendar/task/Deadline.java
@@ -19,6 +19,7 @@ public class Deadline extends Task {
         super(description);
         this.by = by;
         this.taskType = "D";
+        this.isImportant = getIsImportant();
     }
 
     /**
