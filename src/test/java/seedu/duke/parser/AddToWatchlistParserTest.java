203fb96d631156eb2dc22e4d61fd04132058e36c
Michael Dinata <michaeldinata@Michaels-MacBook-Pro.local>
2020-10-18 22:51:29 +0800

    Modified addToWatchlistParserTest JUnit

--- src/test/java/seedu/duke/parser/AddToWatchlistParserTest.java
+++ src/test/java/seedu/duke/parser/AddToWatchlistParserTest.java
@@ -9,27 +9,13 @@ import seedu.duke.storage.StorageManager;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AddToWatchlistParserTest {
-    AnimeData animeData;
-    protected static int ANIME_DATA_SIZE;
     protected static final String INVALID_PARAMETERS_TEST1 = "";
     protected static final String INVALID_PARAMETERS_TEST2 = "-a";
     protected static final String INVALID_PARAMETERS_TEST3 = "-n 1";
     protected static final String INVALID_FIELD_TEST1 = "-a Gundam";
-
-    @BeforeEach
-    void setUp() {
-        ArrayList<Anime> testList = new ArrayList<>();
-        Anime testAnime1 = new Anime();
-        Anime testAnime2 = new Anime();
-        testList.add(testAnime1);
-        testList.add(testAnime2);
-        animeData = new AnimeData(testList);
-        ANIME_DATA_SIZE = animeData.getSize();
-    }
 
     @Test
     void parse_emptyDescription_throwsAniException() {
