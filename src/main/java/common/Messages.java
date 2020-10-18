f6df6112709440c04737023b60042ef84b374b4f
neojiaern <e0417959@u.nus.edu>
2020-10-18 21:07:56 +0800

    Add common package to store message class

---
+++ src/main/java/common/Messages.java
@@ -0,0 +1,6 @@
+package common;
+
+public class Messages {
+    public static final String MESSAGE_INVALID_ACCESS = "Sorry, you are currently at %1$s"
+            + ", please go to %2$s level first.";
+}
