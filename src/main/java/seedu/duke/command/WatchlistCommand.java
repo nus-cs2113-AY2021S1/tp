35a8794c6f0c9defabd426273c79b7db43da2c34
Ong De Zhi <39303087+OngDeZhi@users.noreply.github.com>
2020-10-18 17:14:00 +0800

    Update WatchlistCommand to support select and delete of watchlist

--- src/main/java/seedu/duke/command/WatchlistCommand.java
+++ src/main/java/seedu/duke/command/WatchlistCommand.java
@@ -14,66 +14,69 @@ import java.util.logging.Logger;
 public class WatchlistCommand extends Command {
     private static final String CREATE_OPTION = "-n";
     private static final String LIST_OPTION = "-l";
+    private static final String SELECT_OPTION = "-s";
+    private static final String DELETE_OPTION = "-d";
 
     private final String option;
     private String optionInformation;
     private static final Logger LOGGER = Logger.getLogger(WatchlistCommand.class.getName());
 
     public WatchlistCommand(String description) {
-        LOGGER.setLevel(Level.WARNING);
         String[] descriptionSplit = description.split(" ", 2);
         option = descriptionSplit[0];
         optionInformation = "";
         if (descriptionSplit.length == 2) {
             optionInformation = descriptionSplit[1];
         }
+
+        LOGGER.setLevel(Level.WARNING);
     }
 
     @Override
     public String execute(AnimeData animeData, StorageManager storageManager, User user) throws AniException {
         Workspace activeWorkspace = user.getActiveWorkspace();
 
-        String commandOutput = "";
-        assert option != null : "Command option cannot be null.";
         switch (option) {
         case CREATE_OPTION:
-            commandOutput = createWatchlist(storageManager, activeWorkspace);
-            break;
+            return createWatchlist(storageManager, activeWorkspace);
         case LIST_OPTION:
-            commandOutput = listAllWatchlist(activeWorkspace);
-            break;
+            return listAllWatchlist(activeWorkspace);
+        case SELECT_OPTION:
+            return selectWatchlist(activeWorkspace);
+        case DELETE_OPTION:
+            return deleteWatchlist(storageManager, activeWorkspace);
         default:
-            LOGGER.log(Level.WARNING, "Provided invalid option: " + option);
-            throw new AniException("Watchlist command only accepts the option: \"-n\" and \"-l\".");
+            LOGGER.log(Level.WARNING, "Invalid option received: \"" + option + "\".");
+            throw new AniException("Watchlist command only accepts the options: -n, -l, -s, and -d.");
         }
-
-        LOGGER.log(Level.INFO, System.lineSeparator() + commandOutput);
-        return commandOutput;
     }
 
     private String createWatchlist(StorageManager storageManager, Workspace activeWorkspace) throws AniException {
         if (optionInformation.isBlank()) {
             LOGGER.log(Level.WARNING, "Watchlist name is empty.");
-            throw new AniException("Watchlist name cannot be empty.");
+            throw new AniException("Watchlist name cannot be empty!");
         }
 
-        Watchlist newWatchlist = new Watchlist(optionInformation);
+        Watchlist createdWatchlist = new Watchlist(optionInformation);
         ArrayList<Watchlist> watchlistList = activeWorkspace.getWatchlistList();
-        boolean isWatchlistNameUnique = !watchlistList.contains(newWatchlist);
+        boolean isWatchlistNameUnique = !watchlistList.contains(createdWatchlist);
         if (!isWatchlistNameUnique) {
-            LOGGER.log(Level.WARNING, optionInformation + " is already one of the watchlist name.");
-            throw new AniException("You already have a watchlist named \"" + optionInformation + "\".");
+            LOGGER.log(Level.WARNING, "Watchlist name \"" + optionInformation + "\" is not unique.");
+            throw new AniException("There is already a watchlist named \"" + optionInformation + "\".");
         }
 
-        watchlistList.add(newWatchlist);
+        watchlistList.add(createdWatchlist);
+        LOGGER.log(Level.INFO, "Watchlist \"" + createdWatchlist.getName() + "\" created successfully.");
+
         storageManager.saveWatchlistList(activeWorkspace.getName(), watchlistList);
-        return "Watchlist created successfully!";
+        return "Watchlist \"" + optionInformation + "\" has been created successfully!";
     }
 
     private String listAllWatchlist(Workspace activeWorkspace) {
         ArrayList<Watchlist> watchlistList = activeWorkspace.getWatchlistList();
         if (watchlistList.size() == 0) {
-            return "You have no watchlist to list.";
+            LOGGER.log(Level.INFO, "Attempts to list watchlist when there is none.");
+            return "Uhh.. You have no watchlist to list..";
         }
 
         StringBuilder sbWatchlistList = new StringBuilder();
@@ -87,7 +90,72 @@ public class WatchlistCommand extends Command {
         }
 
         String watchlistListString = sbWatchlistList.toString();
-        assert !(watchlistListString.isBlank()) : "The String that list watchlist(s) cannot be empty.";
+        assert !(watchlistListString.isBlank()) : "The string listing watchlist(s) cannot be empty.";
         return watchlistListString;
+    }
+
+    private String selectWatchlist(Workspace activeWorkspace) throws AniException {
+        ArrayList<Watchlist> watchlistList = activeWorkspace.getWatchlistList();
+        int listSize = watchlistList.size();
+        if (listSize == 0) {
+            LOGGER.log(Level.INFO, "Attempts to select from an empty watchlist list.");
+            return "Uhh.. You have no watchlist to select..";
+        }
+
+        int selectIndex = parseInteger(optionInformation);
+        if (selectIndex < 0 || selectIndex >= listSize) {
+            LOGGER.log(Level.WARNING, "Select index specified is out of range.");
+            throw new AniException("\"" + (selectIndex + 1) + "\" is not a valid watchlist index.");
+        }
+
+        Watchlist selectedWatchlist = watchlistList.get(selectIndex);
+        Watchlist activeWatchlist = activeWorkspace.getActiveWatchlist();
+        if (selectedWatchlist.equals(activeWatchlist)) {
+            LOGGER.log(Level.INFO, "Selected watchlist is currently the active watchlist.");
+            throw new AniException("Uhh.. Current active watchlist is \"" + selectedWatchlist.getName() + "\"..");
+        }
+
+        activeWorkspace.setActiveWatchlist(selectedWatchlist);
+        LOGGER.log(Level.INFO, "Watchlist at index \"" + selectIndex + "\" selected successfully.");
+        return "\"" + selectedWatchlist.getName() + "\" is now your active watchlist.";
+    }
+
+    private String deleteWatchlist(StorageManager storageManager, Workspace activeWorkspace) throws AniException {
+        ArrayList<Watchlist> watchlistList = activeWorkspace.getWatchlistList();
+        int listSize = watchlistList.size();
+        if (listSize == 0) {
+            LOGGER.log(Level.INFO, "Attempts to delete from an empty watchlist list.");
+            return "Uhh.. You have no watchlist to delete..";
+        }
+
+        int deleteIndex = parseInteger(optionInformation);
+        if (deleteIndex < 0 || deleteIndex >= watchlistList.size()) {
+            LOGGER.log(Level.WARNING, "Delete index specified is out of range.");
+            throw new AniException("\"" + (deleteIndex + 1) + "\" is not a valid watchlist index.");
+        }
+
+        Watchlist deletedWatchlist = watchlistList.get(deleteIndex);
+        Watchlist activeWatchlist = activeWorkspace.getActiveWatchlist();
+        watchlistList.remove(deleteIndex);
+        LOGGER.log(Level.INFO, "Watchlist at index \"" + deleteIndex + "\" deleted successfully.");
+        String commandOutput = "Watchlist \"" + deletedWatchlist.getName() + "\" has been deleted successfully!";
+
+        if (deletedWatchlist.equals(activeWatchlist)) {
+            activeWorkspace.setActiveWatchlist(watchlistList.get(0));
+            commandOutput += System.lineSeparator();
+            commandOutput += "Changed active watchlist to: \"" + activeWorkspace.getActiveWatchlistName() + "\".";
+        }
+
+        storageManager.saveWatchlistList(activeWorkspace.getName(), watchlistList);
+        return commandOutput;
+    }
+
+    private int parseInteger(String optionInformation) throws AniException {
+        try {
+            return Integer.parseInt(optionInformation) - 1;
+        } catch (NumberFormatException exception) {
+            LOGGER.log(Level.WARNING, "Failed to parse \"" + optionInformation + "\" into a integer.");
+            throw new AniException("The watchlist index has to be a number.");
+        }
     }
 }
