d140c5e56f87ec9bc7d5d3a4f9f54af9f39c888f
czlin98 <czlin98@gmail.com>
2020-10-18 11:45:39 +0800

    Fix line lengths in messages again

--- src/main/java/seedu/messages/Messages.java
+++ src/main/java/seedu/messages/Messages.java
@@ -8,6 +8,10 @@ import static seedu.font.Colors.ANSI_YELLOW;
 
 public class Messages {
     public static final String LS = System.lineSeparator();
+    public static final String HELP_BORDER = " =================================================" +
+            "================================================= ";
+    public static final String HELP_BORDER_SPACE = "||                                                " +
+            "                                                ||";
 
     public static final String INVALID_PRIORITY = "Invalid priority!" + LS;
     public static final String INVALID_COMMAND = "Invalid command!" + LS;
@@ -26,38 +30,52 @@ public class Messages {
             + "||         " + ANSI_CYAN + "/_/   /_/\\__,_/_/ |_/\\____//____/  " + ANSI_RESET
             + ANSI_CYAN+ "v1.0" + ANSI_RESET + "        ||" + LS
             + "||                                                        ||";
-    public static final String HELP_MESSAGE =
-            " ================================================================================================== " + LS
-            + "||                                                                                                ||" + LS
-            + "||       " + "List of available commands:" + "                                                              ||" + LS
-            + "||       " + "Note:" + "                                                                                    ||" + LS
-            + "||       " + "1. Square bracket input is optional." + "                                                     ||" + LS
-            + "||       " + "- help: show list of available commands" + "                                                  ||" + LS
+
+
+    public static final String HELP_MESSAGE = HELP_BORDER + LS
+            + HELP_BORDER_SPACE + LS
+            + "||       " + "List of available commands:"
+            + "                                                              ||" + LS
+            + "||       " + "Note:"
+            + "                                                                                    ||" + LS
+            + "||       " + "1. Square bracket input is optional."
+            + "                                                     ||" + LS
+            + "||       " + "- help: show list of available commands"
+            + "                                                  ||" + LS
             + "||       " + "format: " + ANSI_PURPLE + "help" + ANSI_RESET
             + "                                                                             ||" + LS
-            + "||       " + "- add: add a task" + "                                                                        ||" + LS
+            + "||       " + "- add: add a task"
+            + "                                                                        ||" + LS
             + "||       " + "format: " + ANSI_PURPLE
-            + "add DESCRIPTION [d/dd-MM-yyyy] [st/HHmm] [et/HHmm] [p/NUMBER]" + ANSI_RESET + "                    ||" + LS
-            + "||       " + "- edit: edit a task" + "                                                                      ||" + LS
+            + "add DESCRIPTION [d/dd-MM-yyyy] [st/HHmm] [et/HHmm] [p/NUMBER]"
+            + ANSI_RESET + "                    ||" + LS
+            + "||       " + "- edit: edit a task"
+            + "                                                                      ||" + LS
             + "||       " + "format: " + ANSI_PURPLE
-            + "edit INDEX [des/DESCRIPTION] [d/dd-MM-yyyy] [st/HHmm] [et/HHmm] [p/NUMBER]" + ANSI_RESET + "       ||" + LS
-            + "||       " + "- list: show list of tasks" + "                                                               ||" + LS
+            + "edit INDEX [des/DESCRIPTION] [d/dd-MM-yyyy] [st/HHmm] [et/HHmm] [p/NUMBER]"
+            + ANSI_RESET + "       ||" + LS
+            + "||       " + "- list: show list of tasks"
+            + "                                                               ||" + LS
             + "||       " + "format: " + ANSI_PURPLE + "list" + ANSI_RESET
             + "                                                                             ||" + LS
-            + "||       " + "- search: find task(s) with keyword in description" + "                                       ||" + LS
+            + "||       " + "- search: find task(s) with keyword in description"
+            + "                                       ||" + LS
             + "||       " + "format: " + ANSI_PURPLE + "search KEYWORD(S)" + ANSI_RESET
             + "                                                                ||" + LS
-            + "||       " + "- delete remove particular task by index" + "                                                 ||" + LS
+            + "||       " + "- delete remove particular task by index"
+            + "                                                 ||" + LS
             + "||       " + "format: " + ANSI_PURPLE + "delete INDEX" + ANSI_RESET
             + "                                                                     ||" + LS
-            + "||       " + "- clear: remove all tasks" + "                                                                ||" + LS
+            + "||       " + "- clear: remove all tasks"
+            + "                                                                ||" + LS
             + "||       " + "format: " + ANSI_PURPLE + "clear" + ANSI_RESET
             + "                                                                            ||" + LS
-            + "||       " + "- bye: exit the program" + "                                                                  ||" + LS
+            + "||       " + "- bye: exit the program"
+            + "                                                                  ||" + LS
             + "||       " + "format: " + ANSI_PURPLE + "bye" + ANSI_RESET
             + "                                                                              ||" + LS
-            + "||                                                                                                ||" + LS
-            + " ================================================================================================== " + LS;
+            + HELP_BORDER_SPACE + LS
+            + HELP_BORDER + LS;
 
     public static final String BYE_MESSAGE = "Bye! See you again!";
     public static final String LIST_MESSAGE = "Here is your list of tasks:";
