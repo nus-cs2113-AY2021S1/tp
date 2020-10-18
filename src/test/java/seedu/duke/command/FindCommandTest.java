3ee70a6236881b67c52e8f762284afe83120425e
hong ming <imhongminggg@gmail.com>
2020-10-18 16:46:09 +0800

    Add JUnit test for Find Command

---
+++ src/test/java/seedu/duke/command/FindCommandTest.java
@@ -0,0 +1,35 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.DukeException;
+import seedu.duke.Storage;
+import seedu.duke.calendar.CalendarList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindCommandTest {
+
+    @Test
+    void findCommand_emptyKeyword_exceptionThrown() {
+        FindCommand findCommand = new FindCommand("/f");
+        CalendarList calendarList = new CalendarList();
+        Storage storage = null;
+        try {
+            findCommand.execute(calendarList, storage);
+        } catch (Exception e) {
+            assertEquals("keyword not found", e.getMessage());
+        }
+    }
+
+    @Test
+    void findCommand_noSuchKeyword_exceptionThrown() {
+        FindCommand findCommand = new FindCommand("/feruidfmkj");
+        CalendarList calendarList = new CalendarList();
+        Storage storage = null;
+        try {
+            findCommand.execute(calendarList, storage);
+        } catch (Exception e) {
+            assertEquals("keyword not found", e.getMessage());
+        }
+    }
+}
