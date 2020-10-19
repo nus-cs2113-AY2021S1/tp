035cc9e8773b8541bf3e9f70d96a271cf7d52121
Johnson-Yee <e0494363@u.nus.edu>
2020-10-19 23:32:54 +0800

    Addition of one more exception MISSING_BUS_CODE: to remind user to key in bus code

--- src/main/java/seedu/duke/exceptions/ExceptionType.java
+++ src/main/java/seedu/duke/exceptions/ExceptionType.java
@@ -5,6 +5,7 @@ public enum ExceptionType {
     INVALID_BUS("Sorry,there is no such bus in our database!"),
     NO_ROUTE_DELIMITER("Oops! You are missing the delimiter."),
     NO_LOCATIONS("Oh dear! I don't have all locations."),
+    MISSING_BUS_CODE("Oh no! You seem to have missed typing in the bus code!"),
     SAME_LOCATIONS("You don't need a bus to get there...You are right there!"),
     INVALID_BUS_STOP("Oh no! I do not understand which bus stop you are looking for");
 
