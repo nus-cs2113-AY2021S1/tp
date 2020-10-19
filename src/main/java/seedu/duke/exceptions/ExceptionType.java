b1b793458bb959de87fad5cac3f9c474204dc30d
mrwsy1 <e0406495@u.nus.edu>
2020-10-20 00:25:13 +0800

    Add invalid faculty exception

--- src/main/java/seedu/duke/exceptions/ExceptionType.java
+++ src/main/java/seedu/duke/exceptions/ExceptionType.java
@@ -6,7 +6,8 @@ public enum ExceptionType {
     NO_ROUTE_DELIMITER("Oops! You are missing the delimiter."),
     NO_LOCATIONS("Oh dear! I don't have all locations."),
     SAME_LOCATIONS("You don't need a bus to get there...You are right there!"),
-    INVALID_BUS_STOP("Oh no! I do not understand which bus stop you are looking for");
+    INVALID_BUS_STOP("Oh no! I do not understand which bus stop you are looking for"),
+    INVALID_FACULTY("Oh no! Please enter a faculty.");
 
     private String message;
 
