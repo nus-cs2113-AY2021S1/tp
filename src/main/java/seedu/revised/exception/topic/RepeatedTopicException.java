f1496a14e670b12e80c11b4251cd0d067d84bb33
DESKTOP-97301AT\jiaji <jiajia350@gmail.com>
2020-10-20 23:34:23 +0800

    java checkstyle

--- src/main/java/seedu/revised/exception/topic/RepeatedTopicException.java
+++ src/main/java/seedu/revised/exception/topic/RepeatedTopicException.java
@@ -1,6 +1,6 @@
 package seedu.revised.exception.topic;
 
-public class RepeatedTopicException extends Exception{
+public class RepeatedTopicException extends Exception {
     public RepeatedTopicException(String repeatedTopicError) {
         super(repeatedTopicError);
     }
