7dc209633ce5c639edf2e280f01fe3be58f42eb8
benardotang <benardotang@gmail.com>
2020-10-20 14:45:39 +0800

    Add tests for WatchCommand and UpdateTimeLimitCommand

---
+++ src/test/java/seedu/duke/commands/UpdateTimeLimitCommandTest.java
@@ -0,0 +1,30 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.classes.WatchTime;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UpdateTimeLimitCommandTest {
+
+    public static UpdateTimeLimitCommand updateObject;
+
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void main() {
+        WatchTime limitation = new WatchTime(java.time.LocalDate.of(2020,10,19),60,120);
+        assertEquals(WatchTime.getdailyWatchLimit(), 120);
+        ArrayList<String> inputExample = new ArrayList<>(Arrays.asList("updatetimelimit", "420"));
+        updateObject = new UpdateTimeLimitCommand("updatetimelimit", inputExample);
+    }
+
+
+    @Test
+    void processCommand() {
+        updateObject.processCommand();
+        assertEquals(WatchTime.getdailyWatchLimit(), 420);
+    }
+}
