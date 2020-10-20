0b6dba36312b3f327ed897e23271558cd735625a
DESKTOP-OV9KQRO\User <E0389113@u.nus.edu>
2020-10-20 19:27:04 +0800

    Add JunitTest Files have been tested to be able to load all three datat types for now

--- src/test/java/seedu/duke/storage/StorageTest.java
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -43,6 +43,32 @@ class StorageTest {
                         + "3. [P][✕] others" + System.lineSeparator()
                         + "_________________________________" + System.lineSeparator(),
                 outputStreamCaptor.toString());
+
+        outputStreamCaptor.reset();
+
+        inputString = "zoom";
+        listCommand = ListCommand.parse(inputString);
+        listCommand.execute(data, ui, store);
+
+        assertEquals("Here is a list of your Zoom events:" + System.lineSeparator()
+                        + "1. [Z][✕] math, Link: www.zoom.com/blah" + System.lineSeparator()
+                        + "   Repeated daily for 4 times." + System.lineSeparator()
+                        + "2. [Z][✓] computing, Link: www.zoom.com/hello" + System.lineSeparator()
+                        + "_________________________________" + System.lineSeparator(),
+                outputStreamCaptor.toString());
+
+        outputStreamCaptor.reset();
+
+        inputString = "timetable";
+        listCommand = ListCommand.parse(inputString);
+        listCommand.execute(data, ui, store);
+
+        assertEquals("Here is a list of your Timetable events:" + System.lineSeparator()
+                        + "1. [T][✕] math, Location: S17 on 2010-01-01, 12:00" + System.lineSeparator()
+                        + "   Repeated monthly for 4 times." + System.lineSeparator()
+                        + "2. [T][✓] computing, Location: COM2 on 2010-01-01, 12:00" + System.lineSeparator()
+                        + "_________________________________" + System.lineSeparator(),
+                outputStreamCaptor.toString());
     }
 
 }
