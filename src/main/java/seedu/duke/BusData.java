32a6f75b79327094db1a9757cf0070c676cc7170
Johnson-Yee <e0494363@u.nus.edu>
2020-10-21 03:18:50 +0800

    Implement search frequency increment to record search frequency

--- src/main/java/seedu/duke/BusData.java
+++ src/main/java/seedu/duke/BusData.java
@@ -13,6 +13,12 @@ public class BusData {
 
     public static ArrayList<Bus> possibleBuses(String startingLoc, String destination) {
         ArrayList<Bus> busOptions = new ArrayList<>();
+        if (BusStops.isValidBusStop(startingLoc)) {
+            BusStops.findBusStop(startingLoc).incrementSearchCount();
+        }
+        if (BusStops.isValidBusStop(destination)) {
+            BusStops.findBusStop(destination).incrementSearchCount();
+        }
         for (Bus bus : buses) {
             ArrayList<BusStops> route = bus.getPossibleRoute(startingLoc, destination);
             if (route != null) {
@@ -23,7 +29,7 @@ public class BusData {
     }
 
     public static Bus selectBus(String busCode) throws NullPointerException {
-        for (Bus bus: buses) {
+        for (Bus bus : buses) {
             if (bus.getBusNumber().equals(busCode.toUpperCase())) {
                 return bus;
             }
@@ -33,9 +39,9 @@ public class BusData {
 
     public static ArrayList<Bus> busAtStop(String busStop) {
         ArrayList<Bus> busList = new ArrayList<>();
-        for (Bus bus: buses) {
+        for (Bus bus : buses) {
             ArrayList<String> stopNames = bus.getStopNames();
-            for (String name: stopNames) {
+            for (String name : stopNames) {
                 if (name.equalsIgnoreCase(busStop)) {
                     busList.add(bus);
                     break;
@@ -49,9 +55,9 @@ public class BusData {
         return buses;
     }
 
-    public static ArrayList<Integer> getAllSearchCount(){
+    public static ArrayList<Integer> getAllSearchCount() {
         ArrayList<Integer> allSearchCount = new ArrayList<Integer>();
-        for (BusStops busStop: EnumSet.allOf(BusStops.class)){
+        for (BusStops busStop : EnumSet.allOf(BusStops.class)) {
             allSearchCount.add(busStop.getSearchCount());
         }
         return allSearchCount;
