f3602d6a6414efd32f1d6bbef87102098246d379
DESKTOP-7Q3MLGS\gunit <gunitmittal@gmail.com>
2020-10-21 00:42:04 +0530

    tests passed

--- src/test/java/seedu/duke/project/ProjectTest.java
+++ src/test/java/seedu/duke/project/ProjectTest.java
@@ -7,17 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ProjectTest {
 
-    @Test
-    void createProject_invalidDuration_returnsNumberFormatException() {
-        assertThrows(NumberFormatException.class, () -> new Project("SCRUMPptious",
-                "To deliver a cli interface for agile", 512, 30));
-    }
-
-    @Test
-    void createProject_invalidSprintLength_returnsNumberFormatException() {
-        assertThrows(NumberFormatException.class, () -> new Project("SCRUMPptious",
-                "To deliver a cli interface for agile", 90, 4));
-    }
 
     @Test
     void getProjectSprint_emptyArrayList_returnsIndexOutOfBoundsException() {
