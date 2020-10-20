18c2ef23dddf2ce65b1fbc562e9792b7c70b209c
chloesyy <chlyiyi@hotmail.com>
2020-10-20 15:55:24 +0800

    Add booktitlecomparator class to compare book titles regardless of case.

--- src/main/java/seedu/quotesify/book/BookTitleComparator.java
+++ src/main/java/seedu/quotesify/book/BookTitleComparator.java
@@ -1,2 +1,10 @@
-package seedu.quotesify.book;public class BookTitleComparator {
+package seedu.quotesify.book;
+
+import java.util.Comparator;
+
+public class BookTitleComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book b1, Book b2) {
+        return b1.getTitle().toLowerCase().compareTo(b2.getTitle().toLowerCase());
+    }
 }
