3845b0056b36f5b7d93b8e2a525ffbb6d042712d
QX-CHEN <c.qixing@yahoo.com.sg>
2020-10-20 12:45:56 +0800

    Fix code style

--- src/main/java/seedu/ui/DayStructure.java
+++ src/main/java/seedu/ui/DayStructure.java
@@ -11,13 +11,6 @@ public class DayStructure extends DisplayDateStructure {
 
     @Override
     protected void generateScreen(TaskMap tasks) {
-//        TaskMap tasksInDate = tasks.searchByDate(date);
-//
-//        // Display date
-//        String dateString = date.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"))
-//                + " " + date.getDayOfWeek().getDisplayName(WEEKDAY_TEXT_STYLE, LOCALE) + ":" + LS;
-
-
 
     }
 }
