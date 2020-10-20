18ffc98bda987b6ac25a83d3633b485c9fa37307
wamikamalik <wamikamalik@hotmail.com>
2020-10-20 08:57:59 +0400

    Apply similarity check to bus command

--- src/main/java/seedu/duke/logic/commands/BusCommand.java
+++ src/main/java/seedu/duke/logic/commands/BusCommand.java
@@ -4,8 +4,11 @@ import seedu.duke.Bus;
 import seedu.duke.BusData;
 import seedu.duke.BusStops;
 import seedu.duke.exceptions.CustomException;
+import seedu.duke.logic.SimilarityCheck;
+import seedu.duke.ui.Ui;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 
 import static seedu.duke.ui.Ui.printLine;
 
@@ -14,19 +17,38 @@ public class BusCommand extends Command {
     public String busStop;
 
     public BusCommand(String busStop) throws CustomException {
-        this.busStop = BusStops.formatName(busStop.trim());
+        ArrayList<String> possibleLocs = new ArrayList<>(similarLocations(busStop));
+        if(possibleLocs.isEmpty()) {
+            this.busStop = BusStops.formatName(busStop.trim());
+        } else {
+            Ui.printPossibleLocsMessage(possibleLocs);
+        }
     }
 
     @Override
     public void executeCommand() {
-        assert !busStop.isBlank() : "Bus stop is empty";
-        printLine();
-        ArrayList<Bus> busList = BusData.busAtStop(busStop);
-        System.out.print(busStop + ":  ");
-        for (Bus bus: busList) {
-            System.out.print(bus.getBusNumber() + "   ");
+        if(busStop!=null) {
+            assert !busStop.isBlank() : "Bus stop is empty";
+            printLine();
+            ArrayList<Bus> busList = BusData.busAtStop(busStop);
+            System.out.print(busStop + ":  ");
+            for (Bus bus : busList) {
+                System.out.print(bus.getBusNumber() + "   ");
+            }
+            System.out.println();
+            printLine();
         }
-        System.out.println();
-        printLine();
+    }
+
+    private ArrayList<String> similarLocations(String location) {
+        ArrayList<String> possibleLocs = new ArrayList<>();
+        ArrayList<String> routeNames = new ArrayList<>();
+        for (BusStops info: EnumSet.allOf(BusStops.class)) {
+            routeNames.add(info.getName().toLowerCase());
+        }
+        if(!routeNames.contains(location.trim().toLowerCase())) {
+            possibleLocs = SimilarityCheck.similarLoc(location);
+        }
+        return possibleLocs;
     }
 }
