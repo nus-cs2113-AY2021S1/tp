e745093a0181ca211716f3ed37db4cbfa70c04e3
ethan2212 <e0407555@u.nus.edu>
2020-10-22 03:16:38 +0800

    Create DeleteFavCommand class

---
+++ src/main/java/seedu/duke/logic/commands/favCommand/DeleteFavCommand.java
@@ -0,0 +1,11 @@
+package seedu.duke.logic.commands.favCommand;
+
+import seedu.duke.exceptions.CustomException;
+import seedu.duke.logic.commands.Command;
+
+public class DeleteFavCommand extends Command {
+    @Override
+    public void executeCommand() throws CustomException {
+
+    }
+}
