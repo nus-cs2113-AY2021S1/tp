174cae4c232e5c45acdf3db8c353374900265f69
ethan2212 <e0407555@u.nus.edu>
2020-10-22 03:16:12 +0800

    Create AddFavCommand class

---
+++ src/main/java/seedu/duke/logic/commands/favCommand/AddFavCommand.java
@@ -0,0 +1,14 @@
+package seedu.duke.logic.commands.favCommand;
+
+import seedu.duke.exceptions.CustomException;
+import seedu.duke.favorite.Fav;
+import seedu.duke.favorite.FavList;
+import seedu.duke.logic.commands.Command;
+
+public class AddFavCommand extends Command {
+    @Override
+    public void executeCommand() throws CustomException {
+//        Fav fav = new Fav("test", "tt");
+//        FavList.addFav(fav);
+    }
+}
