23c9a75719758aa799fb1de59f3e2b66234215d7
HengFuYuen <heng.fu.yuen@gmail.com>
2020-10-23 00:57:52 +0800

    Add JUnit tests for two Ui methods

--- src/test/java/seedu/dietbook/UiTest.java
+++ src/test/java/seedu/dietbook/UiTest.java
@@ -33,4 +33,54 @@ class UiTest {
         LocalDateTime dateTime = LocalDateTime.parse("2020-10-21T23:59:22");
         assertEquals("21 Oct 2020 2359",ui.stringDateTime(dateTime));
     }
+
+    @Test
+    void trimStringGetLength_nullInput_expectAssertionError() {
+        assertThrows(AssertionError.class, () -> ui.trimStringGetLength(null));
+    }
+
+    @Test
+    void trimStringGetLength_stringWithNoLeadingOrTrailingSpaces_returnsLengthFour() {
+        assertEquals(4, ui.trimStringGetLength("food"));
+    }
+
+    @Test
+    void trimStringGetLength_StringWithLeadingSpaces_returnsLengthFour() {
+        assertEquals(4, ui.trimStringGetLength("    food"));
+    }
+
+    @Test
+    void trimStringGetLength_StringWithTrailingSpaces_returnsLengthFour() {
+        assertEquals(4, ui.trimStringGetLength("food    "));
+    }
+
+    @Test
+    void trimStringGetLength_StringWithLeadingAndTrailingSpaces_returnsLengthFour() {
+        assertEquals(4, ui.trimStringGetLength("    food    "));
+    }
+
+    @Test
+    void trimString_nullInput_expectAssertionError() {
+        assertThrows(AssertionError.class, () -> ui.trimString(null));
+    }
+
+    @Test
+    void trimString_StringWithLeadingSpaces_returnsStringWithoutLeadingSpaces() {
+        assertEquals("food", ui.trimString("    food"));
+    }
+
+    @Test
+    void trimString_StringWithTrailingSpaces_returnsStringWithoutTrailingSpaces() {
+        assertEquals("food", ui.trimString("food    "));
+    }
+
+    @Test
+    void trimString_StringWithLeadingAndTrailingSpaces_returnsStringWithoutLeadingAndTrailingSpaces() {
+        assertEquals("food", ui.trimString("    food    "));
+    }
+
+    @Test
+    void trimString_StringWithNoLeadingAndTrailingSpaces_returnsString() {
+        assertEquals("food", ui.trimString("food"));
+    }
 }
