c910bde8579a6f0371e8b32de41bb785814f93e3
harris maung <harrismaung3@gmail.com>
2020-10-23 17:00:19 +0800

    Updates

--- src/main/java/fitr/command/RecommendCommand.java
+++ src/main/java/fitr/command/RecommendCommand.java
@@ -28,13 +28,21 @@ public class RecommendCommand extends Command {
         StandardExerciseList recommendList = recommender.recommend();
         int fitnessLevel = user.getFitnessLevel();
         for (int i = 0; i < 4; i++) {
+            StandardExercise standardExercise = recommendList.getExercise(i);
+            int calorieBurnt = (int) (standardExercise.getDuration().get(fitnessLevel)
+                    * standardExercise.getMet()
+                    * standardExercise.getSets().get(fitnessLevel)
+                    * user.getWeight())
+                    / 60;
             Ui.printCustomMessage(OPEN_SQUARE_BRACKET + (i + 1) + CLOSE_SQUARE_BRACKET
                     + EXERCISE_HEADER + recommendList.getExercise(i).getName()
                     + SPACE_FORMATTING + INTENSITY_CAL_HEADER
                     + recommendList.getExercise(i).getSets().get(fitnessLevel) + " sets of "
-                    + recommendList.getExercise(i).getDuration().get(fitnessLevel) + " minutes");
+                    + recommendList.getExercise(i).getDuration().get(fitnessLevel) + " minutes"
+                    + SPACE_FORMATTING + BURNT_CAL_HEADER + calorieBurnt);
         }
-        Ui.printCustomMessage("Will you be doing this workout?/n type y for yes and n for no");
+        Ui.printCustomMessage("Will you be doing this workout?\n"
+                + "type y for yes and n for no");
         String checker = Ui.read();
         if (checker.equals("y")) {
             Ui.printCustomMessage("The following Exercises has been added:");
