8639a70117dad6aa7e073a48780d9f45ff1e1a5f
DESKTOP-7Q3MLGS\gunit <gunitmittal@gmail.com>
2020-10-23 17:29:38 +0530

    refactored member class

---
+++ src/main/java/seedu/duke/command/member/DeleteMemberCommand.java
@@ -0,0 +1,29 @@
+package seedu.duke.command.member;
+
+import seedu.duke.model.member.Member;
+import seedu.duke.model.project.Project;
+import seedu.duke.model.project.ProjectList;
+import seedu.duke.ui.Ui;
+
+import java.util.Hashtable;
+
+public class DeleteMemberCommand extends MemberCommand {
+
+
+    public DeleteMemberCommand(Hashtable<String, String> parameters, ProjectList projectListManager) {
+        super(parameters, projectListManager);
+    }
+
+    public void execute() {
+        Project proj;
+        proj = projectListManager.getProject();
+        for (int i = 0; i < parameters.size(); i++) {
+            if (proj.getProjectMember().containMember(new Member(parameters.get(Integer.toString(i))))) {
+                proj.getProjectMember().removeMember(new Member(parameters.get(Integer.toString(i))));
+                Ui.showToUserLn(parameters.get(Integer.toString(i)) + " has been removed from the project.");
+            } else {
+                Ui.showToUserLn(parameters.get(Integer.toString(i)) + " is not associated with the project.");
+            }
+        }
+    }
+}
