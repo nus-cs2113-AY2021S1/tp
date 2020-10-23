e04c91b6f99b3072f724a55eea8df96ce01f4590
judowha <e0376925@u.nus.edu>
2020-10-23 17:13:02 +0800

    implement search function

---
+++ src/main/java/seedu/duke/commands/SearchCommand.java
@@ -0,0 +1,31 @@
+package seedu.duke.commands;
+
+
+import seedu.duke.utility.ShowList;
+import seedu.duke.utility.Ui;
+
+import java.util.ArrayList;
+
+
+public class SearchCommand extends Command{
+    ArrayList<String> inputs;
+
+
+    public SearchCommand(String description, ArrayList<String> inputs) throws NullPointerException {
+        super(description);
+        this.inputs = inputs;
+        if (inputs.size() != 2) {
+            throw new NullPointerException();
+        }
+    }
+
+    public void processCommand() {
+        String showName = inputs.get(1);
+        if (ShowList.getShowList().containsKey(showName)) {
+            Ui.printSearchSuccessful(showName);
+        }
+        else {
+            throw new NullPointerException();
+        }
+    }
+}
