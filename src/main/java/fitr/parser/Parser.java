abba640a1e31cf69b96abdc4b09596c6550f186a
dmbclub <dmb437026158@gmail.com>
2020-10-23 15:49:15 +0800

    Update Parser

--- src/main/java/fitr/parser/Parser.java
+++ src/main/java/fitr/parser/Parser.java
@@ -4,17 +4,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import fitr.command.AddFoodCommand;
+import fitr.command.ClearCommand;
 import fitr.command.Command;
 import fitr.command.InvalidCommand;
 import fitr.command.AddExerciseCommand;
 import fitr.command.ViewCommand;
 import fitr.command.EditProfileCommand;
+import fitr.command.RecommendCommand;
 import fitr.command.HelpCommand;
 import fitr.command.DeleteCommand;
 import fitr.command.ExitCommand;
 import fitr.command.AddGoalCommand;
-import fitr.command.RecommendCommand;
 import fitr.common.Commands;
+
+import static fitr.DateManager.getCurrentDate;
 
 /**
  * Parses the user input.
@@ -43,18 +46,20 @@ public class Parser {
             return new AddExerciseCommand(arguments);
         case Commands.COMMAND_VIEW:
             return new ViewCommand(arguments);
+        case Commands.COMMAND_RECOMMEND:
+            return new RecommendCommand();
         case Commands.COMMAND_EDIT_PROFILE:
             return new EditProfileCommand(arguments);
         case Commands.COMMAND_HELP:
             return new HelpCommand(arguments);
         case Commands.COMMAND_DELETE:
             return new DeleteCommand(arguments);
+        case Commands.COMMAND_CLEAR:
+            return new ClearCommand(arguments);
         case Commands.COMMAND_BYE:
             return new ExitCommand(arguments);
         case Commands.COMMAND_GOAL:
-            return new AddGoalCommand(arguments);
-        case Commands.COMMAND_RECOMMEND:
-            return new RecommendCommand();
+            return new AddGoalCommand(arguments, getCurrentDate());
         default:
             return new InvalidCommand(arguments);
         }
