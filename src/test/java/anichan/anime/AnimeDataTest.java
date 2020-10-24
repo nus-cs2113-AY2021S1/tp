e050a2bad9af5c10ac1d7cfd813170e3159ef07c
n3wsoldier <n3wsoldier@gmail.com>
2020-10-24 13:32:53 +0800

    Remove unused variable and import

--- src/test/java/anichan/anime/AnimeDataTest.java
+++ src/test/java/anichan/anime/AnimeDataTest.java
@@ -1,11 +1,8 @@
 package anichan.anime;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import anichan.exception.AniException;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
