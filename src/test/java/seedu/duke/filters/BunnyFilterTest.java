274aa7bb3ee741149dbbe7af0b88ab4a899a151f
hungvo0603 <hungvo1011@gmail.com>
2020-10-25 00:06:07 +0800

    Fix gradle issue

--- src/test/java/seedu/duke/filters/BunnyFilterTest.java
+++ src/test/java/seedu/duke/filters/BunnyFilterTest.java
@@ -18,10 +18,8 @@ class BunnyFilterTest {
         }
     }
 
-//    @Test
-//    void filterBunny_filterIdeaTerm_getOneResult() {
-//        FilterExecutor.executeFilterCommand("filter bunny i\\ fun");
-//
-//
-//    }
+    //@Test
+    //void filterBunny_filterIdeaTerm_getOneResult() {
+    //    FilterExecutor.executeFilterCommand("filter bunny i\\ fun");
+    //}
 }
