f25a2bdba8d346d55f8bdc8958d0faae929bfbdd
e0406981 <e0406981@u.nus.edu>
2020-10-26 02:03:16 +0800

    some tests for add and delete

---
+++ src/test/java/seedu/commands/DeleteTest.java
@@ -0,0 +1,27 @@
+package seedu.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.data.TaskMap;
+import seedu.exceptions.InvalidCommandException;
+import seedu.exceptions.InvalidTaskNumberException;
+import seedu.task.Task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteTest {
+
+
+    @Test
+    public void constructor_noRawInput_throws_InvalidCommandException() {
+        assertThrows(InvalidCommandException.class, () -> new Delete(""));
+    }
+
+    @Test
+    public void deleteCommand_invalidData(){
+        final String[] invalidInputs = { "", " ",  "&*^%*&^", "abc"};
+        for(int i=0; i< invalidInputs.length; i++) {
+            int finalI = i;
+            assertThrows(InvalidCommandException.class, () -> new Add("delete " + invalidInputs[finalI]));
+        }
+    }
+}
