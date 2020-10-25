2034ac8be3eb80039d5c567ef1e19be623d07cba
keke101 <zack_1997@hotmail.com>
2020-10-26 03:29:59 +0800

    Fix javadoc

--- src/main/java/seedu/duke/storage/JsonableArray.java
+++ src/main/java/seedu/duke/storage/JsonableArray.java
@@ -5,8 +5,8 @@ import com.github.cliftonlabs.json_simple.Jsonable;
 
 public interface JsonableArray extends Jsonable {
     /**
-     * Parse the given JsonArray and map the values to the current class
-     * @param jsonArr JsonArray that contains all values
+     * Parse the given JsonArray and map the values to the current class.
+     * @param jsonArr JsonArray that contains all values.
      */
     void fromJson(JsonArray jsonArr);
 }
