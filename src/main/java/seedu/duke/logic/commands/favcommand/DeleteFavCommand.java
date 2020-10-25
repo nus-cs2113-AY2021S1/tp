59cf1fe1f5b0be7af23f563748f5d6b240cc9d2a
Johnson-Yee <e0494363@u.nus.edu>
2020-10-26 03:16:31 +0800

    Error handling for empty index and out of range errors

--- src/main/java/seedu/duke/logic/commands/favcommand/DeleteFavCommand.java
+++ src/main/java/seedu/duke/logic/commands/favcommand/DeleteFavCommand.java
@@ -1,11 +1,29 @@
 package seedu.duke.logic.commands.favcommand;
 
 import seedu.duke.exceptions.CustomException;
+import seedu.duke.exceptions.ExceptionType;
+import seedu.duke.favorite.Fav;
+import seedu.duke.favorite.FavList;
 import seedu.duke.logic.commands.Command;
 
 public class DeleteFavCommand extends Command {
+    public int index;
+
+    public DeleteFavCommand(String index) throws CustomException {
+        if (index.isBlank()) {
+            throw new CustomException(ExceptionType.EMPTY_INDEX);
+        }
+        int indexNum = Integer.parseInt(index.trim());
+        this.index = indexNum;
+    }
+
     @Override
     public void executeCommand() throws CustomException {
-
+        int sizeOfArray = FavList.getSize();
+        if (index <= 0 || index > sizeOfArray) {
+            throw new CustomException(ExceptionType.INDEX_NOT_IN_RANGE);
+        } else {
+            FavList.deleteFav(index);
+        }
     }
 }
