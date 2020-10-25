0f493eae7dba32b0a39e419142ac98a1e724a02e
mrwsy1 <e0406495@u.nus.edu>
2020-10-26 00:07:54 +0800

    Update listfav and clearfav methods

--- src/main/java/seedu/duke/favorite/FavList.java
+++ src/main/java/seedu/duke/favorite/FavList.java
@@ -1,6 +1,12 @@
 package seedu.duke.favorite;
 
+import seedu.duke.exceptions.CustomException;
+import seedu.duke.exceptions.ExceptionType;
+
 import java.util.ArrayList;
+
+import static seedu.duke.ui.Ui.printClearFavMessage;
+import static seedu.duke.ui.Ui.printFavList;
 
 public class FavList {
     private static ArrayList<Fav> favList;
@@ -15,7 +21,16 @@ public class FavList {
     public static void deleteFav(int index) {
     }
 
-    public static void listFav() {
+    public static void listFav() throws CustomException {
+        if (favList.size() == 0) {
+            throw new CustomException(ExceptionType.EMPTY_FAVLIST);
+        }
+        printFavList(favList);
+    }
+
+    public static void clearFav() {
+        favList.clear();
+        printClearFavMessage();
     }
 
     public static ArrayList<Fav> getList() {
