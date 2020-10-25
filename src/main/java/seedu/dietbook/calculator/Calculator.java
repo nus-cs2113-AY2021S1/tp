4cf65bfc5c47a6fa709530a69b185bb3bd68d146
yuqiaoluolong <yuqiaoluolong@gmail.com>
2020-10-26 00:05:03 +0800

    Recommendation of daily calorie intake calculation is added

--- src/main/java/seedu/dietbook/calculator/Calculator.java
+++ src/main/java/seedu/dietbook/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package seedu.dietbook.calculator;
 
 import seedu.dietbook.food.Food;
+import seedu.dietbook.person.Gender;
+import seedu.dietbook.person.Person;
 
 import java.util.ArrayList;
 
@@ -63,5 +65,65 @@ public class Calculator {
      */
     public int calculateFat() {
         return totalFat;
+    }
+
+    /**
+     * Returns an int type variable containing the value of recommended daily calorie intake.
+     * It is calculated based on the gender, activity level, age, height, original weight,
+     * and targeted weight.
+     *
+     * @param person person whose recommended daily calorie intake are to return.
+     * @return the value of recommended daily calorie intake.
+     */
+    public int calculateRecomendation(Person person) {
+        double requirement = 0;
+        int recomendation;
+        double activityScore;
+        switch (person.getActivityLevel()) {
+        case NONE:
+            activityScore = 1;
+            break;
+        case LOW:
+            if (person.getGender() == Gender.MALE) {
+                activityScore = 1.11;
+            } else {
+                activityScore = 1.12;
+            }
+            break;
+        case MEDIUM:
+            if (person.getGender() == Gender.MALE) {
+                activityScore = 1.26;
+            } else {
+                activityScore = 1.27;
+            }
+            break;
+        case HIGH:
+        default:
+            if (person.getGender() == Gender.MALE) {
+                activityScore = 1.48;
+            } else {
+                activityScore = 1.45;
+            }
+            break;
+        }
+
+        switch (person.getGender()) {
+        case MALE:
+            requirement = 662 - 9.53 * person.getAge() + 15.91 * activityScore * person.getOriginalWeight()
+                    + 539.6 * person.getHeight() / 100;
+            break;
+        case FEMALE:
+            requirement = 354 - 6.91 * person.getAge() + 9.36 * activityScore * person.getOriginalWeight()
+                    + 726 * person.getHeight() / 100;
+            break;
+        default:
+        }
+
+        if (person.getOriginalWeight() > person.getTargetWeight()) {
+            recomendation = (int) requirement - 300;
+        } else {
+            recomendation = (int) requirement + 100;
+        }
+        return recomendation;
     }
 }
