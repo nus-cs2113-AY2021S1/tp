71a507cb8e536915fdbec6ce554c8b1f0ceb342a
jiaaaqi <e0425649@u.nus.edu>
2020-10-26 01:01:59 +0800

    add method description comments

--- src/main/java/command/LocateCommand.java
+++ src/main/java/command/LocateCommand.java
@@ -21,6 +21,16 @@ public class LocateCommand extends Command {
         this.input = input;
     }
 
+    /**
+     * Provides location information when user gives a number within size of EventList or name of a location
+     *
+     * @param events    the list of Events.
+     * @param locations the list of Locations.
+     * @param busStops  the list of BusStops.
+     * @param ui        do outputs.
+     * @param storage   store the data.
+     * @throws NuScheduleException
+     */
     @Override
     public void execute(EventList events, LocationList locations, BusStopList busStops, UI ui, Storage storage) throws NuScheduleException {
         int event_num;
