fae3edc3ed19cabc63bc569b1a20d3daceda33b5
jiaaaqi <e0425649@u.nus.edu>
2020-10-26 01:23:40 +0800

    removed unnecessary test

--- src/test/java/location/LocationListTest.java
+++ src/test/java/location/LocationListTest.java
@@ -5,11 +5,4 @@ import org.junit.jupiter.api.Test;
 import parser.Parser;
 
 public class LocationListTest {
-
-    @Test
-    void execute_cannotFindLocation_NullPointerException() {
-        Assertions.assertThrows(NullPointerException.class, ()-> {
-           Location l = Parser.parseLocation("location", null);
-        });
-    }
 }
