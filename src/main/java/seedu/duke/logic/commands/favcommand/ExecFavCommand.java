a51c7b6374f363926af4a4d50aa15d9d1c831141
ethan2212 <e0407555@u.nus.edu>
2020-10-25 23:19:33 +0800

    Added executeCommand method and constructor for ExecFavCommand

--- src/main/java/seedu/duke/logic/commands/favcommand/ExecFavCommand.java
+++ src/main/java/seedu/duke/logic/commands/favcommand/ExecFavCommand.java
@@ -1,11 +1,23 @@
 package seedu.duke.logic.commands.favcommand;
 
 import seedu.duke.exceptions.CustomException;
+import seedu.duke.favorite.Fav;
+import seedu.duke.favorite.FavList;
 import seedu.duke.logic.commands.Command;
+import seedu.duke.logic.parser.Parser;
 
 public class ExecFavCommand extends Command {
+    private int index;
+
+    public ExecFavCommand(String input) {
+        this.index = Integer.parseInt(input) - 1;
+    }
+
     @Override
     public void executeCommand() throws CustomException {
-
+        Fav f = FavList.getFav(index);
+        Parser parser = new Parser(f.getCommand());
+        System.out.println(f.getCommand());
+        boolean done = !parser.extractType();
     }
 }
