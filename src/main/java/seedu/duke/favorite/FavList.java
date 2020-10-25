e687b16d43b96bad1bfb66f7deb5fbcd861a3b9b
wamikamalik <wamikamalik@hotmail.com>
2020-10-25 16:45:01 +0400

    Update fav and favlist to modify description

--- src/main/java/seedu/duke/favorite/FavList.java
+++ src/main/java/seedu/duke/favorite/FavList.java
@@ -1,5 +1,8 @@
 package seedu.duke.favorite;
 
+import seedu.duke.exceptions.CustomException;
+import seedu.duke.exceptions.ExceptionType;
+
 import java.util.ArrayList;
 
 public class FavList {
@@ -10,12 +13,32 @@ public class FavList {
     }
 
     public static void addFav(Fav fav) {
+        favList.add(fav);
     }
 
     public static void deleteFav(int index) {
     }
 
     public static void listFav() {
+    }
+
+    public static String changeDesc(int index, String newDesc) throws CustomException {
+        String oldDesc = checkIndexAndDesc(index, newDesc);
+        favList.get(index-1).changeDesc(newDesc);
+        return oldDesc;
+    }
+
+    private static String checkIndexAndDesc(int index, String newDesc) throws CustomException {
+        if(index ==0 || index > favList.size()) {
+            throw new CustomException(ExceptionType.INVALID_INDEX);
+        }
+        assert index > 0 && index < FavList.getList().size() : "Index out of bounds.";
+        String oldDesc = favList.get(index -1).getDesc();
+        if(oldDesc.equals(newDesc)) {
+            throw new CustomException(ExceptionType.SAME_DESCRIPTION);
+        }
+        assert !oldDesc.equals(newDesc) : "description is the same as before.";
+        return oldDesc;
     }
 
     public static ArrayList<Fav> getList() {
