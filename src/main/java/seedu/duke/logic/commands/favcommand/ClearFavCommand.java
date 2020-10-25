025e6bbf20ba4daf69e122690ffd46fbf5503cbb
mrwsy1 <e0406495@u.nus.edu>
2020-10-26 00:08:08 +0800

    Add clearfav

--- src/main/java/seedu/duke/logic/commands/favcommand/ClearFavCommand.java
+++ src/main/java/seedu/duke/logic/commands/favcommand/ClearFavCommand.java
@@ -1,2 +1,12 @@
-package seedu.duke.logic.commands.favcommand;public class ClearFavCommand {
+package seedu.duke.logic.commands.favcommand;
+
+import seedu.duke.favorite.FavList;
+import seedu.duke.logic.commands.Command;
+
+public class ClearFavCommand extends Command {
+
+    @Override
+    public void executeCommand() {
+        FavList.clearFav();
+    }
 }
