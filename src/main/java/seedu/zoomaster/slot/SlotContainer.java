d1fe545352eb5bba7bd7b12fbefdea2fe92497f9
Francisco Chensan <chensan@u.nus.edu>
2020-10-25 23:59:41 +0800

    Move sortSlotsByTime to SlotContainer

--- src/main/java/seedu/zoomaster/slot/SlotContainer.java
+++ src/main/java/seedu/zoomaster/slot/SlotContainer.java
@@ -26,4 +26,31 @@ public class SlotContainer {
     public void setModuleCodesList(ArrayList<String> moduleCodeListInput) {
         thisDayModuleCodesList = moduleCodeListInput;
     }
+
+    public static SlotContainer sortSlotsByTime(SlotContainer slotContainer) {
+        ArrayList<Slot> thisDaySlots = slotContainer.getSlotList();
+        ArrayList<String> thisDayModuleCodesList = slotContainer.getModuleCodesList();
+        ArrayList<Slot> sortedThisDaySlots = new ArrayList<>();
+        ArrayList<String> sortedThisDayModuleCodesList = new ArrayList<>();
+        int indexEarliestTimeSlot;
+        Slot earliestTimeSlot;
+
+        while (thisDaySlots.size() != 0) {
+            earliestTimeSlot = thisDaySlots.get(0);
+            indexEarliestTimeSlot = 0;
+            for (int i = 1; i < thisDaySlots.size(); i++) {
+                if (earliestTimeSlot.getStartTime().isAfter(thisDaySlots.get(i).getStartTime())) {
+                    earliestTimeSlot = thisDaySlots.get(i);
+                    indexEarliestTimeSlot = i;
+                }
+            }
+            sortedThisDaySlots.add(thisDaySlots.get(indexEarliestTimeSlot));
+            sortedThisDayModuleCodesList.add(thisDayModuleCodesList.get(indexEarliestTimeSlot));
+            thisDaySlots.remove(indexEarliestTimeSlot);
+            thisDayModuleCodesList.remove(indexEarliestTimeSlot);
+        }
+
+
+        return new SlotContainer(sortedThisDaySlots, sortedThisDayModuleCodesList);
+    }
 }
