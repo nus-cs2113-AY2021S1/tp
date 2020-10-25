12759ac718540cde8cf7b32b8eb7408cfd9164ee
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2020-10-25 23:05:00 +0800

    Add random bunny generator

--- src/main/java/seedu/duke/bunnylist/GenBunny.java
+++ src/main/java/seedu/duke/bunnylist/GenBunny.java
@@ -1,7 +1,18 @@
 package seedu.duke.bunnylist;
+import seedu.duke.bunny.Bunny;
+import seedu.duke.ui.UI;
 
+import java.util.ArrayList;
+import java.util.Random;
 
+import static seedu.duke.bunnylist.BunnyList.bunniesList;
 
 public class GenBunny {
 
+    public static void pickRandomBunny(ArrayList<Bunny> bunniesList){
+        Random rand = new Random();
+        int bunnySelected = rand.nextInt(bunniesList.size());
+        // print response
+        UI.bunnyRandomlySelected(bunnySelected);
+    }
 }
