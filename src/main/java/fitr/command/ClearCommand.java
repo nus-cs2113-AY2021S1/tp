65cbd3f31aa546a05f0fb4d822899e58c7e66d7e
dmbclub <dmb437026158@gmail.com>
2020-10-26 22:35:28 +0800

    update clearcommand and listmanager

--- src/main/java/fitr/command/ClearCommand.java
+++ src/main/java/fitr/command/ClearCommand.java
@@ -26,27 +26,27 @@ public class ClearCommand extends Command {
             listManager.clearFoodList();
             listManager.clearExerciseList();
             listManager.clearGoalList();
-            Ui.printCustomMessage("Food, exercise and goal lists are all cleared!");
+            Ui.printCustomMessage("Food, exercise and goal lists are both cleared!");
         } else {
             switch (command) {
-            case Commands.COMMAND_FOOD:
-                LOGGER.fine("Clearing food list.");
-                listManager.clearFoodList();
-                Ui.printCustomMessage("Food list is cleared!");
-                break;
-            case Commands.COMMAND_EXERCISE:
-                LOGGER.fine("Clearing exercise list.");
-                listManager.clearExerciseList();
-                Ui.printCustomMessage("Exercise list is cleared!");
-                break;
-            case Commands.COMMAND_GOAL:
-                LOGGER.fine("Clearing goal list.");
-                listManager.clearGoalList();
-                Ui.printCustomMessage("Goal list is cleared!");
-                break;
-            default:
-                Ui.printCustomMessage("Invalid clear command entered!");
-                break;
+                case Commands.COMMAND_FOOD:
+                    LOGGER.fine("Clearing food list.");
+                    listManager.clearFoodList();
+                    Ui.printCustomMessage("Food list is cleared!");
+                    break;
+                case Commands.COMMAND_EXERCISE:
+                    LOGGER.fine("Clearing exercise list.");
+                    listManager.clearExerciseList();
+                    Ui.printCustomMessage("Exercise list is cleared!");
+                    break;
+                case Commands.COMMAND_GOAL:
+                    LOGGER.fine("Clearing goal list.");
+                    listManager.clearGoalList();
+                    Ui.printCustomMessage("Goal list is cleared!");
+                    break;
+                default:
+                    Ui.printCustomMessage("Invalid clear command entered!");
+                    break;
             }
         }
 
--- src/main/java/fitr/list/ListManager.java
+++ src/main/java/fitr/list/ListManager.java
@@ -77,5 +77,4 @@ public class ListManager {
     public void clearGoalList() {
         goalList.clearList();
     }
-
 }
