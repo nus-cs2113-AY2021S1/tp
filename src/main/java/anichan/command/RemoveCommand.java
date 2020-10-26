cd05aa1ba44cab0cedb5bba9cfc5fb7b39a0e768
Michael Dinata <michaeldinata@Michaels-MacBook-Pro.local>
2020-10-26 11:06:24 +0800

    Modified naming in removeCommand to 'animeIndexInWatchlist'

--- src/main/java/anichan/command/RemoveCommand.java
+++ src/main/java/anichan/command/RemoveCommand.java
@@ -18,7 +18,7 @@ public class RemoveCommand extends Command {
     protected static final String OUT_OF_BOUND_INDEX_ERROR = "Invalid Watchlist Index!";
     protected static final String EMPTY_WATCHLIST_ERROR = "Watchlist is empty!";
 
-    private Integer watchlistListIndex;
+    private Integer animeIndexInWatchlist;
     private Integer animeIndex;
     private static final Logger LOGGER = getAniLogger(RemoveCommand.class.getName());
     
@@ -46,24 +46,24 @@ public class RemoveCommand extends Command {
         if (activeWatchlist.getWatchlistSize() == 0) {
             LOGGER.log(Level.WARNING, EMPTY_WATCHLIST_ERROR);
             throw new AniException(EMPTY_WATCHLIST_ERROR);
-        } else if (activeWatchlist.getWatchlistSize() <= watchlistListIndex - 1) {
+        } else if (activeWatchlist.getWatchlistSize() <= animeIndexInWatchlist - 1) {
             LOGGER.log(Level.WARNING, OUT_OF_BOUND_INDEX_ERROR);
             throw new AniException(OUT_OF_BOUND_INDEX_ERROR);
-        } else if (watchlistListIndex < 0) {
+        } else if (animeIndexInWatchlist < 0) {
             LOGGER.log(Level.WARNING, OUT_OF_BOUND_INDEX_ERROR);
             throw new AniException(OUT_OF_BOUND_INDEX_ERROR);
         } 
         
-        assert this.watchlistListIndex >= 0 : "Watchlist index has to be valid";
-        animeIndex = activeWatchlist.getWatchlistListAnimeIndex(watchlistListIndex);
-        activeWatchlist.removeAnimeFromList(watchlistListIndex);
+        assert this.animeIndexInWatchlist >= 0 : "anime index has to be valid";
+        animeIndex = activeWatchlist.getWatchlistListAnimeIndex(animeIndexInWatchlist);
+        activeWatchlist.removeAnimeFromList(animeIndexInWatchlist);
 
         ArrayList<Watchlist> watchlistList = activeWorkspace.getWatchlistList();
         storageManager.saveWatchlistList(activeWorkspace.getName(), watchlistList);
         LOGGER.log(Level.INFO, "Successfully removed anime from active watchlist");
     }
     
-    public void setWatchlistListIndex(Integer watchlistListIndex) {
-        this.watchlistListIndex = watchlistListIndex - 1;
+    public void setWatchlistListIndex(Integer animeIndexInWatchlist) {
+        this.animeIndexInWatchlist = animeIndexInWatchlist - 1;
     }
 }
