bf791211249105e84bae80e77517a1b6db0dd081
farice9 <limyb50@gmail.com>
2020-10-26 17:33:56 +0800

    Changed the welcome menu date & time printing format

--- src/main/java/userinterface/MainMenu.java
+++ src/main/java/userinterface/MainMenu.java
@@ -5,14 +5,17 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 public class MainMenu extends Ui {
-    private static DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("d MMM yyyy HH:mm");
-    private static LocalDateTime now = LocalDateTime.now();
-    private static String nowFormatted = dateTimeFormat.format(now);
+    private static DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("EEE d MMM yyyy");
+    private static DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("hh:mm a");
+    private static LocalDateTime currentDateTime = LocalDateTime.now();
+    private static String currentDateFormatted = dateFormat.format(currentDateTime);
+    private static String currentTimeFormatted = timeFormat.format(currentDateTime);
 
     private static final String WELCOME_MESSAGE = LINE_DIVIDER + System.lineSeparator()
             + "Welcome to Study It! Your personal study assistant!"
             + "\n\n"
-            + "Current date & time : " + nowFormatted
+            + "Today's date: " + currentDateFormatted
+            + "\nThe time now is " + currentTimeFormatted
             + "\n\n"
             + "Let's get productive!"
             + "\n"
