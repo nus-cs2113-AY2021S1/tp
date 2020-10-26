933039b8865f5627df7b507350a1e807f1017e5b
Shikai Zhou <shikaizhou@outlook.com>
2020-10-26 15:52:09 +0800

    added new tests and new exceptions

--- src/main/java/seedu/duke/utility/TimeParser.java
+++ src/main/java/seedu/duke/utility/TimeParser.java
@@ -1,6 +1,7 @@
 package seedu.duke.utility;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * TimeParser converts user's time input into int.
@@ -14,28 +15,21 @@ public class TimeParser {
     /**
      * Converts time from string format to int.
      * @param input user's input
-     * @return time in minutes as an integer
+     * @return time in minutes as an integer if input is valid or -1 if input is invalid.
      */
     public int parseTime(String input) {
-
-        String[] splitHours = input.split("h");
-        String[] splitMinutes = splitHours[1].split("m");
-        /*
-        When the user input does not specify hours and minutes
-         */
         try {
-            if (splitHours.equals(splitMinutes)) {
-                int result = Integer.parseInt(input);
-                return result;
-
-            } else {
-                int hours = Integer.parseInt(splitHours[0]);
-                int minutes = Integer.parseInt(splitMinutes[0]);
-                return hours * 60 + minutes;
-            }
+            String[] splitHours = input.split("h");
+            String[] splitMinutes = splitHours[1].split("m");
+            int hours = Integer.parseInt(splitHours[0]);
+            int minutes = Integer.parseInt(splitMinutes[0]);
+            return hours * 60 + minutes;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            int result = Integer.parseInt(input);
+            return result;
 
         } catch (NumberFormatException e) {
-            Ui.printBadInputException();
+            Ui.printInvalidFormatException();
             return -1;
         }
     }
--- src/test/java/seedu/duke/utility/TimeParserTest.java
+++ src/test/java/seedu/duke/utility/TimeParserTest.java
@@ -6,9 +6,21 @@ public class TimeParserTest {
     private TimeParser parser = new TimeParser();
 
     @org.junit.jupiter.api.Test
-    public void timeParseTime() {
+    public void timeParseTest() {
         final String input = "2h45m";
         assertEquals(165, parser.parseTime(input));
 
+    }
+
+    @org.junit.jupiter.api.Test
+    public void parseIntegerTest() {
+        final String input = "45";
+        assertEquals(45, parser.parseTime(input));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void parseInvalidInput() {
+        final String input = "onehour";
+        assertEquals(-1,parser.parseTime(input));
     }
 }
