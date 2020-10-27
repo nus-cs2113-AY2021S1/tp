f3241d3a8b3473c4627c9889ac80cae90795a4d0
matthewgani <matthewgani2@gmail.com>
2020-10-27 18:48:37 +0800

    Finished Javadoc for extractCommand

--- src/main/java/seedu/duke/command/ExtractCommand.java
+++ src/main/java/seedu/duke/command/ExtractCommand.java
@@ -26,7 +26,7 @@ public class ExtractCommand extends Command {
     /**
      * Constructor for parsing email/texts seedu.duke.
      *
-     * @param command from user input
+     * @param command from user input.
      */
     public ExtractCommand(String command) {
         this.isExit = false;
@@ -34,7 +34,7 @@ public class ExtractCommand extends Command {
     }
 
     /**
-     * abstract class for commands.
+     * Extracts dates and timings from any block of text and creates Personal Event.
      *
      * @param data    object of UserData class containing user's data.
      * @param ui      containing the responses to print.
@@ -78,9 +78,72 @@ public class ExtractCommand extends Command {
         storage.saveFile(storage.getFileLocation("Personal"), data, "Personal");
     }
 
+    /**
+     * Detects 12 and 24 hour time slots from a text.
+     *
+     * @param textBody A string of the text body that is scanned through.
+     * @return An ArrayList of LocalTime objects for the time slots detected.
+     */
+    private ArrayList<LocalTime> detectTime(String textBody) {
+        ArrayList<String> timeListInString = new ArrayList<>();
+        String upperCaseTextBody = textBody.toUpperCase();
+        Pattern timePattern = Pattern.compile("\\b(1[0-9]|0?[0-9]|2[0-3])([:.][0-5][0-9])?[\\h]?([AP][M])?\\b");
+        Matcher timeMatcher = timePattern.matcher(upperCaseTextBody);
+
+        while (timeMatcher.find()) {
+            String time = timeMatcher.group(0);
+            if (time.contains(".")) {
+                time = time.replaceAll("\\.", ":");
+            }
+            if (time.contains("PM") || time.contains("AM")) {
+                if (!time.contains(":")) {
+                    time = time.substring(0, time.length() - 2) + ":00 " + time.substring(time.length() - 2);
+                }
+                if (!time.contains(" ")) {
+                    time = time.substring(0, time.length() - 2) + " " + time.substring(time.length() - 2);
+                }
+            }
+            time = time.toLowerCase();
+            timeListInString.add(time);
+        }
+
+        ArrayList<LocalTime> timeList = verifyTime(timeListInString);
+        timeCount = timeList.size();
+
+        return timeList;
+    }
+
+    /**
+     * Verifies whether the time slots detected are legitimate and converts to LocalTime.
+     *
+     * @param timeListInString An ArrayList of strings of time slots detected.
+     * @return An ArrayList of LocalTime containing time slots that have been successfully converted.
+     */
+    private ArrayList<LocalTime> verifyTime(ArrayList<String> timeListInString) {
+        ArrayList<LocalTime> timeList = new ArrayList<>();
+
+        for (String timeInString : timeListInString) {
+            try {
+                LocalTime localTime = DateTimeParser.timeParser(timeInString);
+                timeList.add(localTime);
+            } catch (TimeErrorException e) {
+                // something went wrong with date parsing
+                // Log something?
+            }
+        }
+
+        return timeList;
+    }
+
+    /**
+     * Allows user to choose the time they want from timeList.
+     *
+     * @param timeList An ArrayList of LocalTime containing all time slots detected.
+     * @param ui contains responses to print.
+     * @return the LocalTime object chosen by user.
+     */
     private LocalTime chooseFinalTime(ArrayList<LocalTime> timeList, Ui ui) {
         LocalTime finalTime = null;
-
         if (timeCount > 1) {
             System.out.println("We have detected " + timeCount + " time slots in this text body!");
             System.out.println("Please select the time you want for this event from the list below!");
@@ -116,52 +179,12 @@ public class ExtractCommand extends Command {
 
     }
 
-
-    private ArrayList<LocalTime> detectTime(String textBody) {
-        ArrayList<String> timeListInString = new ArrayList<>();
-        String upperCaseTextBody = textBody.toUpperCase();
-        Pattern timePattern = Pattern.compile("\\b(1[0-9]|0?[0-9]|2[0-3])([:.][0-5][0-9])?[\\h]?([AP][M])?\\b");
-        Matcher timeMatcher = timePattern.matcher(upperCaseTextBody);
-
-        while (timeMatcher.find()) {
-            String time = timeMatcher.group(0);
-            if (time.contains(".")) {
-                time = time.replaceAll("\\.", ":");
-            }
-            if (time.contains("PM") || time.contains("AM")) {
-                if (!time.contains(":")) {
-                    time = time.substring(0, time.length() - 2) + ":00 " + time.substring(time.length() - 2);
-                }
-                if (!time.contains(" ")) {
-                    time = time.substring(0, time.length() - 2) + " " + time.substring(time.length() - 2);
-                }
-            }
-            time = time.toLowerCase();
-            timeListInString.add(time);
-        }
-
-        ArrayList<LocalTime> timeList = verifyTime(timeListInString);
-        timeCount = timeList.size();
-
-        return timeList;
-    }
-
-    private ArrayList<LocalTime> verifyTime(ArrayList<String> timeListInString) {
-        ArrayList<LocalTime> timeList = new ArrayList<>();
-
-        for (String timeInString : timeListInString) {
-            try {
-                LocalTime localTime = DateTimeParser.timeParser(timeInString);
-                timeList.add(localTime);
-            } catch (TimeErrorException e) {
-                // something went wrong with date parsing
-                // Log something?
-            }
-        }
-
-        return timeList;
-    }
-
+    /**
+     * Detects all the dates from a block of text.
+     *
+     * @param textBody A string containing the text to be scanned through.
+     * @return An ArrayList of LocalDate objects for the dates detected.
+     */
     private ArrayList<LocalDate> detectDate(String textBody) {
         ArrayList<String> dateListInString = new ArrayList<>();
         String upperCaseTextBody = textBody.toUpperCase();
@@ -197,7 +220,34 @@ public class ExtractCommand extends Command {
         return dateList;
     }
 
+    /**
+     * Verifies dates detected and converts them from string to LocalDate.
+     *
+     * @param dateListInString An ArrayList of strings containing dates.
+     * @return An ArrayList of LocalDate that contains verified dates.
+     */
+    private ArrayList<LocalDate> verifyDate(ArrayList<String> dateListInString) {
+        ArrayList<LocalDate> dateList = new ArrayList<>();
+        for (String dateInString : dateListInString) {
+            try {
+                LocalDate localDate = DateTimeParser.dateParser(dateInString);
+                dateList.add(localDate);
+            } catch (DateErrorException e) {
+                // something went wrong with date parsing
+                // Log something?
+            }
+        }
 
+        return dateList;
+    }
+
+    /**
+     * Allows user to choose the dates they want from the dateList.
+     *
+     * @param dateList An ArrayList containing LocalDate objects to choose from.
+     * @param ui contains responses to print.
+     * @return A localDate object chosen for the event.
+     */
     private LocalDate chooseFinalDate(ArrayList<LocalDate> dateList, Ui ui) {
         LocalDate finalDate = null;
         if (dateCount > 1) {
@@ -235,21 +285,12 @@ public class ExtractCommand extends Command {
         return finalDate;
     }
 
-    private ArrayList<LocalDate> verifyDate(ArrayList<String> dateListInString) {
-        ArrayList<LocalDate> dateList = new ArrayList<>();
-        for (String dateInString : dateListInString) {
-            try {
-                LocalDate localDate = DateTimeParser.dateParser(dateInString);
-                dateList.add(localDate);
-            } catch (DateErrorException e) {
-                // something went wrong with date parsing
-                // Log something?
-            }
-        }
-
-        return dateList;
-    }
-
+    /**
+     * Detects the day portion of the date.
+     *
+     * @param date String containing the full date detected.
+     * @return String containing the day.
+     */
     private String detectDay(String date) {
         String day = null;
         Pattern dayPattern = Pattern.compile("\\b(([0]?[0-9])|([0-2][0-9])|([3][0-1])){1,2}(ST|ND|RD|TH)?\\b");
@@ -269,25 +310,12 @@ public class ExtractCommand extends Command {
         return day;
     }
 
-    private String getCurrentYear() {
-        Calendar currentDay = Calendar.getInstance();
-        int yearInInt = currentDay.get(Calendar.YEAR);
-        String year = String.valueOf(yearInInt);
-        return year;
-    }
-
-    private String detectYear(String date) {
-        String year = date.substring(date.length() - 4);
-        try {
-            if (Integer.parseInt(year) > 2100 || Integer.parseInt(year) < 1900) {
-                year = getCurrentYear();
-            }
-        } catch (NumberFormatException e) {
-            year = getCurrentYear();
-        }
-        return year;
-    }
-
+    /**
+     * Detects the month from the date.
+     *
+     * @param date A string containing the full date detected.
+     * @return A string containing the month detected.
+     */
     private String detectMonth(String date) {
         String month = null;
         Pattern monthPattern = Pattern.compile("(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)");
@@ -336,6 +364,36 @@ public class ExtractCommand extends Command {
         }
 
         return month;
+    }
+
+    /**
+     * Detects the year from the date.
+     *
+     * @param date A string containing the full date.
+     * @return A string containing the year portion of the date.
+     */
+    private String detectYear(String date) {
+        String year = date.substring(date.length() - 4);
+        try {
+            if (Integer.parseInt(year) > 2100 || Integer.parseInt(year) < 1900) {
+                year = getCurrentYear();
+            }
+        } catch (NumberFormatException e) {
+            year = getCurrentYear();
+        }
+        return year;
+    }
+
+    /**
+     * Retrieves the current year.
+     *
+     * @return A string containing the current year.
+     */
+    private String getCurrentYear() {
+        Calendar currentDay = Calendar.getInstance();
+        int yearInInt = currentDay.get(Calendar.YEAR);
+        String year = String.valueOf(yearInInt);
+        return year;
     }
 
 
