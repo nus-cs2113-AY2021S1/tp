726d0940451811eface846f533618ee2dbb99580
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-10-27 22:43:47 +0800

    add Junit test for PersonSaveLoadManager

--- src/test/java/seedu/dietbook/saveload/PersonSaveLoadManagerTest.java
+++ src/test/java/seedu/dietbook/saveload/PersonSaveLoadManagerTest.java
@@ -1,6 +1,9 @@
 package seedu.dietbook.saveload;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +20,25 @@ class PersonSaveLoadManagerTest {
         pslTest.setOriginalWeight(200);
         pslTest.setCurrentWeight(300);
         pslTest.setTargetWeight(100);
+        pslTest.save("pslTest");
+    }
+
+    @Test
+    private void Load_NoSuchFile_ExpectFileNotFoundException() {
+        PersonSaveLoadManager localpslTest = new PersonSaveLoadManager();
+        assertThrows(FileNotFoundException.class, () -> localpslTest.load("pie die pie"));
+    }
+
+    @Test
+    private void Load_CorrectFile_AllContentsCorrect() throws Exception {
+        PersonSaveLoadManager localpslTest = new PersonSaveLoadManager();
+        localpslTest.load("pslTest");
+        assertEquals("Victor Chng", localpslTest.getName());
+        assertEquals("Unknown", localpslTest.getGender());
+        assertEquals(200, localpslTest.getOriginalWeight());
+        assertEquals(100, localpslTest.getAge());
+        assertEquals(300, localpslTest.getCurrentWeight());
+        assertEquals(100, localpslTest.getTargetWeight());
+        assertEquals(0, localpslTest.getActivityLevel());
     }
 }
