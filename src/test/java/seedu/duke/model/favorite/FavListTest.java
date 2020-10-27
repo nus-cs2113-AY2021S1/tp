1482daf5afea235733e33be1747c0bea94b23ee9
Lezn0 <yuxinwu1234@gmail.com>
2020-10-27 16:48:16 +0800

    Update style

--- src/test/java/seedu/duke/model/favorite/FavListTest.java
+++ src/test/java/seedu/duke/model/favorite/FavListTest.java
@@ -1,4 +1,5 @@
 package seedu.duke.model.favorite;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -49,14 +50,14 @@ class FavListTest {
     }
 
     @Test
-    void contains_duplicateFav_expectTrue(){
+    void contains_duplicateFav_expectTrue() {
         Fav fav = new Fav("/allbus","List out all buses in nus");
         FavList.addFav(fav);
         assertTrue(FavList.contains(fav));
     }
 
     @Test
-    void contains_newFav_expectFalse(){
+    void contains_newFav_expectFalse() {
         Fav fav1 = new Fav("/allbus","List out all buses in nus");
         FavList.addFav(fav1);
         Fav fav2 = new Fav("/liststops","/liststops");
