d3569253511d21934bcf40d98b40106fd4c3973a
prachi.goenka@u.nus.edu <prachi.goenka@u.nus.edu>
2020-10-27 14:39:40 +0800

    Save note details once tags have been deleted

--- src/main/java/seedu/notus/command/DeleteTagCommand.java
+++ src/main/java/seedu/notus/command/DeleteTagCommand.java
@@ -3,6 +3,7 @@ package seedu.notus.command;
 import seedu.notus.data.tag.Tag;
 import seedu.notus.ui.Formatter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static seedu.notus.util.PrefixSyntax.PREFIX_DELIMITER;
@@ -23,6 +24,7 @@ public class DeleteTagCommand extends Command {
     public static final String COMMAND_SUCCESSFUL_MESSAGE = "Deleted the tag! ";
     public static final String COMMAND_UNSUCCESSFUL_MESSAGE = "The tag does not exist! ";
     public static final String HEADER_DELETE_TAG = "Deleting tags...";
+    public static final String FILE_WRITE_UNSUCCESSFUL_MESSAGE = "Unable to write to file";
 
     private ArrayList<Tag> tags;
 
@@ -38,6 +40,13 @@ public class DeleteTagCommand extends Command {
         ArrayList<String> executedMessage = tagManager.deleteTag(tags, COMMAND_SUCCESSFUL_MESSAGE,
                 COMMAND_UNSUCCESSFUL_MESSAGE);
         executedMessage.add(0, HEADER_DELETE_TAG);
+
+        try {
+            storageManager.saveAllNoteDetails(notebook, false);
+        } catch (IOException exception) {
+            Formatter.formatString(FILE_WRITE_UNSUCCESSFUL_MESSAGE);
+        }
+
         return Formatter.formatString(executedMessage, true);
     }
 }
