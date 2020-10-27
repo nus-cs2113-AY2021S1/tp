e27ca41bb9a74fb26868fcfe6311d6b26468e54b
ethan2212 <e0407555@u.nus.edu>
2020-10-27 20:33:45 +0800

    Update ExecFav Parser

--- src/main/java/seedu/duke/logic/parser/ExecFavParser.java
+++ src/main/java/seedu/duke/logic/parser/ExecFavParser.java
@@ -1,8 +1,34 @@
 package seedu.duke.logic.parser;
 
+import seedu.duke.exceptions.CustomException;
+import seedu.duke.exceptions.ExceptionType;
+
 public class ExecFavParser extends Parser {
 
-    public ExecFavParser(String userInput) {
+    private String userInput;
+    private int index;
+
+    public ExecFavParser(String userInput) throws CustomException {
         super(userInput);
+        this.userInput = userInput;
+        checkInput();
     }
+
+    public int getIndex() {
+        return index;
+    }
+
+    private void checkInput() throws CustomException {
+        if (super.getUserInput().isBlank()){
+            throw new CustomException(ExceptionType.NO_INPUT);
+        }
+        try {
+            int index = Integer.parseInt(super.getUserInput()) - 1;
+            this.index=index;
+        } catch (NumberFormatException e) {
+            throw new CustomException(ExceptionType.NOT_A_NUMBER);
+        }
+    }
+
+
 }
