deed64d595321e1b0b4c07317dcfd1a43699d6a2
kaiwen98 <e0406836@u.nus.edu>
2020-10-28 15:01:11 +0800

    Added logger to InputParser to display CommandPacket contents for log, as well as fix parser bug

--- src/main/java/seedu/financeit/utils/LoggerCentre.java
+++ src/main/java/seedu/financeit/utils/LoggerCentre.java
@@ -1,11 +1,14 @@
 package seedu.financeit.utils;
 
+import seedu.financeit.parser.InputParser;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LoggerCentre {
     private Level level;
     public static Logger loggerParamChecker = Logger.getLogger(ParamChecker.class.getName());
+    public static Logger loggerInputParser = Logger.getLogger(InputParser.class.getName());
     private static LoggerCentre loggerCentre = null;
 
     private LoggerCentre() {
@@ -21,5 +24,6 @@ public class LoggerCentre {
     public void setLevel(Level level) {
         this.level = level;
         loggerParamChecker.setLevel(level);
+        loggerInputParser.setLevel(level);
     }
 }
