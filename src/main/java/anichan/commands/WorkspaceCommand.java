9b2788bc20d2c14a7db378b111ef63100247eac5
ChanJianHao <49814823+ChanJianHao@users.noreply.github.com>
2020-10-28 22:54:08 +0800

    Update WorkspaceCommand

--- src/main/java/anichan/commands/WorkspaceCommand.java
+++ src/main/java/anichan/commands/WorkspaceCommand.java
@@ -18,13 +18,14 @@ public class WorkspaceCommand extends Command {
     private static final String LIST_OPTION = "l";
     private static final String DELETE_OPTION = "d";
     private static final Logger LOGGER = AniLogger.getAniLogger(WatchlistCommand.class.getName());
+    public static final String REGEX_ALPHANUMERIC = "^[a-zA-Z0-9]*$";
 
     private final String commandOption;
-    private final String commandDescription;
+    private final String workspaceName;
 
-    public WorkspaceCommand(String commandOption, String commandDescription) {
+    public WorkspaceCommand(String commandOption, String workspaceName) {
         this.commandOption = commandOption;
-        this.commandDescription = commandDescription;
+        this.workspaceName = workspaceName;
     }
 
     @Override
@@ -41,13 +42,14 @@ public class WorkspaceCommand extends Command {
         case DELETE_OPTION:
             return deleteWorkspace(user, storageManager);
         default:
-            LOGGER.log(Level.WARNING, "Invalid watchlist command provided.");
-            throw new AniException("Watchlist command only accepts the options: -n, -s, -l, and -d.");
+            LOGGER.log(Level.WARNING, "Invalid workspace command provided.");
+            throw new AniException("Workspace command only accepts the options: -n, -s, -l, and -d.");
         }
     }
 
     public String createWorkspace(User user, StorageManager storageManager) throws AniException {
-        Workspace newWorkspace = user.addWorkspace(commandDescription);
+        checkName(workspaceName);
+        Workspace newWorkspace = user.addWorkspace(workspaceName);
 
         ArrayList<Watchlist> watchlistList = new ArrayList<>();
         watchlistList.add(new Watchlist("Default"));
@@ -59,7 +61,8 @@ public class WorkspaceCommand extends Command {
     }
 
     public String switchWorkspace(User user) throws AniException {
-        String trimmedName = commandDescription;
+        checkName(workspaceName);
+        String trimmedName = workspaceName;
         user.switchActiveWorkspace(trimmedName);
 
         LOGGER.log(Level.INFO, "Successfully switched to workspace: " + trimmedName);
@@ -67,15 +70,17 @@ public class WorkspaceCommand extends Command {
     }
 
     public String deleteWorkspace(User user, StorageManager storageManager) throws AniException {
-        if (user.getActiveWorkspace().toString().equals(commandDescription)) {
+        checkName(workspaceName);
+
+        if (user.getActiveWorkspace().toString().equals(workspaceName)) {
             throw new AniException("Please switch workspace before trying to delete it.");
         }
 
-        user.deleteWorkspace(commandDescription);
-        storageManager.deleteWorkspace(commandDescription);
+        user.deleteWorkspace(workspaceName);
+        storageManager.deleteWorkspace(workspaceName);
 
-        LOGGER.log(Level.INFO, "Successfully deleted workspace: " + commandDescription);
-        return "Successfully deleted workspace: " + commandDescription;
+        LOGGER.log(Level.INFO, "Successfully deleted workspace: " + workspaceName);
+        return "Successfully deleted workspace: " + workspaceName;
     }
 
     public String listWorkspace(User user) {
@@ -90,5 +95,16 @@ public class WorkspaceCommand extends Command {
         }
 
         return workspacesString.toString();
+    }
+
+    public void checkName(String workspaceName) throws AniException {
+        if (workspaceName != null) {
+            boolean isValid = workspaceName.matches(REGEX_ALPHANUMERIC);
+
+            if (!isValid) {
+                LOGGER.log(Level.WARNING, "Workspace name provided does not meet standards.");
+                throw new AniException("Invalid parameters detected!");
+            }
+        }
     }
 }
