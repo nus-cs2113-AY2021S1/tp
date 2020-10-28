e7f1e4ed76bcd46ad171cde631d399c67d564a56
hailqueenflo <florencelim14@gmail.com>
2020-10-29 04:28:04 +0800

    Edited timetable message

--- src/main/java/timetable/Message.java
+++ src/main/java/timetable/Message.java
@@ -9,7 +9,7 @@ public class Message {
             + "\nUse \"show schedule\" to view your timetable.";
 
     public static final String printShowSchedule = "This is your schedule for the next 7 days.";
-    public static final String printShowLink = "This is the conference link u need for the next two hour.";
+    public static final String printShowLink = "This is the conference link you need for the next two hours.";
 
     public static final String printInvalidEvent = "Invalid command!\n\n"
             + "Use \"add class\" or \"add activity\" to start adding items\n"
