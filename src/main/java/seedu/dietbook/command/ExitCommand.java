a13a65844cde172d757517c48e892321437b00d1
tikimonarch <tikimonarch@users.noreply.github.com>
2020-10-29 07:40:59 +0800

    Update ExitCommand.java

--- src/main/java/seedu/dietbook/command/ExitCommand.java
+++ src/main/java/seedu/dietbook/command/ExitCommand.java
@@ -19,42 +19,42 @@ public class ExitCommand extends Command {
 
     @Override
     public void execute(Manager manager, Ui ui) {
-        ActivityLevel actLvl = manager.getPerson().getActivityLevel();
-        int actLvlInt = 1;
-        if (actLvl.equals(ActivityLevel.NONE)) {
-            actLvlInt = 1;
-        } else if (actLvl.equals(ActivityLevel.LOW)) {
-            actLvlInt = 2;
-        } else if (actLvl.equals(ActivityLevel.MEDIUM)) {
-            actLvlInt = 3;
-        } else if (actLvl.equals(ActivityLevel.HIGH)) {
-            actLvlInt = 4;
-        } else if (actLvl.equals(ActivityLevel.EXTREME)) {
-            actLvlInt = 5;
-        }
+        //ActivityLevel actLvl = manager.getPerson().getActivityLevel();
+        //int actLvlInt = 1;
+        //if (actLvl.equals(ActivityLevel.NONE)) {
+        //    actLvlInt = 1;
+        //} else if (actLvl.equals(ActivityLevel.LOW)) {
+        //    actLvlInt = 2;
+        //} else if (actLvl.equals(ActivityLevel.MEDIUM)) {
+        //    actLvlInt = 3;
+        //} else if (actLvl.equals(ActivityLevel.HIGH)) {
+        //    actLvlInt = 4;
+        //} else if (actLvl.equals(ActivityLevel.EXTREME)) {
+        //    actLvlInt = 5;
+        //}
 
-        Gender gender = manager.getPerson().getGender();
-        String genderString;
-        if (gender.equals(Gender.MALE)) {
-            genderString = "Male";
-        } else if (gender.equals(Gender.FEMALE)) {
-            genderString = "Female";
-        } else {
-            genderString = "Others";
-        }
+        //Gender gender = manager.getPerson().getGender();
+        //String genderString;
+        //if (gender.equals(Gender.MALE)) {
+        //    genderString = "Male";
+        //} else if (gender.equals(Gender.FEMALE)) {
+        //    genderString = "Female";
+        //} else {
+        //    genderString = "Others";
+        //}
 
-        personSave.setActivityLevel(actLvlInt);
-        personSave.setAge(manager.getPerson().getAge());
-        personSave.setCurrentWeight(manager.getPerson().getCurrentWeight());
-        personSave.setGender(genderString);
-        personSave.setHeight(manager.getPerson().getHeight());
-        personSave.setName(manager.getPerson().getName());
-        personSave.setOriginalWeight(manager.getPerson().getOriginalWeight());
-        personSave.setTargetWeight(manager.getPerson().getTargetWeight());
-        personSave.save("UserInfo.txt");
-        foodSave.save("FoodList.txt", manager.getFoodList().getFoods());
-        ui.dataSuccessfullySavedMessage();
-        DietBook.isExit = true;
+        //personSave.setActivityLevel(actLvlInt);
+        //personSave.setAge(manager.getPerson().getAge());
+        //personSave.setCurrentWeight(manager.getPerson().getCurrentWeight());
+        //personSave.setGender(genderString);
+        //personSave.setHeight(manager.getPerson().getHeight());
+        //personSave.setName(manager.getPerson().getName());
+        //personSave.setOriginalWeight(manager.getPerson().getOriginalWeight());
+        //personSave.setTargetWeight(manager.getPerson().getTargetWeight());
+        //personSave.save("UserInfo.txt");
+        //foodSave.save("FoodList.txt", manager.getFoodList().getFoods());
+        //ui.dataSuccessfullySavedMessage();
         ui.printExitMessage();
+        DietBook.isExit = true;
     }
 }
