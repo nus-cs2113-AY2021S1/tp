dfe32eb12e05902be2f1604aa181bce4948f0452
Brendan <bwsjsg@gmail.com>
2020-10-28 23:25:08 +0800

    Added tests for delete and find

---
+++ src/test/java/command/FindCommandTest.java
@@ -0,0 +1,32 @@
+package command;
+
+import exception.DateFormatException;
+import exception.EmptyFindDateException;
+import exception.EmptyFindException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import parser.Parser;
+
+public class FindCommandTest {
+
+    @Test
+    void execute_NoDescriptionGiven_EmptyEventException() {
+        Assertions.assertThrows(EmptyFindException.class, () -> {
+            Command c = Parser.parse("find",  null);
+        });
+    }
+
+    @Test
+    void execute_NoDateGiven_EmptyFindDateException() {
+        Assertions.assertThrows(EmptyFindDateException.class, () -> {
+            Command c = Parser.parse("date",  null);
+        });
+    }
+
+    @Test
+    void execute_InvalidTimeDate_DateFormatException() {
+        Assertions.assertThrows(DateFormatException.class, () -> {
+            Command c = Parser.parse("date 2100",  null);
+        });
+    }
+}
