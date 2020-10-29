4684e5d7946b23c8b3824480483aaff7ff1d35f5
Michael Dinata <michaeldinata@Michaels-MacBook-Pro.local>
2020-10-29 22:58:23 +0800

    Bug fix on parsers

--- src/main/java/anichan/parser/AddToWatchlistParser.java
+++ src/main/java/anichan/parser/AddToWatchlistParser.java
@@ -37,6 +37,10 @@ public class AddToWatchlistParser extends CommandParser {
     public AddToWatchlistCommand parse(String description) throws AniException {
         String[] paramGiven = description.split(SPLIT_DASH, 2);
 
+        if(paramGiven[1] == null || paramGiven[1].trim().isBlank()) {
+            throw new AniException(NO_PARAMETER_PROVIDED);
+        }
+
         paramIsSetCheck(paramGiven);
         parameterParser(paramGiven);
         LOGGER.log(Level.INFO, "Parameter parsed properly");
