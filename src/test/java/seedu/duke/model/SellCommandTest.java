e02a053d986ef4f6f400a36f25d295df0f5effc8
wly99 <wang1426@e.ntu.edu.sg>
2020-10-29 09:55:48 +0800

    Added InvalidCommandTest

--- src/test/java/seedu/duke/model/SellCommandTest.java
+++ src/test/java/seedu/duke/model/SellCommandTest.java
@@ -9,13 +9,13 @@ public class SellCommandTest {
 
     @Test
     public void getSymbol_noInputRequired_symbolString() {
-        SellCommand buyCommand = new SellCommand("aapl", 10);
-        assertEquals(buyCommand.getSymbol(), "aapl");
+        SellCommand sellCommand = new SellCommand("aapl", 10);
+        assertEquals(sellCommand.getSymbol(), "aapl");
     }
 
     @Test
     public void getQuantity_noInputRequired_int() {
-        SellCommand buyCommand = new SellCommand("aapl", 10);
-        assertEquals(buyCommand.getQuantity(), 10);
+        SellCommand sellCommand = new SellCommand("aapl", 10);
+        assertEquals(sellCommand.getQuantity(), 10);
     }
 }
