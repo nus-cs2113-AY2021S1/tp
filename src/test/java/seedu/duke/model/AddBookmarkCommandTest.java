6747986e63b960fc4d7efb5a7754f057ff68a5a0
wly99 <wang1426@e.ntu.edu.sg>
2020-10-29 10:03:05 +0800

    Added AddBookMarkCommand

--- src/test/java/seedu/duke/model/AddBookmarkCommandTest.java
+++ src/test/java/seedu/duke/model/AddBookmarkCommandTest.java
@@ -1,2 +1,15 @@
-package seedu.duke.model;public class AddBookmarkCommandTest {
+package seedu.duke.model;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.command.AddBookmarkCommand;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddBookmarkCommandTest {
+
+    @Test
+    public void getSymbol_noInputRequired_symbolString() {
+        AddBookmarkCommand addBookmarkCommand = new AddBookmarkCommand("aapl");
+        assertEquals(addBookmarkCommand.getSymbol(), "aapl");
+    }
 }
