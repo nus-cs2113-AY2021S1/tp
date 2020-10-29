46f4ba61574e80e6a357917f1287580f131bca4b
lingsihui <e0426314@u.nus.edu>
2020-10-30 01:11:11 +0800

    Add Junit test for AddCategoryCommand, RemoveCategoryCommand and StarCommand

---
+++ src/test/java/bookmark/BookmarkStorageTest.java
@@ -0,0 +1,16 @@
+package bookmark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BookmarkStorageTest {
+
+    @Test
+    void loadFile() {
+    }
+
+    @Test
+    void saveLinksToFile() {
+    }
+}
