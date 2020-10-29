429b22aa5ebfd0e803ee6114b47d9b5aa0b300d1
Ong De Zhi <39303087+OngDeZhi@users.noreply.github.com>
2020-10-29 20:18:25 +0800

    Add JavaDoc and authorship

--- src/main/java/anichan/storage/StorageManager.java
+++ src/main/java/anichan/storage/StorageManager.java
@@ -63,7 +63,12 @@ public class StorageManager {
 
     // ========================== Workspace Saving ==========================
 
-    //@@author
+    /**
+     * Saves the workspace
+     *
+     * @param workspace the name of workspace
+     * @throws AniException when an error occurred while trying to save the workspace
+     */
     public void saveWorkspace(Workspace workspace) throws AniException {
         new File(storageDirectory + workspace.getName()).mkdirs();
         watchlistStorage.save(workspace.getName(), workspace.getWatchlistList());
@@ -71,6 +76,7 @@ public class StorageManager {
 
     // ========================== Workspace Deletion ==========================
 
+    //@@author
     /**
      * Deletes directory containing specified workspace.
      *
