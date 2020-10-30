b407098003e446000f450006ef70f957f78a85f2
QX-CHEN <c.qixing@yahoo.com.sg>
2020-10-30 23:02:04 +0800

    Import timetable

--- src/main/java/seedu/storage/Storage.java
+++ src/main/java/seedu/storage/Storage.java
@@ -3,6 +3,7 @@ package seedu.storage;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import seedu.data.TaskMap;
+import seedu.task.Priority;
 import seedu.task.Task;
 
 import java.io.File;
@@ -10,6 +11,13 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Storage {
@@ -33,10 +41,12 @@ public class Storage {
     /**
      * Load data from file and add tasks to TaskList.
      */
-    public TaskMap loadTasks() throws IOException {
+    public TaskMap loadTasks() throws IOException, ParseException {
         // If both dir and file are newly created, return empty taskMap.
         if (!createDirectory()) {
-            return readTasksFromFile();
+            TaskMap taskMap = readTasksFromFile();
+            readTasksFromTimetable(taskMap);
+            return taskMap;
         }
         return new TaskMap();
     }
@@ -71,5 +81,59 @@ public class Storage {
             tasks.addTask(gson.fromJson(scanner.nextLine(), type));
         }
         return tasks;
+    }
+
+
+    private void readTasksFromTimetable(TaskMap taskMap) throws FileNotFoundException, ParseException {
+        File file = new File("data/nusmods_calendar.ics");
+        if (file.exists()) {
+            Task task;
+            String description;
+            LocalDate localDate = LocalDate.now();
+            LocalTime startTime = LocalTime.now();
+            LocalTime endTime = LocalTime.now();
+            Priority priority = Priority.LOW;
+
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String currentLine = scanner.nextLine();
+                String temp;
+                if (currentLine.startsWith("DTSTART:")) {
+                    temp = currentLine.replace("DTSTART:", "");
+                    DateFormat df = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+                    Date date = df.parse(temp);
+                    localDate = date.toInstant().atZone(ZoneId.of("+16")).toLocalDate();
+                    startTime = date.toInstant().atZone(ZoneId.of("+16")).toLocalTime();
+                } else if (currentLine.startsWith("DTEND")) {
+                    temp = currentLine.replace("DTEND:", "");
+                    DateFormat df = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+                    Date date = df.parse(temp);
+                    endTime = date.toInstant().atZone(ZoneId.of("+16")).toLocalTime();
+                } else if (currentLine.startsWith("SUMMARY:")) {
+                    description = currentLine.replace("SUMMARY:", "");
+                    if (description.contains("Exam")) {
+                        priority = Priority.HIGH;
+                    }
+                    task = new Task(description, localDate, startTime, endTime, priority);
+                    addTaskToTaskmap(taskMap, task);
+                }
+            }
+        }
+    }
+
+    private void addTaskToTaskmap(TaskMap taskMap, Task task) {
+        if (task.getDescription().contains("Exam")) {
+            taskMap.addTask(task);
+        } else {
+            for (int i = 0; i < 14; i++) {
+                if (i == 6) {
+                    continue;
+                }
+                LocalDate date = task.getDate().plusDays(i * 7);
+                Task tempTask = new Task(task.getDescription(), date,
+                        task.getStartTime(), task.getEndTime(), task.getPriority());
+                taskMap.addTask(tempTask);
+            }
+        }
     }
 }
