3af090953318d1d5979346ef1d4f4a17fded33e9
0xZ3RO <>
2020-10-31 22:10:47 +0800

    Fix style

--- src/main/java/seedu/duke/ui/UI.java
+++ src/main/java/seedu/duke/ui/UI.java
@@ -113,8 +113,8 @@ public class UI {
 
     public static void printHelp() {
         String[] listCommands = {"help", "divider", "bunny", "list bunny", "filter bunny", "save bunny",
-                "list", "list filter words", "start", "filter words", "stats", "reset", "name",
-                "list name", "filter name", "add name", "delete name", "remind", "clear", "exit"};
+            "list", "list filter words", "start", "filter words", "stats", "reset", "name",
+            "list name", "filter name", "add name", "delete name", "remind", "clear", "exit"};
         System.out.println("Type 'help <function name here>' to view help for each command.");
         System.out.println("Available commands:");
         for (String listCommand : listCommands) {
