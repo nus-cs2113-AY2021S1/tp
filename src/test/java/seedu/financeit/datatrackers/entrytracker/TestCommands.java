355a40d9427f48b635e6a331a39dff9d00412a5c
kaiwen98 <e0406836@u.nus.edu>
2020-11-01 02:17:44 +0800

    Improved Junit testing for manual tracker and entry tracker

--- src/test/java/seedu/financeit/datatrackers/entrytracker/TestCommands.java
+++ src/test/java/seedu/financeit/datatrackers/entrytracker/TestCommands.java
@@ -3,6 +3,7 @@ package seedu.financeit.datatrackers.entrytracker;
 import seedu.financeit.common.CommandPacket;
 import seedu.financeit.testutil.TestUtil;
 import seedu.financeit.utils.ParamChecker;
+import seedu.financeit.utils.RandomGenerator;
 
 import static seedu.financeit.utils.ParamChecker.PARAM_AMOUNT;
 import static seedu.financeit.utils.ParamChecker.PARAM_CATEGORY;
@@ -13,14 +14,22 @@ import static seedu.financeit.utils.ParamChecker.PARAM_TIME;
 
 public class TestCommands {
     public static CommandPacket generateCreateEntryErrorCommand(String wrongParam) {
+        int seed = RandomGenerator.generateRandomPositiveIntUntilLimit(3);
         String paramTime = (wrongParam == "time")
-            ? "5600"
-            : "1500";
+            ? (seed == 0) ? "5600"
+            : (seed == 1) ? "-1200"
+            : " -&&*&*(-"
+            : "1500" ;
+
         String paramCat = (wrongParam == "cat")
             ? "wrong"
             : "tpt";
+
         String paramAmt = (wrongParam == "amt")
-            ? String.format("%f", (Double.MAX_VALUE - 1))
+            ? (seed == 0) ? String.format("%f", (Double.MAX_VALUE - 1))
+            : (seed == 1) ? "-1200"
+            : (seed == 2) ? " -&&*&*(-"
+            : "4e2"
             : "5600";
 
         System.out.println("lol: " + paramAmt.length());
@@ -67,12 +76,17 @@ public class TestCommands {
     }
 
     public static CommandPacket generateDeleteEntryByIdErrorCommand() {
+        int seed = RandomGenerator.generateRandomPositiveIntUntilLimit(3);
+        String wrongInput = (seed == 1) ? "-1000000000000"
+            : (seed == 2) ? "100000000000000"
+            : (seed == 3) ? "-5":
+            "4e2";
         CommandPacket testPacket = TestUtil.createCommandPacket(
             "delete",
             new String[][] {
                 {
                     PARAM_INDEX,
-                    "100000000000000"
+                    wrongInput
                 }
             }
         );
@@ -81,9 +95,9 @@ public class TestCommands {
 
     public static CommandPacket generateCreateEntryCorrectCommand(int seed) {
         // Generates entries with time range from 0000 to 2400 in 24-h intervals.
-        String paramTime = ("".concat(String.valueOf(seed % 24)));
+        String paramTime = "1500";
         String paramCat = "tpt";
-        String paramAmt = "1500";
+        String paramAmt = String.valueOf(1500 + seed);
         CommandPacket testPacket = TestUtil.createCommandPacket(
             "edit",
             new String[][] {
@@ -114,9 +128,9 @@ public class TestCommands {
 
     public static CommandPacket generateEditEntryCorrectCommand(int seed) {
         // Generates entries with time range from 0000 to 2400 in 24-h intervals.
-        String paramTime = ("".concat(String.valueOf(seed % 24)));
+        String paramTime = "1500";
         String paramCat = "tpt";
-        String paramAmt = "1500";
+        String paramAmt = String.valueOf(1500 + seed);
         CommandPacket testPacket = TestUtil.createCommandPacket(
             "edit",
             new String[][] {
