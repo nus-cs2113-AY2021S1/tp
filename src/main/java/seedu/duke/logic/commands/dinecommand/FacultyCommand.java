00b9378dc5c2458f57e57c85f3630fd2ac15c7a1
mrwsy1 <e0406495@u.nus.edu>
2020-10-31 16:49:38 +0800

    Modify /faculty command

--- src/main/java/seedu/duke/logic/commands/dinecommand/FacultyCommand.java
+++ src/main/java/seedu/duke/logic/commands/dinecommand/FacultyCommand.java
@@ -1,2 +1,22 @@
-package seedu.duke.logic.commands.dinecommand;public class FacultyCommand {
+package seedu.duke.logic.commands.dinecommand;
+
+import seedu.duke.logic.commands.commons.Command;
+import seedu.duke.model.foodoptions.FoodPlace;
+import seedu.duke.model.foodoptions.FoodPlacesData;
+
+import java.util.ArrayList;
+
+import static seedu.duke.ui.Ui.printFacultyResult;
+import static seedu.duke.ui.Ui.printLine;
+
+public class FacultyCommand extends Command {
+
+    @Override
+    public void executeCommand() {
+        printLine();
+        ArrayList<FoodPlace> foodPlaceList = FoodPlacesData.getDiningOptionsInNus();
+        printFacultyResult(foodPlaceList);
+        printLine();
+    }
+
 }
