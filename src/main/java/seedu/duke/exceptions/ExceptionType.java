f24dd13e2da4d9fccd1d1b30260a0216d19a1394
wamikamalik <wamikamalik@hotmail.com>
2020-10-31 13:49:59 +0400

    no busStop exception

--- src/main/java/seedu/duke/exceptions/ExceptionType.java
+++ src/main/java/seedu/duke/exceptions/ExceptionType.java
@@ -8,6 +8,11 @@ public enum ExceptionType {
     MISSING_BUS_CODE("Oh no! You seem to have missed typing in the bus code!"),
     SAME_LOCATIONS("You don't need a bus to get there...You are right there!"),
     INVALID_BUS_STOP("Oh no! I do not understand which bus stop you are looking for."),
+    INVALID_START_LOC("The starting location you have provided is not the name of any bus stop in our\n"
+            + "database :("),
+    INVALID_DEST("The destination you have provided is not the name of any bus stop in our database :("),
+    INVALID_LOCATIONS("The starting location and destination you have provided are not the names of any bus\n"
+            + "stops in our database :("),
     INVALID_FACULTY("Oh no! Please enter a faculty."),
     INVALID_FOODPLACE("Oh no! I cannot detect any inputs."),
     INVALID_FAVOURITE("Oh no! I cannot seem to detect the command you want to store as your favourite."),
