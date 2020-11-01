b0ea4550dcb032dc14b555f8536759f9f6f08470
hungvo0603 <hungvo1011@gmail.com>
2020-11-01 16:12:39 +0800

    Add parse DateTime exception

--- src/main/java/seedu/duke/reminder/WritingReminder.java
+++ src/main/java/seedu/duke/reminder/WritingReminder.java
@@ -1,11 +1,11 @@
 package seedu.duke.reminder;
 
-import seedu.duke.ui.UI;
 import seedu.duke.writing.WritingList;
 import seedu.duke.writing.Writings;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
@@ -17,21 +17,25 @@ public class WritingReminder {
         for (int i = 0; i < words.length; i++) {
             words[i] = words[i].trim();
         }
-        LocalDate date = LocalDate.parse(words[1], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        ArrayList<Writings> writingsOnThisDate = (ArrayList<Writings>) writings.stream()
-                .filter((s) -> s.getReminderDate().compareTo(date) == 0)
-                .collect(Collectors.toList());
-        if (writingsOnThisDate.size() == 0) {
-            System.out.println("You don't have any writings due that day!!!");
-        } else {
-            System.out.printf("On %s, you should continue on the following writing(s):\n",
-                    date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-        }
-        int filterCount = 1;
-        for (Writings w : writingsOnThisDate) {
-            System.out.println(filterCount + ".");
-            w.printWritingsReminder();
-            filterCount++;
+        try {
+            LocalDate date = LocalDate.parse(words[1], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            ArrayList<Writings> writingsOnThisDate = (ArrayList<Writings>) writings.stream()
+                    .filter((s) -> s.getReminderDate().compareTo(date) == 0)
+                    .collect(Collectors.toList());
+            if (writingsOnThisDate.size() == 0) {
+                System.out.println("You don't have any writings due that day!!!");
+            } else {
+                System.out.printf("On %s, you should continue on the following writing(s):\n",
+                        date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            }
+            int filterCount = 1;
+            for (Writings w : writingsOnThisDate) {
+                System.out.println(filterCount + ".");
+                w.printWritingsReminder();
+                filterCount++;
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Cannot parse date and time. Please enter date in a correct format.");
         }
     }
 }
