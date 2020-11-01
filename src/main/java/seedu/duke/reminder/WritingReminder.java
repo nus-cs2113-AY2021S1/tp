a743ac26177ab9fa528b9852d94b18e85362ca14
hungvo0603 <hungvo1011@gmail.com>
2020-11-01 16:37:41 +0800

    Refactor messages

--- src/main/java/seedu/duke/reminder/WritingReminder.java
+++ src/main/java/seedu/duke/reminder/WritingReminder.java
@@ -1,5 +1,6 @@
 package seedu.duke.reminder;
 
+import seedu.duke.constants.FluffleMessages;
 import seedu.duke.writing.WritingList;
 import seedu.duke.writing.Writings;
 
@@ -8,6 +9,8 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+
+import static seedu.duke.constants.InputFormat.INPUT_DATE_FORMAT;
 
 public class WritingReminder {
     private static ArrayList<Writings> writings = WritingList.writinglist;
@@ -18,15 +21,15 @@ public class WritingReminder {
             words[i] = words[i].trim();
         }
         try {
-            LocalDate date = LocalDate.parse(words[1], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            LocalDate date = LocalDate.parse(words[1], DateTimeFormatter.ofPattern(INPUT_DATE_FORMAT));
             ArrayList<Writings> writingsOnThisDate = (ArrayList<Writings>) writings.stream()
                     .filter((s) -> s.getReminderDate().compareTo(date) == 0)
                     .collect(Collectors.toList());
             if (writingsOnThisDate.size() == 0) {
-                System.out.println("You don't have any writings due that day!!!");
+                System.out.println(FluffleMessages.NO_WRITING_DUE);
             } else {
-                System.out.printf("On %s, you should continue on the following writing(s):\n",
-                        date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+                System.out.printf(FluffleMessages.CONTINUE_WRITINGS,
+                        date.format(DateTimeFormatter.ofPattern(INPUT_DATE_FORMAT)));
             }
             int filterCount = 1;
             for (Writings w : writingsOnThisDate) {
@@ -35,7 +38,7 @@ public class WritingReminder {
                 filterCount++;
             }
         } catch (DateTimeParseException e) {
-            System.out.println("Cannot parse date and time. Please enter date in a correct format.");
+            System.out.println(FluffleMessages.PARSE_DATETIME_EXCEPTION);
         }
     }
 }
