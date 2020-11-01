b8863b51198f5f8bc5541d3f71af512bedd3e61b
tys <tanyushing@outlook.sg>
2020-11-01 21:33:22 +0800

    Signed-off-by: tys <tanyushing@outlook.sg>

--- src/main/java/seedu/zoomaster/Ui.java
+++ src/main/java/seedu/zoomaster/Ui.java
@@ -411,7 +411,8 @@ public class Ui {
 
                 printYellow("You can also add bookmarks tagged to the entire module" + NEW_LINE);
                 printCyan("Format: add {module} {description} {URL}" + NEW_LINE);
-                printGreen("eg. add CS2113 homepage https://nus-cs2113-ay2021s1.github.io/website/index.html" + NEW_LINE);
+                printGreen("eg. add CS2113 homepage https://nus-cs2113-ay2021s1.github.io/website/index.html"
+                        + NEW_LINE);
                 System.out.println(LINE);
                 break;
             case DeleteSlotCommand.DEL_KW:
@@ -492,7 +493,8 @@ public class Ui {
 
                 printYellow("You can also add bookmarks tagged to the entire module" + NEW_LINE);
                 printCyan("Format: add {module} {description} {URL}" + NEW_LINE);
-                printGreen("eg. add CS2113 homepage https://nus-cs2113-ay2021s1.github.io/website/index.html" + NEW_LINE);
+                printGreen("eg. add CS2113 homepage https://nus-cs2113-ay2021s1.github.io/website/index.html"
+                        + NEW_LINE);
                 System.out.println(LINE);
                 break;
             case LoadPlannerCommand.LOAD_KW:
