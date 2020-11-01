f83cec67c444f4e72cebb3f37e4b260a7f2b6326
Lezn0 <yuxinwu1234@gmail.com>
2020-11-01 10:26:29 +0800

    Add isValid checks

--- src/main/java/seedu/duke/logic/commands/favcommand/AddFavCommand.java
+++ src/main/java/seedu/duke/logic/commands/favcommand/AddFavCommand.java
@@ -20,6 +20,7 @@ public class AddFavCommand extends Command {
     public AddFavCommand(String input, String description) {
         this.input = input;
         this.description = description;
+        super.isValid = false;
     }
 
     @Override
@@ -43,6 +44,7 @@ public class AddFavCommand extends Command {
         if (!FavList.contains(item)) {
             FavList.addFav(item);
             addFavMessage(input);
+            isValid = true;
         }
     }
 }
