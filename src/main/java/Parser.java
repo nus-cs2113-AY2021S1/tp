964aa6b411bd414e8bcefc4bf037f0ad104b9711
Feng Qingyu <louisfqy@gmail.com>
2020-11-01 13:34:17 +0800

    v2.0

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,11 +1,11 @@
 public class Parser {//name week time
-    public static Customer parseCustomer(String inputMessage,int customerID) {
+    public static Customer parseCustomer(String inputMessage) {
         String[] inputWords = inputMessage.split("/"); //split the input message
         String customerName = inputWords[0];
         String dayOfWeek = inputWords[1];
         String arriveTime = inputWords[2];
 
 
-        return new Customer(customerName,customerID,Integer.parseInt(arriveTime),Integer.parseInt(dayOfWeek));
+        return new Customer(customerName,1,Integer.parseInt(arriveTime),Integer.parseInt(dayOfWeek));
     }
 }
