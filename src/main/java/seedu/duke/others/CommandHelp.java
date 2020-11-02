9027433b38c8ae1d1c2e8a45e0d589ee9c1abc34
YeYutong <E0425083@u.nus.edu>
2020-11-03 00:03:24 +0800

    Added changInfo into help function

--- src/main/java/seedu/duke/others/CommandHelp.java
+++ src/main/java/seedu/duke/others/CommandHelp.java
@@ -10,16 +10,17 @@ public class CommandHelp extends Command {
                 + "\t1. Add members:  hr add /n NAME /p PHONE_NUMBER /e EMAIL /r ROLE\n"
                 + "\t2. Delete member: hr delete index\n"
                 + "\t3. View hr list: hr list\n"
-                + "\t4. Add events: event addEvent /n EVENT_NAME /d EVENT_DATE <YYYY-MM-dd> /t EVENT_TIME <HH-mm>\n"
-                + "\t5. Delete event: event delEvent EVENT_INDEX\n"
-                + "\t6. View event list: event listEvent\n"
-                + "\t7. View countdown to events: event countdown\n"
-                + "\t8. Search for an event by NAME or DATE<YYYY-MM-DD>: event search /s KEYWORD\n"
-                + "\t9. Mark an event as done: event done EVENT_INDEX\n"
-                + "\t10. Add financial log: finance addLog ITEM_NAME ITEM_VALUE\n"
-                + "\t11. Delete financial log:  finance delLog ITEM_INDEX\n"
-                + "\t12. View financial summary: finance summary\n"
-                + "\t13. Exit the program: bye\n";
+                + "\t4. Change member info: hr changeInfo /n NAME (/p ITEM) (/e ITEM) (/r ITEM)\n"
+                + "\t5. Add events: event addEvent /n EVENT_NAME /d EVENT_DATE <YYYY-MM-dd> /t EVENT_TIME <HH-mm>\n"
+                + "\t6. Delete event: event delEvent EVENT_INDEX\n"
+                + "\t7. View event list: event listEvent\n"
+                + "\t8. View countdown to events: event countdown\n"
+                + "\t9. Search for an event by NAME or DATE<YYYY-MM-DD>: event search /s KEYWORD\n"
+                + "\t10. Mark an event as done: event done EVENT_INDEX\n"
+                + "\t11. Add financial log: finance addLog ITEM_NAME ITEM_VALUE\n"
+                + "\t12. Delete financial log:  finance delLog ITEM_INDEX\n"
+                + "\t13. View financial summary: finance summary\n"
+                + "\t14. Exit the program: bye\n";
     }
 
     @Override
