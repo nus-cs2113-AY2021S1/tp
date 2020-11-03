b8156fccccd34d5f96e3f84072cbb90a4f9b8ef3
Zong Xian <zongxian96@gmail.com>
2020-11-04 03:23:21 +0800

    Add comments and changed the order of Parser Class

--- src/main/java/seedu/smarthomebot/logic/parser/Parser.java
+++ src/main/java/seedu/smarthomebot/logic/parser/Parser.java
@@ -28,181 +28,21 @@ import static seedu.smarthomebot.commons.Messages.MESSAGE_INVALID_LIST_COMMAND;
 import static seedu.smarthomebot.commons.Messages.MESSAGE_POWER_EXCEEDED;
 import static seedu.smarthomebot.commons.Messages.MESSAGE_VALUE_NOT_NUMBER;
 
+/**
+ * Parses user input.
+ */
 public class Parser {
 
     private static final String APPLIANCE_TYPE = "appliance";
     private static final String LOCATION_TYPE = "location";
 
-    private static Command prepareOnCommand(String arguments) {
-        int indexParameter = arguments.indexOf("p/");
-        String name;
-        String parameter;
-        try {
-            if (indexParameter < 1) {
-                name = arguments;
-                if (checkForEmptyInput(name)) {
-                    throw new EmptyParameterException();
-                }
-                parameter = "";
-            } else {
-                name = arguments.substring(0, indexParameter).trim();
-                parameter = arguments.substring(indexParameter + 2).toLowerCase().trim();
-                if (checkForEmptyInput(name) || checkForEmptyInput(parameter)) {
-                    throw new EmptyParameterException();
-                }
-                convertParameterToInt(parameter);
-            }
-            return new OnCommand(name, parameter);
-        } catch (EmptyParameterException e) {
-            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
-        } catch (InvalidNumericalValueException e) {
-            return new InvalidCommand(MESSAGE_VALUE_NOT_NUMBER);
-        }
-    }
-
-    private static Command prepareOffCommand(String arguments) {
-        try {
-            if (checkForEmptyInput(arguments)) {
-                throw new EmptyParameterException();
-            }
-            return new OffCommand(arguments);
-        } catch (EmptyParameterException e) {
-            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
-
-        }
-    }
-
-    private static void convertParameterToInt(String parameter) throws InvalidNumericalValueException {
-        try {
-            Integer.parseInt(parameter);
-        } catch (NumberFormatException e) {
-            throw new InvalidNumericalValueException();
-        }
-
-    }
-
-    private static Command prepareAddCommand(String arguments) {
-        int indexLocation = arguments.indexOf("l/");
-        int indexPower = arguments.indexOf("w/");
-        int indexType = arguments.indexOf("t/");
-        String name;
-        String location;
-        String wattage;
-        String type;
-
-        try {
-            if (indexLocation < indexPower && indexPower < indexType) {
-                name = arguments.substring(0, indexLocation).trim();
-                location = arguments.substring(indexLocation + 2, indexPower).trim();
-                wattage = arguments.substring(indexPower + 2, indexType).trim();
-                type = arguments.substring(indexType + 2).toLowerCase().trim();
-
-                if (checkForEmptyInput(name) | checkForEmptyInput(location)
-                        | checkForEmptyInput(wattage) | checkForEmptyInput(type)) {
-                    throw new EmptyParameterException();
-                }
-                if (checkForIllegalCharacter(name)) {
-                    throw new IllegalCharacterException();
-                }
-                testWattageValidity(wattage);
-            } else {
-                throw new InvalidCommandException();
-            }
-            return new AddCommand(name, location, wattage, type);
-
-        } catch (InvalidCommandException | StringIndexOutOfBoundsException e) {
-            return new InvalidCommand(MESSAGE_INVALID_ADD_COMMAND);
-        } catch (InvalidNumericalValueException e) {
-            return new InvalidCommand(MESSAGE_VALUE_NOT_NUMBER);
-        } catch (WattageExceedException e) {
-            return new InvalidCommand(MESSAGE_POWER_EXCEEDED);
-        } catch (EmptyParameterException e) {
-            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
-        } catch (IllegalCharacterException e) {
-            return new InvalidCommand(MESSAGE_ILLEGAL_CHARACTER + " [APPLIANCE_NAME].");
-        }
-
-    }
-
-    private static Command prepareListCommand(String arguments) {
-        String[] str = arguments.split(" ");
-        if (str[0].equals(LOCATION_TYPE)) {
-            return new ListCommand(LOCATION_TYPE, "");
-        } else if (str[0].equals(APPLIANCE_TYPE)) {
-            if (arguments.equals(APPLIANCE_TYPE)) {
-                return new ListCommand(APPLIANCE_TYPE, "");
-            } else if (str[1].trim().startsWith("l/")) {
-                return new ListCommand(APPLIANCE_TYPE, str[1].trim().substring(2));
-            } else {
-                return new InvalidCommand(MESSAGE_INVALID_LIST_COMMAND);
-            }
-        } else {
-            return new InvalidCommand(MESSAGE_INVALID_LIST_COMMAND);
-        }
-    }
-
-    private static void testWattageValidity(String wattage) throws WattageExceedException,
-            InvalidNumericalValueException {
-        try {
-            int wattageValue = Integer.parseInt(wattage);
-            // Common appliance is between 1 to 9999 watts
-            if ((wattageValue < 1) || (wattageValue > 9999)) {
-                throw new WattageExceedException();
-            }
-        } catch (NumberFormatException e) {
-            throw new InvalidNumericalValueException();
-        }
-    }
-
-    private static boolean checkForEmptyInput(String input) {
-        return (input.isEmpty());
-    }
-
-    private static boolean checkForIllegalCharacter(String input) {
-        return (input.contains(" ") || input.contains("/") || input.contains("|"));
-    }
-
-    private Command prepareCreateCommand(String arguments) {
-        try {
-            if (checkForEmptyInput(arguments)) {
-                throw new EmptyParameterException();
-            }
-            if (checkForIllegalCharacter(arguments)) {
-                throw new IllegalCharacterException();
-            }
-            return new CreateCommand(arguments);
-        } catch (EmptyParameterException e) {
-            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
-        } catch (IllegalCharacterException e) {
-            return new InvalidCommand(MESSAGE_ILLEGAL_CHARACTER + " [LOCATION_NAME].");
-        }
-
-    }
-
-    private Command prepareDeleteCommand(String arguments) {
-        try {
-            if (checkForEmptyInput(arguments)) {
-                throw new EmptyParameterException();
-            }
-            return new DeleteCommand(arguments);
-        } catch (EmptyParameterException e) {
-            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
-        }
-
-    }
-
-    private Command prepareRemoveCommand(String arguments) {
-        try {
-            if (checkForEmptyInput(arguments)) {
-                throw new EmptyParameterException();
-            }
-            return new RemoveCommand(arguments);
-        } catch (EmptyParameterException e) {
-            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
-        }
-
-    }
-
+    //@@author zongxian-ctrl
+    /**
+     * Parses user input into command for execution.
+     *
+     * @param userInput full user input string
+     * @return command based on the user input
+     */
     public Command parseCommand(String userInput) {
         String[] words = userInput.trim().split(" ", 2);
         final String commandWord = words[0];
@@ -235,6 +75,248 @@ public class Parser {
             return new InvalidCommand(MESSAGE_INVALID_COMMAND_FORMAT);
         }
 
+    }
+
+    /**
+     * Parses arguments for CreateCommand by checking if arguments is valid.
+     *
+     * @param arguments parameter of CreateCommand
+     * @return the prepared CreateCommand
+     */
+    private Command prepareCreateCommand(String arguments) {
+        try {
+            if (isEmptyInput(arguments)) {
+                throw new EmptyParameterException();
+            }
+            if (hasIllegalCharacter(arguments)) {
+                throw new IllegalCharacterException();
+            }
+            return new CreateCommand(arguments);
+        } catch (EmptyParameterException e) {
+            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
+        } catch (IllegalCharacterException e) {
+            return new InvalidCommand(MESSAGE_ILLEGAL_CHARACTER + " [LOCATION_NAME].");
+        }
+
+    }
+
+    /**
+     * Parses arguments for RemoveCommand by checking if arguments is valid.
+     *
+     * @param arguments parameter of RemoveCommand
+     * @return the prepared RemoveCommand
+     */
+    private Command prepareRemoveCommand(String arguments) {
+        try {
+            if (isEmptyInput(arguments)) {
+                throw new EmptyParameterException();
+            }
+            return new RemoveCommand(arguments);
+        } catch (EmptyParameterException e) {
+            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
+        }
+
+    }
+
+    /**
+     * Parses arguments into AddCommand format.
+     *
+     * @param arguments parameters of AddComand
+     * @return the prepared AddCommand
+     */
+    private static Command prepareAddCommand(String arguments) {
+        int indexLocation = arguments.indexOf("l/");
+        int indexPower = arguments.indexOf("w/");
+        int indexType = arguments.indexOf("t/");
+        String name;
+        String location;
+        String wattage;
+        String type;
+
+        try {
+            if (indexLocation < indexPower && indexPower < indexType) {
+                name = arguments.substring(0, indexLocation).trim();
+                location = arguments.substring(indexLocation + 2, indexPower).trim();
+                wattage = arguments.substring(indexPower + 2, indexType).trim();
+                type = arguments.substring(indexType + 2).toLowerCase().trim();
+
+                if (isEmptyInput(name) | isEmptyInput(location)
+                        | isEmptyInput(wattage) | isEmptyInput(type)) {
+                    throw new EmptyParameterException();
+                }
+                if (hasIllegalCharacter(name)) {
+                    throw new IllegalCharacterException();
+                }
+                testWattageValidity(wattage);
+            } else {
+                throw new InvalidCommandException();
+            }
+            return new AddCommand(name, location, wattage, type);
+
+        } catch (InvalidCommandException | StringIndexOutOfBoundsException e) {
+            return new InvalidCommand(MESSAGE_INVALID_ADD_COMMAND);
+        } catch (InvalidNumericalValueException e) {
+            return new InvalidCommand(MESSAGE_VALUE_NOT_NUMBER);
+        } catch (WattageExceedException e) {
+            return new InvalidCommand(MESSAGE_POWER_EXCEEDED);
+        } catch (EmptyParameterException e) {
+            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
+        } catch (IllegalCharacterException e) {
+            return new InvalidCommand(MESSAGE_ILLEGAL_CHARACTER + " [APPLIANCE_NAME].");
+        }
+
+    }
+
+    /**
+     * Parses arguments for DeleteCommand by checking if arguments is valid.
+     *
+     * @param arguments parameter of DeleteCommand
+     * @return the prepared DeleteCommand
+     */
+    private Command prepareDeleteCommand(String arguments) {
+        try {
+            if (isEmptyInput(arguments)) {
+                throw new EmptyParameterException();
+            }
+            return new DeleteCommand(arguments);
+        } catch (EmptyParameterException e) {
+            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
+        }
+
+    }
+
+    //@@author
+    /**
+     * Parses arguments into OnCommand format.
+     *
+     * @param arguments parameters of OnCommand
+     * @return the prepared OnCommand
+     */
+    private static Command prepareOnCommand(String arguments) {
+        int indexParameter = arguments.indexOf("p/");
+        String name;
+        String parameter;
+        try {
+            if (indexParameter < 1) {
+                name = arguments;
+                if (isEmptyInput(name)) {
+                    throw new EmptyParameterException();
+                }
+                parameter = "";
+            } else {
+                name = arguments.substring(0, indexParameter).trim();
+                parameter = arguments.substring(indexParameter + 2).toLowerCase().trim();
+                if (isEmptyInput(name) || isEmptyInput(parameter)) {
+                    throw new EmptyParameterException();
+                }
+                checkIfParameterIsInt(parameter);
+            }
+            return new OnCommand(name, parameter);
+        } catch (EmptyParameterException e) {
+            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
+        } catch (InvalidNumericalValueException e) {
+            return new InvalidCommand(MESSAGE_VALUE_NOT_NUMBER);
+        }
+    }
+
+    //@@author
+    /**
+     * Parses arguments into OffCommand format.
+     *
+     * @param arguments parameters of OffCommand
+     * @return the prepared OffCommand
+     */
+    private static Command prepareOffCommand(String arguments) {
+        try {
+            if (isEmptyInput(arguments)) {
+                throw new EmptyParameterException();
+            }
+            return new OffCommand(arguments);
+        } catch (EmptyParameterException e) {
+            return new InvalidCommand(MESSAGE_EMPTY_PARAMETER);
+
+        }
+    }
+
+    //@@author
+    /**
+     * Parses arguments into ListCommand format.
+     *
+     * @param arguments parameters of ListCommand
+     * @return the prepared ListCommand
+     */
+    private static Command prepareListCommand(String arguments) {
+        String[] str = arguments.split(" ");
+        if (str[0].equals(LOCATION_TYPE)) {
+            return new ListCommand(LOCATION_TYPE, "");
+        } else if (str[0].equals(APPLIANCE_TYPE)) {
+            if (arguments.equals(APPLIANCE_TYPE)) {
+                return new ListCommand(APPLIANCE_TYPE, "");
+            } else if (str[1].trim().startsWith("l/")) {
+                return new ListCommand(APPLIANCE_TYPE, str[1].trim().substring(2));
+            } else {
+                return new InvalidCommand(MESSAGE_INVALID_LIST_COMMAND);
+            }
+        } else {
+            return new InvalidCommand(MESSAGE_INVALID_LIST_COMMAND);
+        }
+    }
+
+    //@@author
+    /**
+     * Checks if the parameter is numerical value.
+     *
+     * @param parameter parameter entered by user
+     * @throws InvalidNumericalValueException if the parameter is not numerical value
+     */
+    private static void checkIfParameterIsInt(String parameter) throws InvalidNumericalValueException {
+        try {
+            Integer.parseInt(parameter);
+        } catch (NumberFormatException e) {
+            throw new InvalidNumericalValueException();
+        }
+
+    }
+
+    //@@author
+    /**
+     * Checks if the wattage entered by the user is valid.
+     * @param wattage parameter entered by user
+     * @throws WattageExceedException if the wattage is less 1 or more than 9999
+     * @throws InvalidNumericalValueException if the wattage is not an numerical value
+     */
+    private static void testWattageValidity(String wattage) throws WattageExceedException,
+            InvalidNumericalValueException {
+        try {
+            int wattageValue = Integer.parseInt(wattage);
+            // Common appliance is between 1 to 9999 watts
+            if ((wattageValue < 1) || (wattageValue > 9999)) {
+                throw new WattageExceedException();
+            }
+        } catch (NumberFormatException e) {
+            throw new InvalidNumericalValueException();
+        }
+    }
+
+    //@@author zongxian-ctrl
+    /**
+     * Checks if the input is empty.
+     *
+     * @param input parameter entered by the user
+     * @return true if input is empty
+     */
+    private static boolean isEmptyInput(String input) {
+        return (input.isEmpty());
+    }
+
+    /**
+     * Check if the input contains illegal character.
+     *
+     * @param input parameter entered by the user
+     * @return true if any illegal character is found
+     */
+    private static boolean hasIllegalCharacter(String input) {
+        return (input.contains(" ") || input.contains("/") || input.contains("|"));
     }
 
 }
