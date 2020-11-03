51c2078fd4da9f0d0c1f65a6d8d124eadc6f9268
hui444 <ygreek@gmail.com>
2020-11-03 17:12:44 +0800

    Change symbols, add JUnit tests for Goal

---
+++ src/test/java/fitr/goal/GoalTest.java
@@ -0,0 +1,23 @@
+package fitr.goal;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static fitr.common.DateManager.getCurrentDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoalTest {
+
+    @Test
+    public void getDescription_validGoal_success() {
+        Goal tempGoal = new Goal(getCurrentDate(), "exercise", "run more");
+        assertEquals("run more", tempGoal.getDescription());
+    }
+
+    @Test
+    public void getGoalType_validGoal_success() {
+        Goal tempGoal = new Goal(getCurrentDate(), "food", "Y", "eat clean");
+        assertEquals("food", tempGoal.getGoalType());
+    }
+}
