57e7295db139700a32d57a535e5e177dd91f36fe
tys <tanyushing@outlook.sg>
2020-11-05 10:07:55 +0800

    Signed-off-by: tys <tanyushing@outlook.sg>

--- src/test/java/seedu/zoomaster/ZoomasterTest.java
+++ src/test/java/seedu/zoomaster/ZoomasterTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ZoomasterTest {
+    Zoomaster test;
+
     @Test
-    public void sampleTest() {
+    public void defaultTest() {
+        test = new Zoomaster("./data/bookmarks.txt", "./data/timetable.txt");
+
         assertTrue(true);
     }
 }
