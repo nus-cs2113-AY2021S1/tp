e032a946a8ac27ee89ff9fb1245c1846e37bbdab
LIU_YIFENG <liuyifeng.smc@gmail.com>
2020-11-05 18:34:26 +0800

    update checks on invalide recurring number

--- src/main/java/seedu/duke/command/AddCommand.java
+++ src/main/java/seedu/duke/command/AddCommand.java
@@ -159,8 +159,8 @@ public class AddCommand extends Command {
         if (moduleCode.isEmpty()) {
             throw new CommandException("lab");
         }  else if (recurringCount < 0 || recurringCount > 13) {
-            System.out.println("The number of lab session can only be in the range of 0 to 13. \n");
-            throw new CommandException("invalid recurring number");
+            System.out.println("Warning! The number of lab session can only be in the range of 0 to 13. \n");
+            throw new CommandException("lab");
         } else {
             for (int i = 0; i < recurringCount; i++) {
                 calendarList.addEvent(new Lab(moduleCode, date, time, venue));
@@ -196,8 +196,8 @@ public class AddCommand extends Command {
         if (moduleCode.isEmpty()) {
             throw new CommandException("tutorial");
         } else if (recurringCount < 0 || recurringCount > 13) {
-            System.out.println("The number of tutorial can only be in the range of 0 to 13. \n");
-            throw new CommandException("invalid recurring number");
+            System.out.println("Warning! The number of tutorial can only be in the range of 0 to 13. \n");
+            throw new CommandException("tutorial");
         } else {
             for (int i = 0; i < recurringCount; i++) {
                 calendarList.addEvent(new Tutorial(moduleCode, date, time, venue));
@@ -228,8 +228,8 @@ public class AddCommand extends Command {
         if (moduleCode.isEmpty()) {
             throw new CommandException("lecture");
         }  else if (recurringCount < 0 || recurringCount > 13) {
-            System.out.println("The number of lecture can only be in the range of 0 to 13. \n");
-            throw new CommandException("invalid recurring number");
+            System.out.println("Warning! The number of lecture can only be in the range of 0 to 13. \n");
+            throw new CommandException("lecture");
         } else {
             for (int i = 0; i < recurringCount; i++) {
                 calendarList.addEvent(new Lecture(moduleCode, date, time, venue));
