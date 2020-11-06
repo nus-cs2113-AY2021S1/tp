56a1b61f12e4447e1ebe53904dfe9980ac2f52f4
Ng Shu Lin Jane <ngshulinjane@hotmail.com>
2020-11-07 04:34:45 +0800

    Add javadoc for data classes

--- src/main/java/manager/admin/ModuleList.java
+++ src/main/java/manager/admin/ModuleList.java
@@ -4,6 +4,9 @@ import manager.module.Module;
 
 import java.util.ArrayList;
 
+/**
+ * A list of modules.
+ */
 public class ModuleList {
     private final ArrayList<Module> modules;
 
@@ -31,6 +34,12 @@ public class ModuleList {
         return modules.get(moduleIndex);
     }
 
+    /**
+     * Checks if the list contains an equivalent module that has the same {@code newModuleName}.
+     *
+     * @param newModuleName new name for a module
+     * @return true if the list contains an equivalent module that has the same name as the given argument.
+     */
     public boolean checkModuleExistence(String newModuleName) {
         for (Module module : modules) {
             String moduleName = module.getModuleName().toLowerCase();
