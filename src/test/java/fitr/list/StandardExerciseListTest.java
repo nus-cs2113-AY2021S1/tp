c8a6984d10503a86a76e677d0a612dbe3cbafad2
dmbclub <dmb437026158@gmail.com>
2020-11-06 17:12:46 +0800

    checkstyle

--- src/test/java/fitr/list/StandardExerciseListTest.java
+++ src/test/java/fitr/list/StandardExerciseListTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StandardExerciseListTest {
+
     ArrayList<Double> duration = new ArrayList<>();
     ArrayList<Integer> sets = new ArrayList<>();
 
