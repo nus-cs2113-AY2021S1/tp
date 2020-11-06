b56b271761bbf1c99e744bb7e3cd67dbabb0d1fd
QX-CHEN <c.qixing@yahoo.com.sg>
2020-11-06 15:11:54 +0800

    Move string conversion methods to Util

--- src/main/java/seedu/commons/Util.java
+++ src/main/java/seedu/commons/Util.java
@@ -1,5 +1,14 @@
 package seedu.commons;
 
+import seedu.exceptions.InvalidDatetimeException;
+import seedu.exceptions.InvalidPriorityException;
+import seedu.task.Priority;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Util {
 
     public static String generatePadStringWithCharAndLength(char pad, int length) {
@@ -29,5 +38,78 @@ public class Util {
         assert (end - start) > string.length();
         int dstBegin = start + (end - start - string.length()) / 2;
         string.getChars(0, string.length(), arr, dstBegin);
+    }
+
+    public static LocalDate dateStringToDate(String dateString) throws InvalidDatetimeException {
+        if (dateString == null) {
+            return LocalDate.now();
+        }
+        String[] dateParts = dateString.split("-");
+        int day = Integer.parseInt(dateParts[0]);
+        int month = Integer.parseInt(dateParts[1]);
+        int year = Integer.parseInt(dateParts[2]);
+
+        try {
+            return LocalDate.of(year, month, day);
+        } catch (DateTimeException e) {
+            throw new InvalidDatetimeException();
+        }
+
+    }
+
+    public static LocalTime timeStringToTime(String timeString) throws InvalidDatetimeException {
+        if (timeString == null) {
+            return null;
+        }
+        int time = Integer.parseInt(timeString);
+        int hour = time / 100;
+        int minute = time % 100;
+
+        try {
+            return LocalTime.of(hour, minute);
+        } catch (DateTimeException e) {
+            throw new InvalidDatetimeException();
+        }
+    }
+
+    public static Priority priorityStringToPriority(String priorityString) throws InvalidPriorityException {
+        if (priorityString == null) {
+            return Priority.LOW;
+        }
+        Priority priority;
+        switch (priorityString) {
+        case "1":
+            priority = Priority.LOW;
+            break;
+        case "2":
+            priority = Priority.MEDIUM;
+            break;
+        case "3":
+            priority = Priority.HIGH;
+            break;
+        default:
+            throw new InvalidPriorityException();
+        }
+        return priority;
+    }
+
+    public static String dateToString(LocalDate date) {
+        if (date == null) {
+            return "";
+        } else {
+            return " " + date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        }
+    }
+
+    public static String timeToString(LocalTime time) {
+        if (time == null) {
+            return "";
+        } else {
+            return " " + time.format(DateTimeFormatter.ofPattern("HHmm"));
+        }
+    }
+
+    public static String priorityToString(Priority priority) {
+        return " " + priority.toString();
     }
 }
