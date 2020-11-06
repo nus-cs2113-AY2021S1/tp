ff30970fa533789cea392a9c0ee3d14c38e634a4
TanLeeWei <tanleewei18@gmail.com>
2020-11-07 02:18:36 +0800

    Move most of the messages into Message class and added new message for empty storage file

--- src/main/java/seedu/smarthomebot/logic/commands/ExitCommand.java
+++ src/main/java/seedu/smarthomebot/logic/commands/ExitCommand.java
@@ -2,6 +2,8 @@ package seedu.smarthomebot.logic.commands;
 
 import seedu.smarthomebot.data.appliance.Appliance;
 
+import static seedu.smarthomebot.commons.Messages.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+
 //@@author Ang_Cheng_Jun
 /**
  * Represent the command to terminate the program.
@@ -10,8 +12,6 @@ public class ExitCommand extends Command {
 
     public static final String COMMAND_WORD = "exit";
     public static final String MESSAGE_USAGE = "Exiting the application: " + COMMAND_WORD;
-    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT =
-            "Exiting SmartHomeBot as requested, all Appliances are switched off.";
 
     public static boolean isExit(Command command) {
         return command instanceof ExitCommand;
