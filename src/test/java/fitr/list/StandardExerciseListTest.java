53a557b76d12b35c35f6ad3c1163b2a3a5e895c7
dmbclub <dmb437026158@gmail.com>
2020-11-06 17:21:24 +0800

    checkstyle

--- src/test/java/fitr/list/StandardExerciseListTest.java
+++ src/test/java/fitr/list/StandardExerciseListTest.java
@@ -4,13 +4,16 @@ import fitr.exercise.StandardExercise;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StandardExerciseListTest {
+
     @Test
     public void addExerciseToEmptyList_validExercise_success() {
         ArrayList<Double> duration = new ArrayList<>();
         ArrayList<Integer> sets = new ArrayList<>();
+
         StandardExerciseList exerciseList = new StandardExerciseList();
         exerciseList.addExercise(new StandardExercise("Push ups", 2.4, duration, sets));
         assertEquals(1, exerciseList.getSize());
@@ -20,6 +23,7 @@ public class StandardExerciseListTest {
     public void addExerciseToNonEmptyList_validExercise_success() {
         ArrayList<Double> duration = new ArrayList<>();
         ArrayList<Integer> sets = new ArrayList<>();
+
         StandardExerciseList exerciseList = new StandardExerciseList(getTestExerciseList());
         exerciseList.addExercise(new StandardExercise("Push ups", 2.4, duration, sets));
         assertEquals(4, exerciseList.getSize());
@@ -28,6 +32,7 @@ public class StandardExerciseListTest {
     private ArrayList<StandardExercise> getTestExerciseList() {
         ArrayList<Double> duration = new ArrayList<>();
         ArrayList<Integer> sets = new ArrayList<>();
+
         ArrayList<StandardExercise> exerciseList = new ArrayList<>();
         exerciseList.add(new StandardExercise("Crunches", 1, duration, sets));
         exerciseList.add(new StandardExercise("Squats", 2, duration, sets));
