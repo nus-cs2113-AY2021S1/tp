defce7ff0c5283acc9ef71905d2d3117f932aae7
czlin98 <czlin98@gmail.com>
2020-11-06 15:01:00 +0800

    Allow spaces around command word and command argument.

--- src/main/java/seedu/parser/Parser.java
+++ src/main/java/seedu/parser/Parser.java
@@ -79,60 +79,6 @@ public class Parser {
         default:
             throw new UnknowCommandException();
         }
-//        if (rawInput.startsWith(Help.COMMAND_WORD)) {
-//            return new Help();
-//        } else if (rawInput.startsWith(List.COMMAND_WORD)) {
-//            matcher = List.COMMAND_PATTERN.matcher(rawInput);
-//            if (matcher.find()) {
-//                return new List(" -d".equals(matcher.group("dateFlag")),
-//                        " -p".equals(matcher.group("priorityFlag")),
-//                        " -w".equals(matcher.group("displayByWeek")),
-//                        " -m".equals(matcher.group("displayByMonth")),
-//                        matcher.group("date"));
-//            } else {
-//                throw new InvalidCommandException();
-//            }
-//        } else if (rawInput.startsWith(Bye.COMMAND_WORD)) {
-//            return new Bye();
-//        } else if (rawInput.startsWith(Clear.COMMAND_WORD)) {
-//            return new Clear();
-//        } else if (rawInput.startsWith(Add.COMMAND_WORD)) {
-//            matcher = Add.COMMAND_PATTERN.matcher(rawInput);
-//            if (matcher.find()) {
-//                return new Add(matcher.group("description"), matcher.group("date"), matcher.group("st"),
-//                        matcher.group("et"), matcher.group("priority"), matcher.group("reminder"),
-//                        matcher.group("t"));
-//            } else {
-//                throw new InvalidCommandException();
-//            }
-//        } else if (rawInput.startsWith(Edit.COMMAND_WORD)) {
-//            matcher = Edit.COMMAND_PATTERN.matcher(rawInput);
-//            if (matcher.find()) {
-//                return new Edit(matcher.group("key"), matcher.group("description"),
-//                        matcher.group("date"), matcher.group("st"), matcher.group("et"),
-//                        matcher.group("priority"), matcher.group("reminder"), matcher.group("t"));
-//            } else {
-//                throw new InvalidCommandException();
-//            }
-//        } else if (rawInput.startsWith(Search.COMMAND_WORD)) {
-//            matcher = Search.COMMAND_PATTERN.matcher(rawInput);
-//            if (matcher.find()) {
-//                return new Search(matcher.group("toSearch"));
-//            } else {
-//                throw new InvalidCommandException();
-//            }
-//        } else if (rawInput.startsWith(Delete.COMMAND_WORD)) {
-//            matcher = Delete.COMMAND_PATTERN.matcher(rawInput);
-//            if (matcher.find()) {
-//                return new Delete(matcher.group("key"));
-//            } else {
-//                throw new InvalidCommandException();
-//            }
-//        } else if (rawInput.startsWith(Undo.COMMAND_WORD)) {
-//            return new Undo();
-//        } else {
-//            throw new UnknowCommandException();
-//        }
     }
 
     public String getCommandWord(String rawInput) {
