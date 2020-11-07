914f19d794418db009c856ae53f45d6a5a308919
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-11-07 14:14:11 +0800

    UI update2

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -3,6 +3,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class UI {
+
+    public static void order(List<Canteen> canteens, Customer customer,Scanner sc, ArrayList<Order> Order){
+
+        Canteen canteenChoosed = getCanteen(canteens, customer,sc);
+
+        Stall stallChoosed = getStall(customer, canteenChoosed,sc);
+
+        List<Dish> orderedDishes = getDishes(stallChoosed,sc);
+
+        Order order = getOrder(customer, canteenChoosed, stallChoosed, orderedDishes,sc,Order);
+
+        System.out.println("Your order created! Thanks.");
+        System.out.println(order);
+        sc.nextLine();
+        System.out.println("____________________________________________________________\n");
+
+    }
+
     public static Customer getCustomer(Scanner sc) {
         try{
             System.out.println("Please enter your name/day of week/time arrive:");
@@ -25,23 +43,6 @@ public class UI {
             System.out.println("____________________________________________________________\n");
             return getCustomer(sc);
         }
-    }
-
-    public static void order(List<Canteen> canteens, Customer customer,Scanner sc, ArrayList<Order> Order){
-
-        Canteen canteenChoosed = getCanteen(canteens, customer,sc);
-
-        Stall stallChoosed = getStall(customer, canteenChoosed,sc);
-
-        List<Dish> orderedDishes = getDishes(stallChoosed,sc);
-
-        Order order = getOrder(customer, canteenChoosed, stallChoosed, orderedDishes,sc,Order);
-
-        System.out.println("Your order created! Thanks.");
-        System.out.println(order);
-        sc.nextLine();
-        System.out.println("____________________________________________________________\n");
-
     }
 
     private static Order getOrder(Customer customer, Canteen canteenChoosed, Stall stallChoosed, List<Dish> orderedDishes,Scanner sc, ArrayList<Order> Order) {
@@ -258,4 +259,119 @@ public class UI {
         }
     }
 
+    public static void checkCanteenOperatingTime(List<Canteen> canteens,Customer customer,Scanner sc){
+        System.out.println("Choose the canteen you want to check:");
+        List<Canteen> openCanteens = customer.checkOpenCanteens(canteens);//list of canteens
+        int j = 0;
+        for(Canteen canteen:openCanteens) {
+            j++;
+            System.out.println(j +". " + canteen);
+        }
+        System.out.println("Enter the number in front to choose:");
+        int canteenIdChoosed = sc.nextInt();
+        Canteen canteenChoosed = openCanteens.get(canteenIdChoosed - 1);
+        System.out.println("Operating hours for the canteen you choosed is: \n");
+        System.out.println("Open Time: " + canteenChoosed.getOpenTime(customer.getDayOfWeek())/100 + ":00");
+        System.out.println("Closing Time: " + canteenChoosed.getCloseTime(customer.getDayOfWeek())/100 +":" +
+                           canteenChoosed.getCloseTime(customer.getDayOfWeek())%100 );
+        System.out.println("____________________________________________________________\n");
+        sc.nextLine();
+    }
+
+
+    public static void checkStallOperatingTime(List<Canteen> canteens,Customer customer,Scanner sc){
+        System.out.println("Input the canteen which your desired stall belongs to: \n");
+        List<Canteen> openCanteens = customer.checkOpenCanteens(canteens);//list of canteens
+        int j = 0;
+        for(Canteen canteen:openCanteens) {
+            j++;
+            System.out.println(j +". " + canteen);
+        }
+        System.out.println("Enter the number in front to choose:");
+        int canteenIdChoosed = sc.nextInt();
+        Canteen canteenChoosed = openCanteens.get(canteenIdChoosed - 1);
+        List<Stall> openStall = customer.checkOpenStalls(canteenChoosed);
+        j = 0;
+        for (Stall stall : openStall) {
+            j++;
+            System.out.println(j +". " + stall);
+        }
+        System.out.println("Please choose a stall:");
+        int stallIdChoosed = sc.nextInt();
+        Stall stallChoosed = openStall.get(stallIdChoosed - 1);
+        System.out.println("Operating hours for the stall you chose is: \n");
+        System.out.println("Open Time: " + stallChoosed.getOpenTime()/100 + ":00" );
+        System.out.println("Closing Time: " + stallChoosed.getCloseTime()/100 + ":"
+                           + String.format("%02d",stallChoosed.getCloseTime()%100));
+        sc.nextLine();
+        System.out.println("____________________________________________________________\n");
+    }
+
+
+    public static void changeOrder(Customer customer,String input,ArrayList<Order> Order)
+    {
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Noted. I've changed this order:  \n");
+        String[] inputWords = input.split("/"); //split the input message
+        String orderNumberchenged = inputWords[1];
+        int orderNumberchanged = Integer.parseInt(orderNumberchenged) - 1;
+        String changedtype = inputWords[2];
+        System.out.println(Order.get(orderNumberchanged) + "\n");
+        Canteen can = Order.get(orderNumberchanged).getCanteen();
+        Stall stall = Order.get(orderNumberchanged).getStall();
+        List<Dish> dishes = Order.get(orderNumberchanged).getDish();
+        Order changedOrder = customer.order(can,stall,dishes,changedtype);
+        Order.set(orderNumberchanged,changedOrder);
+
+        System.out.println("to \n");
+        System.out.println(Order.get(orderNumberchanged) + "\n");
+        System.out.println("____________________________________________________________\n");
+
+    }
+
+    public static void printOrder(String input,ArrayList<Order> Order) {
+        System.out.println("____________________________________________________________\n");
+        for (int i = 0; i < Order.size(); i++) {
+            System.out.println("____________________________________________________________\n");
+            if (Order.get(i) instanceof dineInOrder) {
+                System.out.println((i + 1) + ":" + (dineInOrder) Order.get(i));
+            }
+            if (Order.get(i) instanceof deliveryOrder) {
+                System.out.println((i + 1) + ":" + (deliveryOrder) Order.get(i));
+            }
+            if (Order.get(i) instanceof takeAwayOrder) {
+                System.out.println((i + 1) + ":" + (takeAwayOrder) Order.get(i));
+            }
+            System.out.println("____________________________________________________________\n");
+
+        }
+        System.out.println("____________________________________________________________\n");
+
+
+
+    }
+    public static void deleteOrder(String input,ArrayList<Order> Order) {
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Noted. I've removed this order:  ");
+        int orderNumberdeleted = Integer.parseInt(input.replaceAll("\\D+", "")) - 1; //find the corresponding index of task to be deleted
+        System.out.println(Order.get(orderNumberdeleted));
+        Order.remove(orderNumberdeleted); //remove that task from arrayList
+        System.out.println("Now you have " + Order.size() + " orders in the list. ");
+        System.out.println("____________________________________________________________\n");
+    }
+    public static void findDishinOrder(String input,ArrayList<Order> Order) {
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Here are the matching orders in your list:\n");
+        String keyword = input.substring(5); // to get the keyword string
+        int count = 1;
+        /*iterate the task arrayList to find corresponding items*/
+        for(int i=0;i< Order.size();i++){
+            for(int j=0;j<Order.get(i).getDish().size();j++)
+                if(Order.get(i).getDish().get(j).getDishName().compareTo(keyword) == 0){
+                    System.out.println(count +": " + Order.get(i));
+                    count ++;
+                }
+        }
+        System.out.println("____________________________________________________________\n");
+    }
 }
