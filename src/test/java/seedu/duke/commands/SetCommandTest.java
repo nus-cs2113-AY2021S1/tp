b0661de98bbc7e74949b4c333722cfde655ea241
iamchenjiajun <iamchenjiajun@gmail.com>
2020-11-07 18:47:25 +0800

    Refactor SetCommand test code

--- src/test/java/seedu/duke/commands/SetCommandTest.java
+++ src/test/java/seedu/duke/commands/SetCommandTest.java
@@ -12,52 +12,56 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SetCommandTest {
+    private static final String TEST_DESCRIPTION = "test description";
+    private HashMap<String, String> argumentsMap;
+    private Model model;
+    private TaskList tasks;
+
+    /**
+     * Resets the fields for each test case.
+     */
+    void resetFields() {
+        model = new Model();
+        argumentsMap = new HashMap<>();
+        tasks = (TaskList) model.getList(ListType.TASK_LIST);
+    }
 
     @Test
     void execute_validPriority_setsNewPriority() throws DukeException {
-        Model model = new Model();
-        TaskList tasks = (TaskList) model.getList(ListType.TASK_LIST);
+        resetFields();
 
         int initialPriority = 0;
         int newPriority = 3;
-        HashMap<String, String> argumentsMap = new HashMap<>();
-
         argumentsMap.put("p", Integer.toString(newPriority));
         Command setCommand = new SetCommand(1, argumentsMap);
 
-        tasks.addTodo("test description");
+        tasks.addTodo(TEST_DESCRIPTION);
         assertEquals(initialPriority, tasks.get(0).getPriority());
         setCommand.execute(model);
         assertEquals(newPriority, tasks.get(0).getPriority());
     }
 
     @Test
-    void execute_negativePriority_throwsException() throws DukeException {
+    void execute_negativePriority_throwsException() {
+        resetFields();
+
         int newPriority = -1;
-        Model model = new Model();
-        TaskList tasks = (TaskList) model.getList(ListType.TASK_LIST);
-        HashMap<String, String> argumentsMap = new HashMap<>();
         argumentsMap.put("p", Integer.toString(newPriority));
         Command setCommand = new SetCommand(1, argumentsMap);
 
-        tasks.addTodo("test description");
-        assertThrows(DukeException.class, () -> {
-            setCommand.execute(model);
-        });
+        tasks.addTodo(TEST_DESCRIPTION);
+        assertThrows(DukeException.class, () -> setCommand.execute(model));
     }
 
     @Test
-    void execute_invalidPriority_throwsException() throws DukeException {
+    void execute_invalidPriority_throwsException() {
+        resetFields();
+
         String newPriority = "a";
-        Model model = new Model();
-        TaskList tasks = (TaskList) model.getList(ListType.TASK_LIST);
-        HashMap<String, String> argumentsMap = new HashMap<>();
         argumentsMap.put("p", newPriority);
         Command setCommand = new SetCommand(1, argumentsMap);
 
-        tasks.addTodo("test description");
-        assertThrows(DukeException.class, () -> {
-            setCommand.execute(model);
-        });
+        tasks.addTodo(TEST_DESCRIPTION);
+        assertThrows(DukeException.class, () -> setCommand.execute(model));
     }
 }
