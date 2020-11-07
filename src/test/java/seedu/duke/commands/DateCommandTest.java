d6954f3691862bb79b163726c3559681c31b399a
iamchenjiajun <iamchenjiajun@gmail.com>
2020-11-07 18:43:45 +0800

    Refactor DateCommand test code

--- src/test/java/seedu/duke/commands/DateCommandTest.java
+++ src/test/java/seedu/duke/commands/DateCommandTest.java
@@ -3,27 +3,35 @@ package seedu.duke.commands;
 import org.junit.jupiter.api.Test;
 import seedu.duke.DukeException;
 import seedu.duke.model.Model;
-import seedu.duke.model.itemlist.ItemList;
 import seedu.duke.model.ListType;
 import seedu.duke.model.item.Task;
 import seedu.duke.model.itemlist.TaskList;
 
-import java.util.EnumMap;
 import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DateCommandTest {
+    private static final String TEST_DESCRIPTION = "test description";
+    private HashMap<String, String> argumentsMap;
+    private Model model;
+    private TaskList tasks;
+
+    /**
+     * Resets the fields for each test case.
+     */
+    void resetFields() {
+        model = new Model();
+        argumentsMap = new HashMap<>();
+        tasks = (TaskList) model.getList(ListType.TASK_LIST);
+    }
 
     @Test
     void execute_validDate_setsNewDate() throws DukeException {
-        Model model = new Model();
-        TaskList tasks = (TaskList) model.getList(ListType.TASK_LIST);
-        HashMap<String, String> argumentsMap = new HashMap<>();
+        resetFields();
 
-        tasks.addTodo("test description");
+        tasks.addTodo(TEST_DESCRIPTION);
         argumentsMap.put("date", "13-05-2020");
         Command dateCommand = new DateCommand(1, argumentsMap);
 
@@ -32,31 +40,23 @@ class DateCommandTest {
     }
 
     @Test
-    void execute_invalidDate_throwsException() throws DukeException {
-        Model model = new Model();
-        TaskList tasks = (TaskList) model.getList(ListType.TASK_LIST);
-        HashMap<String, String> argumentsMap = new HashMap<>();
+    void execute_invalidDate_throwsException() {
+        resetFields();
 
-        tasks.addTodo("test description");
+        tasks.addTodo(TEST_DESCRIPTION);
         argumentsMap.put("date", "xx-yy-zzzz");
         Command dateCommand = new DateCommand(1, argumentsMap);
 
-        assertThrows(DukeException.class, () -> {
-            dateCommand.execute(model);
-        });
+        assertThrows(DukeException.class, () -> dateCommand.execute(model));
     }
 
     @Test
-    void execute_noDate_throwsException() throws DukeException {
-        Model model = new Model();
-        TaskList tasks = (TaskList) model.getList(ListType.TASK_LIST);
-        HashMap<String, String> argumentsMap = new HashMap<>();
+    void execute_noDate_throwsException() {
+        resetFields();
 
-        tasks.addTodo("test description");
+        tasks.addTodo(TEST_DESCRIPTION);
         Command dateCommand = new DateCommand(1, argumentsMap);
 
-        assertThrows(DukeException.class, () -> {
-            dateCommand.execute(model);
-        });
+        assertThrows(DukeException.class, () -> dateCommand.execute(model));
     }
 }
