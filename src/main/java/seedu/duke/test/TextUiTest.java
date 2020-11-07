91704c8b43b5a8f17c911a671ae96cfe01368190
keke101 <zack_1997@hotmail.com>
2020-11-07 22:37:32 +0800

    Add main class for text-ui testing - `runtest.sh` and `runtest.bat` will run `TextUiTest.main()` in this class instead of `Duke.main()`

---
+++ src/main/java/seedu/duke/test/TextUiTest.java
@@ -0,0 +1,16 @@
+package seedu.duke.test;
+
+import seedu.duke.Duke;
+
+import java.time.*;
+
+/**
+ * This class is for testing text-ui. It will 
+ */
+public class TextUiTest {
+    public static void main(String[] args) {
+        Instant fakeDateTime = Instant.parse("2020-10-10T00:00:00.000Z");
+        Duke.setClock(Clock.fixed(fakeDateTime, ZoneOffset.systemDefault()));
+        Duke.main(new String[0]);
+    }
+}
