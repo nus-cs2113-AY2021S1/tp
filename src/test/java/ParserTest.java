90d9ed9ea56199a1e719d1c707db587fb0cfa4d8
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-11-07 17:13:09 +0800

    ParserTest

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,5 +1,15 @@
+import exception.ArriveTimeException;
+import exception.DayOfWeekException;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
 
+    @Test
+    void parseCustomer() throws DayOfWeekException, ArriveTimeException {
+        assertEquals("z", Parser.parseCustomer("z/2/1200").name);
+        assertEquals(1, Parser.parseCustomer("z/2/1200").dayOfWeek);
+        assertEquals(1200, Parser.parseCustomer("z/2/1200").arriveTime);
+    }
 }
