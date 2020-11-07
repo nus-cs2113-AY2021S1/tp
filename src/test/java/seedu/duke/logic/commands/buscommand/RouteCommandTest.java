9f88806c931dbbdfd35fd7aeb7c82f874689ed44
wamikamalik <wamikamalik@hotmail.com>
2020-11-07 12:47:41 +0400

    Update tests for route

--- src/test/java/seedu/duke/logic/commands/buscommand/RouteCommandTest.java
+++ src/test/java/seedu/duke/logic/commands/buscommand/RouteCommandTest.java
@@ -3,7 +3,7 @@ package seedu.duke.logic.commands.buscommand;
 import org.junit.jupiter.api.Test;
 import seedu.duke.exceptions.CustomException;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RouteCommandTest {
 
@@ -11,21 +11,64 @@ class RouteCommandTest {
     void executeCommand_oneLocationWithDelimiter_expectException() {
         String locations = "PGP /to   ";
         RouteCommand com = new RouteCommand(locations);
-        assertThrows(CustomException.class, com::executeCommand);
-    }
-
-    @Test
-    void executeCommand_onlyDelimiter_expectException() {
-        String locations = "    /to   ";
-        RouteCommand com = new RouteCommand(locations);
-        assertThrows(CustomException.class, com::executeCommand);
+        try {
+            com.executeCommand();
+        } catch (CustomException error) {
+            assertEquals("Oh dear! I don't have all locations.", error.toString());
+        }
     }
 
     @Test
     void executeCommand_sameLocations_expectException() {
         String locations = " University Town /to univerSity TowN  ";
         RouteCommand com = new RouteCommand(locations);
-        assertThrows(CustomException.class, com::executeCommand);
+        try {
+            com.executeCommand();
+        } catch (CustomException error) {
+            assertEquals("You don't need a bus to get there...You are right there!", error.toString());
+        }
+    }
+
+    @Test
+    void executeCommand_spellingError_expectException() {
+        String locations = " Univarsity Town /to PGP  ";
+        RouteCommand com = new RouteCommand(locations);
+        try {
+            com.executeCommand();
+        } catch (CustomException error) {
+            assertEquals("Possible Locs shown", error.toString());
+        }
+    }
+
+    @Test
+    void executeCommand_nonExistentLocations_expectException() {
+        String locationsInvalidSource = " Orchard /to PGP  ";
+        String locationsInvalidDest = " University Health Centre /to Vivocity ";
+        String locationsInvalidAll = " Santosa /to West coast park";
+        RouteCommand com = new RouteCommand(locationsInvalidSource);
+        try {
+            com.executeCommand();
+        } catch (CustomException error) {
+            assertEquals("The starting location you have provided is not the name of any bus stop in our\n"
+                    + "database :(", error.toString());
+        }
+
+        com = new RouteCommand(locationsInvalidDest);
+        try {
+            com.executeCommand();
+        } catch (CustomException error) {
+            assertEquals("The destination you have provided is not the name of any bus stop in our database :("
+                    , error.toString());
+        }
+
+        com = new RouteCommand(locationsInvalidAll);
+        try {
+            com.executeCommand();
+        } catch (CustomException error) {
+            assertEquals("The starting location and destination you have provided are not the names of any "
+                    + "bus\nstops in our database :(", error.toString());
+        }
+
     }
 
 }
