f0dcba6c4fb3a3fe6f2f00f898fe05f7c6e62f6e
shiorin9 <homingjun9@gmail.com>
2020-11-07 17:59:05 +0800

    Added logging for parser and help commands

--- src/main/java/seedu/duke/command/help/StorageHelpCommand.java
+++ src/main/java/seedu/duke/command/help/StorageHelpCommand.java
@@ -1,20 +1,27 @@
 package seedu.duke.command.help;
 
 import seedu.duke.command.Command;
+import seedu.duke.logger.ScrumLogger;
 import seedu.duke.ui.Ui;
 
 import java.util.Hashtable;
 
-public class StorageHelpCommand extends Command {
+public class StorageHelpCommand extends HelpCommand {
     public StorageHelpCommand(Hashtable<String, String> parameters) {
         super(parameters, false);
     }
 
     @Override
     public void execute() {
+        logExecution();
         Ui.showToUserLn("1. Clear all data. You will be prompted to confirm the decision.");
         Ui.showToUserLn("   Data will be cleared if \"y\" (case-insensitive) is provided.");
         Ui.showToUserLn("   Format: storage /clear");
         Ui.showToUserLn("   Example: storage /clear");
+    }
+
+    @Override
+    public void logExecution() {
+        ScrumLogger.LOGGER.info("Viewed storage help.");
     }
 }
