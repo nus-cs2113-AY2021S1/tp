3adc63935eb8529284118900b79a56f09d9822cf
Jingming_mac <liujingming517@gmail.com>
2020-11-07 12:17:03 +0800

    Fixed user input bugs in timeline

--- src/main/java/seedu/duke/command/PrintTimelineCommand.java
+++ src/main/java/seedu/duke/command/PrintTimelineCommand.java
@@ -1,5 +1,6 @@
 package seedu.duke.command;
 
+import seedu.duke.CommandException;
 import seedu.duke.DateTimeParser;
 import seedu.duke.Storage;
 import seedu.duke.calendar.CalendarItem;
@@ -29,18 +30,25 @@ public class PrintTimelineCommand extends Command {
         LocalDate endDate = null;
         try {
             endDate = detectEndDate(userInput);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (calendarList.getTotalItems() == 0) {
-            System.out.println("Your calendar list is empty!");
-        } else {
-            if (endDate != null) {
-                endDate = startDate.plusDays(7);
-                for (int i = 0; i < calendarList.getTotalItems(); i++) {
-                    if ((calendarList.getItem(i).getDate() == null)
-                            || ((calendarList.getItem(i).getDate().isAfter(startDate))
-                            && (calendarList.getItem(i).getDate().isBefore(endDate)))) {
+            if (calendarList.getTotalItems() == 0) {
+                System.out.println("Your calendar list is empty!");
+            } else {
+                if (endDate != null) {
+                    endDate = startDate.plusDays(7);
+                    for (int i = 0; i < calendarList.getTotalItems(); i++) {
+                        if ((calendarList.getItem(i).getDate() == null)
+                                || ((calendarList.getItem(i).getDate().isAfter(startDate))
+                                && (calendarList.getItem(i).getDate().isBefore(endDate)))) {
+                            CalendarItem temp = calendarList.getItem(i);
+                            if (temp instanceof Todo) {
+                                todoList.addItem(temp);
+                            } else {
+                                timelineList.addItem(temp);
+                            }
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < calendarList.getTotalItems(); i++) {
                         CalendarItem temp = calendarList.getItem(i);
                         if (temp instanceof Todo) {
                             todoList.addItem(temp);
@@ -49,57 +57,51 @@ public class PrintTimelineCommand extends Command {
                         }
                     }
                 }
-            } else {
-                for (int i = 0; i < calendarList.getTotalItems(); i++) {
-                    CalendarItem temp = calendarList.getItem(i);
-                    if (temp instanceof Todo) {
-                        todoList.addItem(temp);
+
+                CalendarList sortedList = sortByDateTime(timelineList);
+
+                System.out.println("Here is your timeline:");
+                System.out.println("Timeline\n|");
+                if (sortedList.getTotalItems() != 0) {
+                    System.out.println("|__ " + sortedList.getItem(0).getDate()
+                            + " " + sortedList.getItem(0).getDate().getDayOfWeek());
+                    if (sortedList.getItem(0).getTime() == null) {
+                        System.out.println("|            |_____ " + "23:59");
                     } else {
-                        timelineList.addItem(temp);
+                        System.out.println("|            |_____ " + sortedList.getItem(0).getTime());
+                    }
+                    System.out.println("|                     |_____ " + sortedList.getItem(0).getDescription());
+
+                    for (int i = 1; i < sortedList.getTotalItems(); i++) {
+                        LocalTime thisTime = (sortedList.getItem(i).getTime() == null
+                                ? LocalTime.of(23, 59) : sortedList.getItem(i).getTime());
+                        LocalTime prevTime = (sortedList.getItem(i - 1).getTime() == null
+                                ? LocalTime.of(23, 59) : sortedList.getItem(i - 1).getTime());
+                        if (!(sortedList.getItem(i - 1).getDate().isEqual(sortedList.getItem(i).getDate()))) {
+                            System.out.println("|__ " + sortedList.getItem(i).getDate()
+                                    + " " + sortedList.getItem(i).getDate().getDayOfWeek());
+                        }
+                        if (!thisTime.equals(prevTime)) {
+                            System.out.println("|            |_____ " + thisTime);
+                        }
+                        System.out.println("|                     |_____ "
+                                + sortedList.getItem(i).getDescription());
                     }
                 }
-            }
 
-            CalendarList sortedList = sortByDateTime(timelineList);
-
-            System.out.println("Here is your timeline:");
-            System.out.println("Timeline\n|");
-            if (sortedList.getTotalItems() != 0) {
-                System.out.println("|__ " + sortedList.getItem(0).getDate()
-                        + " " + sortedList.getItem(0).getDate().getDayOfWeek());
-                if (sortedList.getItem(0).getTime() == null) {
-                    System.out.println("|            |_____ " + "23:59");
-                } else {
-                    System.out.println("|            |_____ " + sortedList.getItem(0).getTime());
-                }
-                System.out.println("|                     |_____ " + sortedList.getItem(0).getDescription());
-
-                for (int i = 1; i < sortedList.getTotalItems(); i++) {
-                    LocalTime thisTime = (sortedList.getItem(i).getTime() == null
-                            ? LocalTime.of(23, 59) : sortedList.getItem(i).getTime());
-                    LocalTime prevTime = (sortedList.getItem(i - 1).getTime() == null
-                            ? LocalTime.of(23, 59) : sortedList.getItem(i - 1).getTime());
-                    if (!(sortedList.getItem(i - 1).getDate().isEqual(sortedList.getItem(i).getDate()))) {
-                        System.out.println("|__ " + sortedList.getItem(i).getDate()
-                                + " " + sortedList.getItem(i).getDate().getDayOfWeek());
+                System.out.println("|__ Todo items");
+                int index = 1;
+                if (todoList.getTotalItems() != 0) {
+                    for (int i = 0; i < todoList.getTotalItems(); i++) {
+                        System.out.println(index + ". " + todoList.getItem(i).toString());
+                        index++;
                     }
-                    if (!thisTime.equals(prevTime)) {
-                        System.out.println("|            |_____ " + thisTime);
-                    }
-                    System.out.println("|                     |_____ "
-                            + sortedList.getItem(i).getDescription());
+                    System.out.println("\n");
                 }
             }
-
-            System.out.println("|__ Todo items");
-            int index = 1;
-            if (todoList.getTotalItems() != 0) {
-                for (int i = 0; i < todoList.getTotalItems(); i++) {
-                    System.out.println(index + ". " + todoList.getItem(i).toString());
-                    index++;
-                }
-                System.out.println("\n");
-            }
+        } catch (Exception e) {
+            System.out.println("Invalid input! "
+                    + "Please key in the command in this format: print timeline <week/month/date [ddMMyy]>");
         }
     }
 
@@ -138,15 +140,22 @@ public class PrintTimelineCommand extends Command {
     public LocalDate detectEndDate(String userInput) throws Exception {
         LocalDate startDate = LocalDate.now();
         LocalDate endDate;
-        if (userInput.contains("week")) {
-            endDate = startDate.plusDays(7);
-        } else if (userInput.contains(("month"))) {
-            endDate = startDate.plusDays(31);
-        } else if (userInput.contains("date")) {
-            String[] userInputSplit = userInput.split("date", 2);
+        String[] userInputSplit = userInput.split(" ");
+        if (userInputSplit.length == 2) {
+            endDate = null;
+        } else if (userInputSplit.length == 3) {
+            if (userInputSplit[2] == "week") {
+                endDate = startDate.plusDays(7);
+            } else if (userInputSplit[2] == "month") {
+                endDate = startDate.plusDays(31);
+            } else {
+                throw new CommandException("invalid command");
+            }
+        } else if (userInputSplit.length == 4 && userInputSplit[2] == "date") {
+            String[] userInputSplitDate = userInput.split("date", 2);
             endDate = DateTimeParser.inputDateProcessor(userInputSplit[1].trim());
         } else {
-            endDate = null;
+            throw new CommandException("invalid command");
         }
         return endDate;
     }
