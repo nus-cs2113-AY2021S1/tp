260dce4003476526c2cb301a941017658ea31546
Johnson-Yee <e0494363@u.nus.edu>
2020-11-08 23:20:05 +0800

    Addition of reset search freq Junit test case

--- src/test/java/seedu/duke/logic/commands/buscommand/ResetSearchFreqCommandTest.java
+++ src/test/java/seedu/duke/logic/commands/buscommand/ResetSearchFreqCommandTest.java
@@ -1,14 +1,23 @@
 package seedu.duke.logic.commands.buscommand;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.CustomException;
+import seedu.duke.model.bus.BusStops;
 import seedu.duke.model.favorite.Fav;
 import seedu.duke.model.favorite.FavList;
+import seedu.duke.storage.FreqStorage;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ResetSearchFreqCommandTest {
-    @BeforeAll
-    public static void makeList() {
+    @Test
+    public void resetSearchFreqCommand_success() throws CustomException {
+        FreqStorage freqFile = new FreqStorage("src/test/StorageTestCases/FreqStorageTestCases/ValidFreqList");
+        freqFile.readFile();
+        ResetSearchFreqCommand command = new ResetSearchFreqCommand();
+        command.executeCommand();
+        assertEquals(0, BusStops.PGP.getSearchCount());
     }
-
 }
