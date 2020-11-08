469aa1525ceb439f5f6faa4446e8fc55011f75cd
harris maung <harrismaung3@gmail.com>
2020-11-08 20:14:49 +0800

    updates

--- src/test/java/fitr/exercise/RecommenderTest.java
+++ src/test/java/fitr/exercise/RecommenderTest.java
@@ -1,19 +1,25 @@
 package fitr.exercise;
 
 import fitr.exception.InvalidRecommendationException;
+import fitr.ui.Ui;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RecommenderTest {
     @Test
-    public void recommenderTest() throws InvalidRecommendationException {
+    public void recommenderTest() {
         Recommender recommender = new Recommender();
-        assertEquals(4, recommender.recommend("upperbody").getSize());
-        assertEquals(4, recommender.recommend("lowebody").getSize());
-        assertEquals(4, recommender.recommend("aerobic").getSize());
-        assertEquals(4, recommender.recommend("stretch").getSize());
-        assertEquals(4, recommender.recommend("").getSize());
+        try {
+            assertEquals(4, recommender.recommend("upperbody").getSize());
+            assertEquals(4, recommender.recommend("lowerbody").getSize());
+            assertEquals(4, recommender.recommend("aerobic").getSize());
+            assertEquals(4, recommender.recommend("stretch").getSize());
+            assertEquals(4, recommender.recommend("").getSize());
+        } catch (InvalidRecommendationException e) {
+            Ui.printCustomError("This should never happen");
+        }
     }
 
     @Test
