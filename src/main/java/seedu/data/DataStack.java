e7789a3d9311338b32f76cf581051505234e781b
QX-CHEN <c.qixing@yahoo.com.sg>
2020-11-08 16:41:16 +0800

    Add java docs for Model and DataSatck

--- src/main/java/seedu/data/DataStack.java
+++ src/main/java/seedu/data/DataStack.java
@@ -1,18 +1,29 @@
 package seedu.data;
 
 import seedu.exceptions.EmptyDataStackException;
-import seedu.task.Task;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * Maintain a stack for undo function.
+ */
 public class DataStack {
     private Deque<TaskMap> stack = new ArrayDeque<>();
 
+    /**
+     * Adds a copy of taskMap into the stack.
+     * @param taskMap copy.
+     */
     public void push(TaskMap taskMap) {
         stack.addFirst(taskMap);
     }
 
+    /**
+     * Get the most recent copy of taskMap and remove it from the stack.
+     * @return most recent taskMap.
+     * @throws EmptyDataStackException when stack is empty.
+     */
     public TaskMap pop() throws EmptyDataStackException {
         if (stack.size() == 0) {
             throw new EmptyDataStackException();
@@ -20,10 +31,18 @@ public class DataStack {
         return stack.removeFirst();
     }
 
+    /**
+     * Get the most recent copy of taskMap.
+     * @return most recent taskMap.
+     */
     public TaskMap peek() {
         return stack.getFirst();
     }
 
+    /**
+     * Get size of stack.
+     * @return size of stack.
+     */
     public int size() {
         return stack.size();
     }
