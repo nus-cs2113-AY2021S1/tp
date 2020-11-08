f2b8b3d5d2abca28ea1866cdcc727c9015e804fc
DESKTOP-OV9KQRO\User <E0389113@u.nus.edu>
2020-11-08 22:35:55 +0800

    change checkstylemain

--- src/main/java/seedu/duke/exception/InvalidStatusException.java
+++ src/main/java/seedu/duke/exception/InvalidStatusException.java
@@ -1,6 +1,6 @@
 package seedu.duke.exception;
 
-public class InvalidStatusException extends DukeException{
+public class InvalidStatusException extends DukeException {
 
     public InvalidStatusException(String message) {
         super(message);
