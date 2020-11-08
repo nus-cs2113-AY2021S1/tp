84ce1c5ebfacac62440f08c2b1a7818b876acdac
Johnson-Yee <e0494363@u.nus.edu>
2020-11-08 22:38:06 +0800

    Addition of reset search freq test case

---
+++ src/test/java/seedu/duke/logic/commands/buscommand/ResetSearchFreqCommandTest.java
@@ -0,0 +1,14 @@
+package seedu.duke.logic.commands.buscommand;
+
+import org.junit.jupiter.api.BeforeAll;
+import seedu.duke.model.favorite.Fav;
+import seedu.duke.model.favorite.FavList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResetSearchFreqCommandTest {
+    @BeforeAll
+    public static void makeList() {
+    }
+
+}
