ee8d4664f01dbd09507873784d0eae06fcd743d3
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-11-08 10:24:26 +0800

    implement 2nd layer of setters and getters

--- src/main/java/seedu/dietbook/saveload/FoodPortionDateSaveLoadManager.java
+++ src/main/java/seedu/dietbook/saveload/FoodPortionDateSaveLoadManager.java
@@ -3,12 +3,13 @@ package seedu.dietbook.saveload;
 import seedu.dietbook.food.Food;
 
 import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
 
 /***
  * this class takes care of saving and loading of food, portion and date
  */
 public class FoodPortionDateSaveLoadManager {
-    private static final int DEFAULT_SAVER_WIDTH = 7;
+    private static final int DEFAULT_SAVER_WIDTH = 8;
     private static final int DEFAULT_SAVER_HEIGHT = 10;
 
     private static final int FOOD_NAME_INDEX = 1;
@@ -20,7 +21,8 @@ public class FoodPortionDateSaveLoadManager {
     private static final int DAY_INDEX = 7;
     private static final int MONTH_INDEX = 8;
     private static final int YEAR_INDEX = 9;
-    private static final int TIME_INDEX = 10;
+    private static final int HOUR_INDEX = 10;
+    private static final int MINUTE_INDEX = 11;
 
     private static final String DEFAULT_FOOD_NAME = "No Food Name";
     private static final String DEFAULT_FOOD_CALORIE = "0";
@@ -31,8 +33,8 @@ public class FoodPortionDateSaveLoadManager {
     private static final String DEFAULT_DAY = "1";
     private static final String DEFAULT_MONTH= "1";
     private static final String DEFAULT_YEAR = "2000";
-    private static final String DEFAULT_TIME = "0";
-
+    private static final String DEFAULT_MINUTE = "0";
+    private static final String DEFAULT_HOUR = "0";
 
     private static final String FOOD_FOLDER_NAME = "Food##PORTION###dDATE##folder";
 
@@ -47,6 +49,8 @@ public class FoodPortionDateSaveLoadManager {
         this.fileLoader = Loader.loadEmpty();
     }
 
+    // ------ saving and loading functions ----------
+
     /**
      * Call this function to load a food file.
      * @param fileName name of file
@@ -60,6 +64,10 @@ public class FoodPortionDateSaveLoadManager {
         this.fileLoader = Loader.loadEmpty();
     }
 
+    public void save(String fileName){
+        this.saver.save(FOOD_FOLDER_NAME, fileName);
+    }
+
     /**
      * Clears the saver and sets the number of entries it can take
      * call this function at the start of a series of functions to store data
@@ -70,21 +78,86 @@ public class FoodPortionDateSaveLoadManager {
         saver.resetSize(DEFAULT_SAVER_WIDTH, numEntry);
     }
 
-    public void addFood(Food food, int entryNumber){
+    // ------- setters and getters ----------
+
+    public void setFood(Food food, int entryNumber){
         setFoodName(food.getName(), entryNumber);
-        addFoodCalorie(food.getCalorie(), entryNumber);
+        setFoodCalorie(food.getCalorie(), entryNumber);
         setFoodCarbohydrate(food.getCarbohydrate(), entryNumber);
-        addFoodProtein(food.getProtein(), entryNumber);
-        addFoodFat(food.getFat(), entryNumber);
+        setFoodProtein(food.getProtein(), entryNumber);
+        setFoodFat(food.getFat(), entryNumber);
     }
 
-    public Food getFood(Integer entryNumber) throws IllegalAccessException {
+    public Food getFood(int entryNumber) throws IllegalAccessException {
         String name = getFoodName(entryNumber);
         int calorie = getFoodCalorie(entryNumber);
         int carbohydrate = getFoodCarbohydrate(entryNumber);
         int protein = getFoodProtein(entryNumber);
         int fat = getFoodFat(entryNumber);
         return new Food(name, calorie, carbohydrate, protein, fat);
+    }
+
+    public void setDateTime(LocalDateTime dateTime, int entryNumber){
+        setDay(dateTime.getDayOfMonth(), entryNumber);
+        setMonth(dateTime.getMonthValue(), entryNumber);
+        setYear(dateTime.getYear(), entryNumber);
+        setHour(dateTime.getHour(), entryNumber);
+        setMinute(dateTime.getMinute(), entryNumber);
+    }
+
+    public LocalDateTime getDateTime(int entryNumber) throws IllegalAccessException {
+        int day = Integer.parseInt(fileLoader.get(DAY_INDEX, entryNumber).orElse(DEFAULT_DAY));
+        int month = Integer.parseInt(fileLoader.get(MONTH_INDEX, entryNumber).orElse(DEFAULT_MONTH));
+        int year = Integer.parseInt(fileLoader.get(YEAR_INDEX, entryNumber).orElse(DEFAULT_YEAR));
+        int hour = Integer.parseInt(fileLoader.get(HOUR_INDEX, entryNumber).orElse(DEFAULT_HOUR));
+        int minute = Integer.parseInt(fileLoader.get(MINUTE_INDEX, entryNumber).orElse(DEFAULT_MINUTE));
+        return LocalDateTime.of(year, month, day, hour, minute);
+    }
+
+    // ------ Basic setters and getters -------
+
+    private void setFoodCarbohydrate( int carbohydrate, int entryNumber) {
+        saver.add(Integer.toString(carbohydrate),  FOOD_CARBOHYDRATE_INDEX, entryNumber);
+    }
+
+    private void setFoodName(String name, int entryNumber) {
+        saver.add(name, FOOD_NAME_INDEX, entryNumber);
+    }
+
+    private void setFoodCalorie(int calorie, int entryNumber) {
+        saver.add(Integer.toString(calorie), FOOD_CALORIE_INDEX, entryNumber);
+    }
+
+    private void setFoodProtein(int protein, int entryNumber){
+        saver.add(Integer.toString(protein), FOOD_PROTEIN_INDEX, entryNumber);
+    }
+
+    private void setFoodFat(int fat, int entryNumber){
+        saver.add(Integer.toString(fat), FOOD_FAT_INDEX, entryNumber);
+    }
+
+    public void setPortionSize(int portionSize, Integer entryNumber){
+        saver.add(Integer.toString(portionSize), PORTION_SIZE_INDEX, entryNumber);
+    }
+
+    public void setDay(int day, int entryNumber){
+        saver.add(Integer.toString(day), DAY_INDEX, entryNumber);
+    }
+
+    public void setMonth(int month, int entryNumber){
+        saver.add(Integer.toString(month), MONTH_INDEX, entryNumber);
+    }
+
+    public void setYear(int year, int entryNumber){
+        saver.add(Integer.toString(year), YEAR_INDEX, entryNumber);
+    }
+
+    public void setHour(int hour, int entryNumber){
+        saver.add(Integer.toString(hour), HOUR_INDEX, entryNumber);
+    }
+
+    public void setMinute(int minute, int entryNumber){
+        saver.add(Integer.toString(minute), MINUTE_INDEX, entryNumber);
     }
 
     private String getFoodName(Integer entryNumber) throws IllegalAccessException {
@@ -107,46 +180,6 @@ public class FoodPortionDateSaveLoadManager {
         return Integer.parseInt(fileLoader.get(FOOD_CALORIE_INDEX, entryNumber).orElse(DEFAULT_FOOD_CALORIE));
     }
 
-    private void setFoodCarbohydrate( int carbohydrate, int entryNumber) {
-        saver.add(Integer.toString(carbohydrate),  FOOD_CARBOHYDRATE_INDEX, entryNumber);
-    }
-
-    private void setFoodName(String name, int entryNumber) {
-        saver.add(name, FOOD_NAME_INDEX, entryNumber);
-    }
-
-    private void addFoodCalorie(int calorie, int entryNumber) {
-        saver.add(Integer.toString(calorie), FOOD_CALORIE_INDEX, entryNumber);
-    }
-
-    private void addFoodProtein(int protein, int entryNumber){
-        saver.add(Integer.toString(protein), FOOD_PROTEIN_INDEX, entryNumber);
-    }
-
-    private void addFoodFat(int fat, int entryNumber){
-        saver.add(Integer.toString(fat), FOOD_FAT_INDEX, entryNumber);
-    }
-
-    public void setPortionSize(int portionSize, Integer entryNumber){
-        saver.add(Integer.toString(portionSize), PORTION_SIZE_INDEX, entryNumber);
-    }
-
-    public void setDay(int day, int entryNumber){
-        saver.add(Integer.toString(day), DAY_INDEX, entryNumber);
-    }
-
-    public void setMonth(int month, int entryNumber){
-        saver.add(Integer.toString(month), MONTH_INDEX, entryNumber);
-    }
-
-    public void setYear(int year, int entryNumber){
-        saver.add(Integer.toString(year), YEAR_INDEX, entryNumber);
-    }
-
-    public void setTime(int time, int entryNumber){
-        saver.add(Integer.toString(time), TIME_INDEX, entryNumber);
-    }
-
     public int getPortionSize(int entryNumber) throws IllegalAccessException {
         return Integer.parseInt(fileLoader.get(PORTION_SIZE_INDEX, entryNumber).orElse(DEFAULT_PORTION_SIZE));
     }
@@ -163,7 +196,11 @@ public class FoodPortionDateSaveLoadManager {
         return Integer.parseInt(fileLoader.get(YEAR_INDEX, entryNumber).orElse(DEFAULT_YEAR));
     }
 
-    public int getTime(int entryNumber) throws IllegalAccessException {
-        return Integer.parseInt(fileLoader.get(TIME_INDEX, entryNumber).orElse(DEFAULT_TIME));
+    public int getHour(int entryNumber) throws IllegalAccessException {
+        return Integer.parseInt(fileLoader.get(HOUR_INDEX, entryNumber).orElse(DEFAULT_HOUR));
+    }
+
+    public int getMinute(int entryNumber) throws IllegalAccessException {
+        return Integer.parseInt(fileLoader.get(MINUTE_INDEX, entryNumber).orElse(DEFAULT_MINUTE));
     }
 }
---
+++ src/test/java/seedu/dietbook/saveload/FoodPortionDateSaveLoadManagerTest.java
@@ -0,0 +1,15 @@
+package seedu.dietbook.saveload;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodPortionDateSaveLoadManagerTest {
+    FoodPortionDateSaveLoadManager test;
+
+    @BeforeEach
+    private void setUp(){
+        test = new FoodPortionDateSaveLoadManager();
+        
+    }
+}
