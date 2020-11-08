2e6899d1aa1ac014a4f12a2029aa7ae6604720ee
gua-guargia <georgiazhang221@gmail.com>
2020-11-08 22:43:39 +0800

    Add javadoc for BackCommandParser, ShowRateCommand and ShowRateCommandParser

--- src/main/java/parser/BackCommandParser.java
+++ src/main/java/parser/BackCommandParser.java
@@ -11,10 +11,22 @@ import static common.Messages.MESSAGE_MISSING_ARGS;
 import static common.Messages.MODULE;
 
 //@@author gua-guargia
+/**
+ * Parses input arguments and creates a new BackModuleCommand or BackAdminCommand object.
+ */
 public class BackCommandParser {
     private static final String MESSAGE_INCORRECT_ACCESS = "Back command can only be called "
             + "at module and chapter level.\n";
 
+    /**
+     * Parses the given arguments in the context of the BackCommand.
+     *
+     * @param commandArgs input arguments of the command
+     * @param accessLevel access level of the user
+     * @return an BackModuleCommand or BackAdminCommand object based on the access level
+     * @throws InvalidInputException if the user input is not of the expected format
+     * @throws IncorrectAccessLevelException if the command cannot be executed at the access level
+     */
     public Command parse(String commandArgs, String accessLevel)
             throws InvalidInputException, IncorrectAccessLevelException {
         switch (accessLevel) {
--- src/main/java/parser/ShowRateCommandParser.java
+++ src/main/java/parser/ShowRateCommandParser.java
@@ -10,8 +10,21 @@ import static common.Messages.MESSAGE_EXTRA_ARGS;
 import static common.Messages.MESSAGE_INVALID_ACCESS;
 
 //@@author gua-guargia
+
+/**
+ * Parses input arguments and creates a new ShowRateCommand object.
+ */
 public class ShowRateCommandParser {
 
+    /**
+     * Parses the given arguments in the context of the ShowRateCommand.
+     *
+     * @param commandArgs input arguments of the command
+     * @param accessLevel access level of the user
+     * @return an ShowRateCommand object
+     * @throws InvalidInputException if the user input is not of the expected format
+     * @throws IncorrectAccessLevelException if the access level is not at chapter level
+     */
     public Command parse(String commandArgs, String accessLevel)
             throws IncorrectAccessLevelException, InvalidInputException {
         if (!accessLevel.equals(CHAPTER)) {
