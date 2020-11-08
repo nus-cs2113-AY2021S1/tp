61f81ac9c094b1759d67e04ddbebb3ebed19cd97
Johnson-Yee <e0494363@u.nus.edu>
2020-11-08 21:24:12 +0800

    Update to remove duplicate function call

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -126,8 +126,9 @@ public class Ui {
     }
 
     public static void printMostSearchedBusStop() {
-        if (BusStops.mostSearchedBusStop() != null) {
-            System.out.println("Your most searched bus stop is: " + BusStops.mostSearchedBusStop().getName());
+        BusStops mostSearchedBusStop = BusStops.mostSearchedBusStop();
+        if (mostSearchedBusStop != null) {
+            System.out.println("Your most searched bus stop is: " + mostSearchedBusStop.getName());
         } else {
             System.out.println("As you search more, your most frequently searched bus stop will appear here "
                     + "each time\nyou run this program!");
