169c2b957d44a43c64631373423634d76b7ebdbf
Ong De Zhi <39303087+OngDeZhi@users.noreply.github.com>
2020-11-08 14:38:50 +0800

    Remove unused import

--- src/test/java/anichan/parser/EstimateParserTest.java
+++ src/test/java/anichan/parser/EstimateParserTest.java
@@ -1,6 +1,5 @@
 package anichan.parser;
 
-import anichan.commands.EstimateCommand;
 import anichan.exception.AniException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
