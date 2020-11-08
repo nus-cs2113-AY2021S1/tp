6cf265dae533cddef7d00aa80e49fef8f9fcb595
DESKTOP-OV9KQRO\User <E0389113@u.nus.edu>
2020-11-08 16:51:29 +0800

    Java doc for bye help repeat

--- src/main/java/seedu/duke/command/HelpCommand.java
+++ src/main/java/seedu/duke/command/HelpCommand.java
@@ -12,6 +12,11 @@ public class HelpCommand extends Command {
     ArrayList<String> helpStuff;
     String helpTopic;
 
+    /**
+     * Creates a help command object to be executed.
+     *
+     * @param argument is a String containing the user input
+     */
     public HelpCommand(String argument) {
         this.isExit = false;
         this.command = null;
@@ -24,6 +29,15 @@ public class HelpCommand extends Command {
 
     }
 
+    /**
+     * Function will execute the command. Prints out the help information requested by user
+     *
+     * @param data    object of UserData class containing user's data.
+     * @param ui      object of UI class that contains the user interface needed to print the help file
+     * @param storage Storage oject with the save file path to write to to save changes.
+     * @throws InvalidHelpTopicException if there is no such help topic available
+     */
+    @Override
     public void execute(UserData data, Ui ui, Storage storage) throws InvalidHelpTopicException {
         storage.loadSystemResources("/helpfile.txt", helpStuff);
         int beginIndex = searchIndex(helpStuff, true);
@@ -40,6 +54,12 @@ public class HelpCommand extends Command {
 
     }
 
+    /**
+     * Finds the line number of the start and end of the help topic located in the txt file.
+     * @param helpData ArrayList of Strings containing all help file info loaded from the helpfile.txt
+     * @param isBegin boolean Set to True to find the beginning of the help topic, set to false to find the end.
+     * @return integer of line number of the help topic beginning or end
+     */
     private int searchIndex(ArrayList<String> helpData, boolean isBegin) {
 
         String toSearchFor;
