424e846a6a2ddd349a78d1583c59b222b312f1e1
hui444 <ygreek@gmail.com>
2020-11-09 15:28:07 +0800

    Fix code style

--- src/main/java/fitr/common/RemoveLeadingZeros.java
+++ src/main/java/fitr/common/RemoveLeadingZeros.java
@@ -1,11 +1,9 @@
 package fitr.common;
 
-import static fitr.common.Messages.EMPTY_STRING;
-
 public class RemoveLeadingZeros {
     public static String removeLeadingZeros(String number) {
         String strPattern = "^0+(?!$)";
-        number = number.replaceAll(strPattern, EMPTY_STRING);
+        number = number.replaceAll(strPattern, "");
         return number;
     }
 }
