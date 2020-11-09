748ca397a566a3a528b139225ef4247d638bbbdf
abnermtj <abnermtj@live.com.sg>
2020-11-09 18:14:40 +0800

    added ui stub

--- src/test/java/command/stuba/UiStub.java
+++ src/test/java/command/stuba/UiStub.java
@@ -2,6 +2,24 @@ package command.stuba;
 
 import ui.Ui;
 
-public class UiStub extends Ui {
+import java.util.LinkedList;
 
+class UiStub extends Ui {
+    private LinkedList<String> userInput;
+
+    protected UiStub() {
+        userInput = new LinkedList<>();
+    }
+
+    protected void pushUserInput(String userInput) {
+        this.userInput.push(userInput);
+    }
+
+    public String getUserInput() {
+        return userInput.removeLast();
+    }
+
+    protected void clearUserInput() {
+        userInput.clear();
+    }
 }
