dd2a3d5e0f0beb279d1fba4730059d32fcfdef99
e0406981 <e0406981@u.nus.edu>
2020-11-09 11:39:21 +0800

    more javadoc

--- src/main/java/seedu/parser/Parser.java
+++ src/main/java/seedu/parser/Parser.java
@@ -20,69 +20,83 @@ import java.util.regex.Matcher;
 
 public class Parser {
 
-
+    /**
+     * Parses the raw user input to make sense of what was inputed.
+     *
+     * @param rawInput the raw user input
+     * @return the command to be executed
+     * @throws UnknowCommandException     the the method can't make sense of the input, default case
+     * @throws InvalidCommandException    if input format is wrong
+     * @throws InvalidTaskNumberException for edit/delete, if task index is wrong
+     */
     public Command processRaw(String rawInput) throws
             UnknowCommandException, InvalidCommandException, InvalidTaskNumberException {
         Matcher matcher;
         String commandWord = getCommandWord(rawInput);
         String commandArgs = getCommandArgs(rawInput);
         switch (commandWord) {
-        case HelpCommand.COMMAND_WORD:
-            return new HelpCommand();
-        case ListCommand.COMMAND_WORD:
-            matcher = ListCommand.COMMAND_PATTERN.matcher(commandArgs);
-            if (matcher.find()) {
-                return new ListCommand("-d".equals(matcher.group("dateFlag")),
-                        "-p".equals(matcher.group("priorityFlag")),
-                        "-w".equals(matcher.group("displayByWeek")),
-                        "-m".equals(matcher.group("displayByMonth")),
-                        matcher.group("date"));
-            } else {
-                throw new InvalidCommandException();
-            }
-        case ByeCommand.COMMAND_WORD:
-            return new ByeCommand();
-        case ClearCommand.COMMAND_WORD:
-            return new ClearCommand();
-        case AddCommand.COMMAND_WORD:
-            matcher = AddCommand.COMMAND_PATTERN.matcher(commandArgs);
-            if (matcher.find()) {
-                return new AddCommand(matcher.group("description"), matcher.group("date"), matcher.group("st"),
-                        matcher.group("et"), matcher.group("priority"), matcher.group("reminder"),
-                        matcher.group("t"));
-            } else {
-                throw new InvalidCommandException();
-            }
-        case EditCommand.COMMAND_WORD:
-            matcher = EditCommand.COMMAND_PATTERN.matcher(commandArgs);
-            if (matcher.find()) {
-                return new EditCommand(matcher.group("key"), matcher.group("description"),
-                        matcher.group("date"), matcher.group("st"), matcher.group("et"),
-                        matcher.group("priority"), matcher.group("reminder"), matcher.group("t"));
-            } else {
-                throw new InvalidCommandException();
-            }
-        case SearchCommand.COMMAND_WORD:
-            matcher = SearchCommand.COMMAND_PATTERN.matcher(commandArgs);
-            if (matcher.find()) {
-                return new SearchCommand(matcher.group("toSearch"));
-            } else {
-                throw new InvalidCommandException();
-            }
-        case DeleteCommand.COMMAND_WORD:
-            matcher = DeleteCommand.COMMAND_PATTERN.matcher(commandArgs);
-            if (matcher.find()) {
-                return new DeleteCommand(matcher.group("key"));
-            } else {
-                throw new InvalidCommandException();
-            }
-        case UndoCommand.COMMAND_WORD:
-            return new UndoCommand();
-        default:
-            throw new UnknowCommandException();
+            case HelpCommand.COMMAND_WORD:
+                return new HelpCommand();
+            case ListCommand.COMMAND_WORD:
+                matcher = ListCommand.COMMAND_PATTERN.matcher(commandArgs);
+                if (matcher.find()) {
+                    return new ListCommand("-d".equals(matcher.group("dateFlag")),
+                            "-p".equals(matcher.group("priorityFlag")),
+                            "-w".equals(matcher.group("displayByWeek")),
+                            "-m".equals(matcher.group("displayByMonth")),
+                            matcher.group("date"));
+                } else {
+                    throw new InvalidCommandException();
+                }
+            case ByeCommand.COMMAND_WORD:
+                return new ByeCommand();
+            case ClearCommand.COMMAND_WORD:
+                return new ClearCommand();
+            case AddCommand.COMMAND_WORD:
+                matcher = AddCommand.COMMAND_PATTERN.matcher(commandArgs);
+                if (matcher.find()) {
+                    return new AddCommand(matcher.group("description"), matcher.group("date"), matcher.group("st"),
+                            matcher.group("et"), matcher.group("priority"), matcher.group("reminder"),
+                            matcher.group("t"));
+                } else {
+                    throw new InvalidCommandException();
+                }
+            case EditCommand.COMMAND_WORD:
+                matcher = EditCommand.COMMAND_PATTERN.matcher(commandArgs);
+                if (matcher.find()) {
+                    return new EditCommand(matcher.group("key"), matcher.group("description"),
+                            matcher.group("date"), matcher.group("st"), matcher.group("et"),
+                            matcher.group("priority"), matcher.group("reminder"), matcher.group("t"));
+                } else {
+                    throw new InvalidCommandException();
+                }
+            case SearchCommand.COMMAND_WORD:
+                matcher = SearchCommand.COMMAND_PATTERN.matcher(commandArgs);
+                if (matcher.find()) {
+                    return new SearchCommand(matcher.group("toSearch"));
+                } else {
+                    throw new InvalidCommandException();
+                }
+            case DeleteCommand.COMMAND_WORD:
+                matcher = DeleteCommand.COMMAND_PATTERN.matcher(commandArgs);
+                if (matcher.find()) {
+                    return new DeleteCommand(matcher.group("key"));
+                } else {
+                    throw new InvalidCommandException();
+                }
+            case UndoCommand.COMMAND_WORD:
+                return new UndoCommand();
+            default:
+                throw new UnknowCommandException();
         }
     }
 
+    /**
+     * Returns the command word string. e.g add/edit/delete
+     *
+     * @param rawInput the user input.
+     * @return the command word.
+     */
     public String getCommandWord(String rawInput) {
         String commandWord = rawInput;
         if (rawInput.contains(" ")) {
@@ -92,6 +106,12 @@ public class Parser {
         return commandWord.trim();
     }
 
+    /**
+     * Returns the arguments, which is everything after the command word.
+     *
+     * @param rawInput the user input.
+     * @return the argument of the command.
+     */
     public String getCommandArgs(String rawInput) {
         String commandArgs = "";
         if (rawInput.contains(" ")) {
