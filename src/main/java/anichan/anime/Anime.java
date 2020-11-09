ab11c7367532c1933b0e0b140f337798bece1ced
Wei Chin <eyoweichin@hotmail.com>
2020-11-09 16:21:22 +0800

    Add Javadocs for Anime.java

--- src/main/java/anichan/anime/Anime.java
+++ src/main/java/anichan/anime/Anime.java
@@ -27,6 +27,9 @@ public class Anime {
     private int avgEpisodeLength;
     private int totalEpisodes;
 
+    /**
+     * Creates a default instance of an Anime object.
+     */
     public Anime() {
         this.animeID = ZERO_VALUE;
         this.animeName = EMPTY_STRING;
@@ -36,6 +39,17 @@ public class Anime {
         animeID = totalAnime;
     }
 
+    /**
+     * Creates an instance of an Anime object.
+     *
+     * @param animeName title of the anime
+     * @param releaseDate release date of the anime
+     * @param rating rating of the anime
+     * @param genre array of genres that the anime is
+     * @param avgEpisodeLength is the average episode length
+     * @param totalEpisodes is the total episode that the anime has
+     * @throws AniException if there is an error setting any of its parameters
+     */
     public Anime(String animeName, String[] releaseDate, int rating, String[] genre,
                  int avgEpisodeLength, int totalEpisodes) throws AniException {
         setAnimeName(animeName);
@@ -48,55 +62,120 @@ public class Anime {
         setAnimeID(totalAnime);
     }
 
+    /**
+     * Gets the Anime ID.
+     *
+     * @return Anime ID
+     */
     public int getAnimeID() {
         return animeID;
     }
 
+    /**
+     * Sets the Anime ID.
+     *
+     * @param animeID indicates the ID to set for this Anime
+     */
     public void setAnimeID(int animeID) {
         this.animeID = animeID;
     }
 
+    /**
+     * Sets the total episode.
+     *
+     * @param totalEpisodes indicates the total episode this Anime has
+     */
     public void setTotalEpisodes(int totalEpisodes) {
         this.totalEpisodes = totalEpisodes;
     }
 
+    /**
+     * Sets the average episode length.
+     *
+     * @param avgEpisodeLength indicates the average length of each episode in this Anime
+     */
     public void setAvgEpisodeLength(int avgEpisodeLength) {
         this.avgEpisodeLength = avgEpisodeLength;
     }
 
+    /**
+     * Gets the Anime total episode.
+     *
+     * @return the total episode of this Anime
+     */
     public int getTotalEpisodes() {
         return totalEpisodes;
     }
 
+    /**
+     * Gets the Anime name.
+     *
+     * @return name of the Anime
+     */
     public String getAnimeName() {
         return animeName;
     }
 
+    /**
+     * Sets the name.
+     *
+     * @param animeName indicates the name of this Anime
+     */
     public void setAnimeName(String animeName) {
         this.animeName = animeName;
     }
 
+    /**
+     * Gets the Anime release date.
+     *
+     * @return the release date of this Anime as a Date object
+     */
     public Date getReleaseDate() {
         return releaseDate;
     }
 
+    /**
+     * Gets the Anime release date as a String.
+     *
+     * @return the release date of this Anime as a String
+     */
     public String getReleaseDateInString() {
         SimpleDateFormat newDateFormat = new SimpleDateFormat(DATE_PATTERN_PRINT);
         return newDateFormat.format(releaseDate);
     }
 
+    /**
+     * Gets the Anime rating.
+     *
+     * @return the rating of this Anime
+     */
     public int getRating() {
         return rating;
     }
 
+    /**
+     * Gets the Anime genre.
+     *
+     * @return a String Array of this Anime's genre
+     */
     public String[] getGenre() {
         return genre;
     }
 
+    /**
+     * Sets the genres of this Anime.
+     *
+     * @param genre indicates all the genre this anime has
+     */
     public void setGenre(String[] genre) {
         this.genre = genre;
     }
 
+    /**
+     * Sets the class-level variable of totalAnime.
+     *
+     * @param totalAnime indicates the total number of Anime objects
+     */
     public static void setTotalAnime(int totalAnime) {
         Anime.totalAnime = totalAnime;
     }
@@ -136,6 +215,11 @@ public class Anime {
         }
     }
 
+    /**
+     * Overrides toString() to return Anime name instead.
+     *
+     * @return the name of the Anime
+     */
     @Override
     public String toString() {
         return getAnimeName();
