36fdba3edce1db26fcfc78648bb47018d61414fd
Lee-Juntong <2310266421@qq.com>
2020-11-09 16:47:09 +0800

    Added the Command to print the locations available at the selected area

---
+++ src/main/java/command/PrintAreaLocationsCommand.java
@@ -0,0 +1,45 @@
+package command;
+
+import eventlist.EventList;
+import exception.NuScheduleException;
+import locationlist.BusStopList;
+import locationlist.LocationList;
+import storage.Storage;
+import ui.UI;
+
+/**
+ * Represents the command for printing locations available at some areas in NUS.
+ */
+public class PrintAreaLocationsCommand extends Command {
+    String area;
+
+    public PrintAreaLocationsCommand(String area) {
+        this.area = area;
+    }
+
+    /**
+     * Purely print the available locations at the area selected by the user.
+     *
+     * @param events    the list of Events.
+     * @param locations the list of Locations.
+     * @param busStops  the list of BusStops.
+     * @param ui        do outputs.
+     * @param storage   store the data.
+     */
+    @Override
+    public void execute(EventList events, LocationList locations, BusStopList busStops, UI ui, Storage storage) {
+        switch (area.toUpperCase()) {
+        case "FOS":
+            ui.printFosLocations();
+        case "FOE":
+            ui.printFoeLocations();
+        case "FASS":
+            ui.printFassLocations();
+        case "SDE":
+            ui.printSdeLocations();
+        case "SOC":
+        case "BIZ":
+            ui.printSocAndBizLocations();
+        }
+    }
+}
