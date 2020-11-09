595812bc7646c288d9549620d630bdff4c191e52
Ong De Zhi <39303087+OngDeZhi@users.noreply.github.com>
2020-11-09 13:58:33 +0800

    Fix variable naming for consistency

--- src/main/java/anichan/parser/EstimateParser.java
+++ src/main/java/anichan/parser/EstimateParser.java
@@ -14,12 +14,12 @@ import java.util.logging.Logger;
 public class EstimateParser extends CommandParser {
     private static final String WORDS_PER_HOUR_PARAM = "-wph";
     private static final String SCRIPT_FILE_FORMAT = ".txt";
-    private static final String INVALID_FILE_NAME_CHARACTERS_REGEX = "^.*[\\\\/:*?\"<>|].*$";
     private static final String DOT = ".";
     private static final String REGEX_LAST_DOT = "\\.(?=[^.]*$)";
+    private static final String REGEX_INVALID_FILE_NAME_CHARACTER = "^.*[\\\\/:*?\"<>|].*$";
 
     private static final String NO_SCRIPT_FILE_SPECIFIED_ERROR = "No script file specified!";
-    private static final String FILE_NAME_CONTAINS_INVALID_CHARACTERS = "File name contains invalid characters!";
+    private static final String FILE_NAME_CONTAINS_INVALID_CHARACTERS_ERROR = "File name contains invalid characters!";
     private static final String INVALID_SCRIPT_FILE_FORMAT_ERROR = "Only \".txt\" script files are accepted!";
     private static final String INVALID_PARAMETER_ERROR = "Estimate command only accepts the parameter: -wph.";
     private static final String ESTIMATE_COMMAND_TOO_MUCH_FIELDS_ERROR = "Estimate command" + TOO_MUCH_FIELDS;
@@ -116,8 +116,8 @@ public class EstimateParser extends CommandParser {
      */
     private boolean isValidFileName(String fileName) throws AniException {
         // If two files are specified, then they are treated as the filename for one file.
-        if (fileName.matches(INVALID_FILE_NAME_CHARACTERS_REGEX)) {
-            throw new AniException(FILE_NAME_CONTAINS_INVALID_CHARACTERS);
+        if (fileName.matches(REGEX_INVALID_FILE_NAME_CHARACTER)) {
+            throw new AniException(FILE_NAME_CONTAINS_INVALID_CHARACTERS_ERROR);
         }
 
         return fileName.trim().endsWith(SCRIPT_FILE_FORMAT);
