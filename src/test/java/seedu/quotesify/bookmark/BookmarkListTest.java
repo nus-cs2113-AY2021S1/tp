5106da806a2301c53cc1dd6bbd1b0400d1e79762
lunzard <60387314+lunzard@users.noreply.github.com>
2020-11-09 12:10:35 +0800

    update Junit Test

--- src/test/java/seedu/quotesify/bookmark/BookmarkListTest.java
+++ src/test/java/seedu/quotesify/bookmark/BookmarkListTest.java
@@ -1,4 +1,66 @@
 package seedu.quotesify.bookmark;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.quotesify.author.Author;
+import seedu.quotesify.book.Book;
+import seedu.quotesify.book.BookList;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookmarkListTest {
+    private BookList bookList;
+    private BookmarkList bookmarkList;
+    private Book book1;
+    private Author author1;
+    private Bookmark bookmark1;
+    private Book book2;
+    private Author author2;
+    private Bookmark bookmark2;
+
+    private int pageNum1 = 66;
+    private String bookName1 = "Harry Potter";
+    private String authorName1 = "JK Rowling";
+
+    private int pageNum2 = 99;
+    private String bookName2 = "JM Barrie";
+    private String authorName2 = "Peter Pan";
+
+    @BeforeEach
+    void setUp() {
+        author1 = new Author(authorName1);
+        book1 = new Book(author1, bookName1);
+        bookList.add(book1);
+        bookmark1 = new Bookmark(book1, pageNum1);
+        bookmarkList.add(bookmark1);
+
+        author2 = new Author(authorName2);
+        book2 = new Book(author2, bookName2);
+        bookList.add(book2);
+        bookmark2 = new Bookmark(book2, pageNum2);
+        bookmarkList.add(bookmark2);
+    }
+
+    @Test
+    public void findByIndex_LargerThanSize() {
+        Bookmark bookmarkResult = bookmarkList.findByIndex(2);
+        assertEquals(null, bookmarkResult);
+    }
+
+    @Test
+    public void findByIndex_withinSize() {
+        Bookmark bookmarkResult = bookmarkList.findByIndex(0);
+        assertEquals(book1, bookmarkResult);
+    }
+
+    @Test
+    public void toStringTest() {
+        String output = "1. \"Harry Potter\" at page: 66" + System.lineSeparator()
+                + "2. \"JM Barrie\" at page: 99" + System.lineSeparator();
+        assertEquals(output, bookmarkList.toString());
+    }
+
+
+
+
 }
--- src/test/java/seedu/quotesify/bookmark/BookmarkTest.java
+++ src/test/java/seedu/quotesify/bookmark/BookmarkTest.java
@@ -1,4 +1,30 @@
 package seedu.quotesify.bookmark;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.quotesify.author.Author;
+import seedu.quotesify.book.Book;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BookmarkTest {
+    private Book book;
+    private Author author;
+    private Bookmark bookmark;
+
+    private int pageNum = 66;
+    private String bookName = "Harry Potter";
+    private String authorName = "JK Rowling";
+
+    @BeforeEach
+    public void SetUp() {
+        author = new Author(authorName);
+        book = new Book(author, bookName);
+        bookmark = new Bookmark(book, pageNum);
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("\"Harry Potter\" at page: 66", bookmark.toString() );
+    }
 }
