e3c373974651d6aec3a1b4426e2adc4f4df788e2
Jaden <jadenwong@mozat.com>
2021-04-05 17:45:00 +0800

    Add Junit test for CommandEvaluator

--- src/test/java/seedu/hdbuy/parser/CommandEvaluatorTest.java
+++ src/test/java/seedu/hdbuy/parser/CommandEvaluatorTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class CommandEvaluatorTest {
 
-    @Test void filterTest() {
+    @Test void filterNegativeTest() {
         List<String> testInputs = new ArrayList<String>(Arrays.asList(
                 "filter location",
                 "filter",
@@ -29,75 +29,111 @@ class CommandEvaluatorTest {
                 "filter lease_remaining 100",
                 "filter lease_remaining 99.0",
                 "filter lease_remaining -1",
-                "filter lease_remaining"
+                "filter lease_remaining",
+                "   filter    location",
+                "   filter",
+                "   filter   type   7    room",
+                "    filter    type   4_room",
+                "filter type 4_room",
+                "   filter    type    ",
+                "   filter   type    10room",
+                "   filter    lease_remaining    woodlands",
+                "   filter    lease_remaining    100",
+                "   filter     lease_remaining    99.0",
+                "   filter   lease_remaining     -1",
+                "     filter     lease_remaining"
         ));
+        negativeTest(testInputs);
+    }
+
+    @Test void filterPositiveTest() {
+        List<String> testInputs = new ArrayList<String>(Arrays.asList(
+                "filter location ang mo kio",
+                "  filter    location   ang   mo   kio   ",
+                "filter location bishan",
+                "   filter    location    bishan  ",
+                "   filter type 4 room",
+                "   filter    type    4    room",
+                "   filter    type    executive",
+                "   filter    type    4    room",
+                "filter type 4 room",
+                "filter    type    4    room",
+                "filter    type    executive",
+                "filter    type    4    room",
+                "   filter    lease_remaining    50   ",
+                "filter lease_remaining 50",
+                "filter lease_remaining 0",
+                "filter lease_remaining 99"
+        ));
+        positiveTest(testInputs);
+    }
+
+    @Test void sortNegativeTest() {
+        List<String> testInputs = new ArrayList<String>(Arrays.asList(
+                "sort",
+                "   sort   ",
+                "sort gg",
+                "  sort   gg  ",
+                "sort asc desc",
+                "  sort    asc   desc"
+        ));
+        negativeTest(testInputs);
+    }
+
+    @Test void sortPositiveTest() {
+        List<String> testInputs = new ArrayList<String>(Arrays.asList(
+                "sort asc",
+                "   sort  asc   ",
+                "sort desc",
+                "  sort   desc  "
+        ));
+        positiveTest(testInputs);
+    }
+
+    @Test void editPositiveTest() {
+        List<String> testInputs = new ArrayList<String>(Arrays.asList(
+                "save 1",
+                "   save  1   ",
+                "remove 1",
+                "  remove   1  "
+        ));
+        positiveTest(testInputs);
+    }
+
+    @Test void editNegativeTest() {
+        List<String> testInputs = new ArrayList<String>(Arrays.asList(
+                "save qowidno",
+                "   save  kqwdbqk  ",
+                "remove qwdionwq",
+                "  remove   oqwdiqw  ",
+                "save",
+                "   save    ",
+                "remove",
+                "  remove     ",
+                "save 1.0",
+                "   save  1.0  ",
+                "remove 1.0",
+                "  remove   1.0  "
+        ));
+        negativeTest(testInputs);
+    }
+
+    private void negativeTest(List<String> testInputs) {
         for (String input : testInputs) {
             try {
                 CommandKey output = CommandEvaluator.extractInfo(input);
                 assertNull(output);
-            } catch (InvalidParameterException | InvalidIndexException e) {
+            } catch (Exception e) {
                 // continue
             }
         }
     }
 
-    @Test void trimTest() {
-        List<String> testInputs = new ArrayList<String>(Arrays.asList(
-                "filter location ang mo kio",
-                "filter    location   ang    mo    kio",
-                "filter location   ang    mo    kio",
-                "filter location woodlands",
-                "filter     location    woodlands",
-                "sort asc",
-                "sort   asc   ",
-                "sort desc     ",
-                "save 1",
-                "save   1   ",
-                "save 1    ",
-                "save   1",
-                "remove 1",
-                "remove   1   ",
-                "remove 1    ",
-                "remove   1",
-                "save 0",
-                "save -1",
-                "save 101",
-                "save 50.5",
-                "save -50.5",
-                "   filter location ang mo kio",
-                "   filter    location   ang    mo    kio",
-                "  filter location   ang    mo    kio",
-                "  filter location woodlands",
-                "  filter     location    woodlands",
-                " sort asc",
-                "    sort   asc   ",
-                " sort desc     ",
-                "   save 1",
-                "  save   1   ",
-                "  save 1    ",
-                "  save   1",
-                " remove 1",
-                "    remove   1   ",
-                "   remove 1    ",
-                "   remove   1",
-                "  save 0",
-                "  save -1",
-                "  save 101",
-                " save 50.5",
-                "  save -50.5"
-        ));
-
+    private void positiveTest(List<String> testInputs) {
         for (String input : testInputs) {
             try {
-                CommandEvaluator.extractInfo(input);
-                HdBuyLogger.info(input + " - passed");
-            } catch (InvalidParameterException e) {
-                fail();
-            } catch (InvalidIndexException e) {
-                String index = e.getIndex();
-                if (index.equals("-1") || index.equals("0") || index.equals("101") || index.equals("50.5") || index.equals("-50.5")) {
-                    continue;
-                }
+                CommandKey output = CommandEvaluator.extractInfo(input);
+            } catch (Exception e) {
                 fail();
             }
         }
