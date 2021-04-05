cf29b2eba73de3ace6a34579ce0c5b71d4bcdfa3
jadenwjh <wongjunhong9723@gmail.com>
2021-04-05 20:40:51 +0800

    no message

--- src/test/java/seedu/hdbuy/HdBuyTest.java
+++ src/test/java/seedu/hdbuy/HdBuyTest.java
@@ -2,29 +2,14 @@ package seedu.hdbuy;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
-import seedu.hdbuy.common.exception.InvalidIndexException;
-import seedu.hdbuy.common.exception.InvalidParameterException;
-import seedu.hdbuy.parser.CommandEvaluator;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.io.ByteArrayInputStream;
 
 class HdBuyTest {
 
     @Test public void testMain() {
-        final FileInputStream testInput;
-        try {
-            testInput = new FileInputStream(new File("./text-ui-test/input.txt"));
-            System.setIn(testInput);
-            HdBuy.main(null);
-        } catch (FileNotFoundException e) {
-            fail();
-        }
+        String testInput = "exit";
+        ByteArrayInputStream bytes = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(bytes);
+        HdBuy.main(null);
     }
 }
