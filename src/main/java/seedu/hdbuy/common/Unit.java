4b93425e6711a779485d167d459d36ac4315c394
Jaden <62279469+jadenwjh@users.noreply.github.com>
2021-04-09 23:32:52 +0800

    Update Unit.java

--- src/main/java/seedu/hdbuy/common/Unit.java
+++ src/main/java/seedu/hdbuy/common/Unit.java
@@ -19,66 +19,31 @@ public class Unit {
         this.address = address;
         this.id = id;
     }
-
-    /**
-     * Return the location of the Unit object.
-     *
-     * @return String of the location of the Unit object.
-     */
+    
     public String getLocation() {
         return location;
     }
-
-    /**
-     * Return the type of the Unit object.
-     *
-     * @return String of the type of the Unit object.
-     */
+    
     public String getType() {
         return type;
     }
-
-    /**
-     * Return the price of the Unit object.
-     *
-     * @return The integer value of the price of the Unit object.
-     */
+    
     public int getPrice() {
         return price;
     }
-
-    /**
-     * Return the address of the Unit object.
-     *
-     * @return String of the address of the Unit object.
-     */
+    
     public String getAddress() {
         return address;
     }
-
-    /**
-     * Return the ID of the Unit object.
-     *
-     * @return The integer value of the ID of the Unit object.
-     */
+    
     public int getId() {
         return id;
     }
-
-    /**
-     * Return the lease value of the Unit object.
-     *
-     * @return The integer value of the lease value of the Unit object.
-     */
+    
     public int getLeaseValue() {
         return leaseValue;
     }
-
-    /**
-     * Return if the Unit object is leashed.
-     *
-     * @return String if the Unit object is leashed.
-     */
+    
     public String getLease() {
         return lease;
     }
